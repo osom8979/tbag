86f1e997ec749e2485fba13ac0397a263ca0ce05
zer0 <osom8979@gmail.com>
2016-04-11 14:06:56 +0900

    Create SingletonLifeManager class.

--- libtbag/pattern/Singleton.hpp
+++ libtbag/pattern/Singleton.hpp
@@ -14,41 +14,158 @@
 #endif
 
 #include <libtbag/config.h>
+#include <libtbag/Noncopyable.hpp>
+#include <libtbag/pattern/Observer.hpp>
+
+#include <cstdlib>
 #include <atomic>
 #include <mutex>
 
+/**
+ * Don't use @c std::atexit function.
+ *
+ * @remarks
+ *  Default variable is true.
+ */
+constexpr bool isManualRelease() noexcept
+{
+    return false;
+}
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace pattern {
+
+/**
+ * SingletonLifeManager class prototype.
+ *
+ * @author zer0
+ * @date   2016-04-11
+ *
+ * @remarks
+ *  Management of Singleton classes.
+ */
+class SingletonLifeManager : public Noncopyable
+{
+public:
+    UnorderedObservable _observable;
+
+protected:
+    SingletonLifeManager() {
+        __EMPTY_BLOCK__
+    }
+
+public:
+    ~SingletonLifeManager() {
+        this->_observable.notify();
+    }
+
+public:
+    void add(std::function<void(void)> const & observer) {
+        this->_observable.add(observer);
+    }
+
+// Singleton section.
+private:
+    static std::atomic<SingletonLifeManager*> __instance;
+    static std::mutex __instance_lock;
+
+public:
+    static void releaseInstance() {
+        SingletonLifeManager * temp = __instance.load();
+        if (temp != nullptr) {
+            std::lock_guard<std::mutex> guard(__instance_lock);
+            temp = __instance.load();
+            if (temp != nullptr) {
+                delete temp; // Release, all singleton objects.
+                __instance.store(nullptr);
+            }
+        }
+    }
+
+public:
+    static SingletonLifeManager * getInstance() {
+        SingletonLifeManager * temp = __instance.load();
+        if (temp == nullptr) {
+            std::lock_guard<std::mutex> guard(__instance_lock);
+            temp = __instance.load();
+            if (temp == nullptr) {
+                temp = new SingletonLifeManager();
+                __instance.store(temp);
+
+                if (!isManualRelease()) {
+                    // Register release method.
+                    std::atexit(&SingletonLifeManager::releaseInstance);
+                }
+            }
+        }
+        return temp;
+    }
+};
+
+std::atomic<SingletonLifeManager*> SingletonLifeManager::__instance(nullptr);
+std::mutex SingletonLifeManager::__instance_lock;
+
+} // namespace pattern
+
 #ifndef SINGLETON_PROTOTYPE
-#define SINGLETON_PROTOTYPE(base_class)             \
-    protected:                                      \
-        base_class();                               \
-    public:                                         \
-        ~base_class();                              \
-    private:                                        \
-        static std::atomic<base_class*> __instance; \
-        static std::mutex __instance_lock;          \
-    public:                                         \
-        static base_class * getInstance();
+#define SINGLETON_PROTOTYPE(base_class)                         \
+    protected:                                                  \
+        base_class();                                           \
+    public:                                                     \
+        ~base_class();                                          \
+    private:                                                    \
+        static std::atomic<base_class*> __instance;             \
+        static std::mutex __instance_lock;                      \
+    private:                                                    \
+        static void releaseInstance();                          \
+    public:                                                     \
+        static base_class * getInstance();                      \
+    private: // default access modifier of class.
 #endif
 
 #ifndef SINGLETON_IMPLEMENT
 #define SINGLETON_IMPLEMENT(base_class)                             \
-        std::atomic<base_class*> base_class::__instance(nullptr);   \
-        std::mutex base_class::__instance_lock;                     \
+    std::atomic<base_class*> base_class::__instance(nullptr);       \
+    std::mutex base_class::__instance_lock;                         \
                                                                     \
-        base_class * base_class::getInstance()                      \
-        {                                                           \
-            base_class * temp = __instance.load();                  \
-            if (temp == nullptr) {                                  \
-                std::lock_guard<std::mutex> guard(__instance_lock); \
-                temp = __instance.load();                           \
-                if (temp == nullptr) {                              \
-                    temp = new base_class();                        \
-                    __instance.store(temp);                         \
-                }                                                   \
+    void base_class::releaseInstance() {                            \
+        base_class * temp = __instance.load();                      \
+        if (temp != nullptr) {                                      \
+            std::lock_guard<std::mutex> guard(__instance_lock);     \
+            temp = __instance.load();                               \
+            if (temp != nullptr) {                                  \
+                delete temp;                                        \
+                __instance.store(nullptr);                          \
             }                                                       \
-            return temp;                                            \
-        }
+        }                                                           \
+    }                                                               \
+                                                                    \
+    base_class * base_class::getInstance()                          \
+    {                                                               \
+        base_class * temp = __instance.load();                      \
+        if (temp == nullptr) {                                      \
+            std::lock_guard<std::mutex> guard(__instance_lock);     \
+            temp = __instance.load();                               \
+            if (temp == nullptr) {                                  \
+                temp = new base_class();                            \
+                __instance.store(temp);                             \
+                using namespace libtbag;                            \
+                using namespace libtbag::pattern;                   \
+                SingletonLifeManager::getInstance()->add([](){      \
+                        base_class::releaseInstance();              \
+                    });                                             \
+            }                                                       \
+        }                                                           \
+        return temp;                                                \
+    }
 #endif
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
 
 #endif // __INCLUDE_LIBTBAG__LIBTBAG_PATTERN_SINGLETON_HPP__
 
