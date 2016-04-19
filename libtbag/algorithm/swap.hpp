ac777d6cc66c8f0526791d31eb08e3fe7a541fc2
zer0 <osom8979@gmail.com>
2016-04-19 21:52:30 +0900

    Create algorithm::swapWithPod() function.

---
+++ libtbag/algorithm/swap.hpp
@@ -0,0 +1,50 @@
+/**
+ * @file   swap.hpp
+ * @brief  swap algorithm prototype.
+ * @author zer0
+ * @date   2016-04-19
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_ALGORITHM_SWAP_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_ALGORITHM_SWAP_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+#include <type_traits>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace algorithm {
+
+/**
+ * swap POD type.
+ *
+ * @translate{ko, @c std::swap 을 사용하는 것이 최적화에 유리하다.}
+ *
+ * @warning
+ *  @c std::swap is more advantageous for optimization.
+ */
+template <typename T>
+inline void swapWithPod(T & a, T & b)
+{
+    static_assert(std::is_pod<T>::value, "Not POD type.");
+
+    a ^= b;
+    b ^= a;
+    a ^= b;
+}
+
+} // namespace algorithm
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_ALGORITHM_SWAP_HPP__
+
---
+++ tester/algorithm/swapTest.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file   swapTest.cpp
+ * @brief  swap algorithm tester.
+ * @author zer0
+ * @date   2016-04-19
+ */
+
+#include <gtest/gtest.h>
+#include <libtbag/algorithm/swap.hpp>
+
+using namespace libtbag;
+
+TEST(swapTest, Default)
+{
+    int v1 = 10;
+    int v2 = 20;
+
+    algorithm::swapWithPod(v1, v2);
+    ASSERT_EQ(v1, 20);
+    ASSERT_EQ(v2, 10);
+}
+
