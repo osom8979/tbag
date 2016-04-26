0c8688b259e44d54c2b159e83d6fba22e7ac378b
zer0 <osom8979@gmail.com>
2016-04-26 13:44:39 +0900

    Remove LIBTBAG_CURSES_~ macros.

--- curses_app/main.cpp
+++ curses_app/main.cpp
@@ -10,117 +10,94 @@
 
 using namespace libtbag;
 
-void onlyTypeCoverage()
+void pressAnyKey(curses::Context & c)
 {
-    LIBTBAG_CURSES_SCREEN  * v1 = nullptr;
-    LIBTBAG_CURSES_WINDOW  * v2 = nullptr;
-    LIBTBAG_CURSES_CHAR    * v3 = nullptr;
-    LIBTBAG_CURSES_ATTR    * v4 = nullptr;
-    LIBTBAG_CURSES_COLOR   * v5 = nullptr;
-    LIBTBAG_CURSES_PAIRS   * v6 = nullptr;
-
-    REMOVE_UNUSED_VARIABLE(v1);
-    REMOVE_UNUSED_VARIABLE(v2);
-    REMOVE_UNUSED_VARIABLE(v3);
-    REMOVE_UNUSED_VARIABLE(v4);
-    REMOVE_UNUSED_VARIABLE(v5);
-    REMOVE_UNUSED_VARIABLE(v6);
+    c.print("Press any key to continue...\n");
+    c.getChar(); // WAIT!
 }
 
 void testCurses()
 {
+    using Window = ::libtbag::curses::Context::Window;
+    using Color  = ::libtbag::curses::Context::ColorTable;
+
+    curses::Context c;
+
     int const BUFFER_SIZE = 256;
     char STR_BUFFER[BUFFER_SIZE] = { 0, };
-    LIBTBAG_CURSES_CLEAR();
-    LIBTBAG_CURSES_INITSCR();
-    LIBTBAG_CURSES_MVPRINTW(0, 0, "Col: %d, Lines: %d\n", COLS, LINES);
-    LIBTBAG_CURSES_REFRESH();
-
-    LIBTBAG_CURSES_START_COLOR();
-    //LIBTBAG_CURSES_INIT_COLOR(c, r, g, b);
-    LIBTBAG_CURSES_INIT_PAIR(1, LIBTBAG_CURSES_COLOR_RED, LIBTBAG_CURSES_COLOR_BLUE);
-    LIBTBAG_CURSES_INIT_PAIR(2, LIBTBAG_CURSES_COLOR_GREEN, LIBTBAG_CURSES_COLOR_YELLOW);
-
-    int x1 = LIBTBAG_CURSES_GETX(LIBTBAG_CURSES_STDSCR);
-    int y1 = LIBTBAG_CURSES_GETY(LIBTBAG_CURSES_STDSCR);
-    int x2 = LIBTBAG_CURSES_GETBEGX(LIBTBAG_CURSES_STDSCR);
-    int y2 = LIBTBAG_CURSES_GETBEGY(LIBTBAG_CURSES_STDSCR);
-    int x3 = LIBTBAG_CURSES_GETMAXX(LIBTBAG_CURSES_STDSCR);
-    int y3 = LIBTBAG_CURSES_GETMAXY(LIBTBAG_CURSES_STDSCR);
-    int x4 = LIBTBAG_CURSES_GETPARX(LIBTBAG_CURSES_STDSCR);
-    int y4 = LIBTBAG_CURSES_GETPARY(LIBTBAG_CURSES_STDSCR);
-
-    LIBTBAG_CURSES_ATTRON(LIBTBAG_CURSES_COLOR_PAIR(1));
-    LIBTBAG_CURSES_PRINTW("Cursor: %d, %d\n", x1, y1);
-    LIBTBAG_CURSES_PRINTW("Beginning: %d, %d\n", x2, y2);
-    LIBTBAG_CURSES_PRINTW("Max: %d, %d\n", x3, y3);
-    LIBTBAG_CURSES_PRINTW("Parent: %d, %d\n", x4, y4);
-    LIBTBAG_CURSES_ATTROFF(LIBTBAG_CURSES_COLOR_PAIR(1));
-
-    LIBTBAG_CURSES_PRINTW("String test 01: ");
-    LIBTBAG_CURSES_GETSTR(STR_BUFFER);
-    LIBTBAG_CURSES_PRINTW("String test 02: ");
-    LIBTBAG_CURSES_WGETNSTR(LIBTBAG_CURSES_STDSCR, STR_BUFFER, BUFFER_SIZE);
-
-    LIBTBAG_CURSES_WINDOW * win = LIBTBAG_CURSES_NEWWIN(10, 32, 10, 0);
-    LIBTBAG_CURSES_BOX(win, '*', '+');
-    LIBTBAG_CURSES_WREFRESH(win);
-
-    LIBTBAG_CURSES_PRINTW("Press any key to continue...\n");
-    LIBTBAG_CURSES_GETCH(); // WAIT!
-
-    LIBTBAG_CURSES_ATTRON(LIBTBAG_CURSES_COLOR_PAIR(2));
-    LIBTBAG_CURSES_GETYX(win, y1, x1);
-    LIBTBAG_CURSES_GETBEGYX(win, y2, x2);
-    LIBTBAG_CURSES_GETMAXYX(win, y3, x3);
-    LIBTBAG_CURSES_GETPARYX(win, y4, x4);
-    LIBTBAG_CURSES_MVWPRINTW(win, 2, 2, "Cursor: %d, %d\n", x1, y1);
-    LIBTBAG_CURSES_MVWPRINTW(win, 3, 2, "Beginning: %d, %d\n", x2, y2);
-    LIBTBAG_CURSES_MVWPRINTW(win, 4, 2, "Max: %d, %d\n", x3, y3);
-    LIBTBAG_CURSES_MVWPRINTW(win, 5, 2, "Parent: %d, %d\n", x4, y4);
-    LIBTBAG_CURSES_ATTROFF(LIBTBAG_CURSES_COLOR_PAIR(2));
-
-    LIBTBAG_CURSES_WBORDER(win, '1', '2', '3', '4', '5', '6', '7', '8');
-    LIBTBAG_CURSES_WREFRESH(win);
-    LIBTBAG_CURSES_DELWIN(win);
-
-    // ONLY COVERAGE.
-    LIBTBAG_CURSES_RAW();
-    LIBTBAG_CURSES_CBREAK();
-    LIBTBAG_CURSES_ECHO();
-    LIBTBAG_CURSES_NOECHO();
-    LIBTBAG_CURSES_KEYPAD(LIBTBAG_CURSES_STDSCR, TRUE);
-    LIBTBAG_CURSES_CURS_SET(0);
-
-    LIBTBAG_CURSES_PRINTW("Press any key to continue...\n");
-    LIBTBAG_CURSES_GETCH(); // WAIT!
-    LIBTBAG_CURSES_ENDWIN();
-}
-
-void testContext()
-{
-    curses::Context c;
 
     c.clear();
     c.initialize();
-    c.startColorMode();
-    c.initPair(3, LIBTBAG_CURSES_COLOR_CYAN, LIBTBAG_CURSES_COLOR_MAGENTA);
-    c.onAttribute(LIBTBAG_CURSES_COLOR_PAIR(3));
-    c.print("Col: %d, Lines: %d\n", LIBTBAG_CURSES_COLS, LIBTBAG_CURSES_LINES);
-    c.offAttribute(LIBTBAG_CURSES_COLOR_PAIR(3));
+    Window * main_window = c.getStandardWindow();
+
+    int cols  = c.getTerminalWidth();
+    int lines = c.getTerminalHeight();
+    c.movePrint(0, 0, "Width: %d, Height: %d\n", cols, lines);
     c.refresh();
 
-    c.print("Press any key to continue...\n");
-    c.getChar(); // WAIT!
+    c.startColorMode();
+    c.initPair(1, Color::CYAN , Color::MAGENTA);
+    c.initPair(2, Color::GREEN, Color::BLUE);
+
+    int x1 = c.getCursorX(main_window);
+    int y1 = c.getCursorY(main_window);
+    int x2 = c.getBeginningX(main_window);
+    int y2 = c.getBeginningY(main_window);
+    int x3 = c.getMaxX(main_window);
+    int y3 = c.getMaxY(main_window);
+    int x4 = c.getParentRelativeX(main_window);
+    int y4 = c.getParentRelativeY(main_window);
+
+    c.onAttributeWithColorPair(c.getColorPair(1));
+    c.print("Cursor: %d, %d\n", x1, y1);
+    c.print("Beginning: %d, %d\n", x2, y2);
+    c.print("Max: %d, %d\n", x3, y3);
+    c.print("Parent: %d, %d\n", x4, y4);
+    c.offAttributeWithColorPair(c.getColorPair(1));
+
+    c.print("String input: ");
+    c.getString(STR_BUFFER, BUFFER_SIZE);
+
+    Window * win = c.createWindow(0, 10, 32, 10);
+    c.setBox(win, '*', '+');
+    c.refresh(win);
+
+    pressAnyKey(c);
+
+    c.onAttributeWithColorPair(c.getColorPair(2));
+    x1 = c.getCursorX(win);
+    y1 = c.getCursorY(win);
+    x2 = c.getBeginningX(win);
+    y2 = c.getBeginningY(win);
+    x3 = c.getMaxX(win);
+    y3 = c.getMaxY(win);
+    x4 = c.getParentRelativeX(win);
+    y4 = c.getParentRelativeY(win);
+    c.movePrintWithWindow(win, 2, 2, "Cursor: %d, %d\n", x1, y1);
+    c.movePrintWithWindow(win, 2, 3, "Beginning: %d, %d\n", x2, y2);
+    c.movePrintWithWindow(win, 2, 4, "Max: %d, %d\n", x3, y3);
+    c.movePrintWithWindow(win, 2, 5, "Parent: %d, %d\n", x4, y4);
+    c.offAttributeWithColorPair(c.getColorPair(2));
+
+    c.setBorder(win, '1', '2', '3', '4', '5', '6', '7', '8');
+    c.refresh(win);
+    c.destroyWindow(win);
+
+    // ONLY COVERAGE.
+    c.setRaw();
+    c.setCbreak();
+    c.setEcho();
+    c.setNoEcho();
+    c.setKeypad(main_window, true);
+    c.setCursor(0);
+
+    pressAnyKey(c);
     c.release();
 }
 
 int main(int argc, char ** argv)
 {
-    onlyTypeCoverage();
     testCurses();
-    testContext();
-
     return 0;
 }
 
--- libtbag/curses/Context.hpp
+++ libtbag/curses/Context.hpp
@@ -18,162 +18,7 @@
 
 #include <exception>
 
-#if defined(USE_NCURSES)
 #include <ncurses/ncurses.h>
-#endif
-
-// NCurses Structures.
-#if defined(USE_NCURSES)
-# define LIBTBAG_CURSES_SCREEN  SCREEN
-# define LIBTBAG_CURSES_WINDOW  WINDOW
-# define LIBTBAG_CURSES_CHAR    chtype
-# define LIBTBAG_CURSES_ATTR    attr_t
-# define LIBTBAG_CURSES_COLOR   NCURSES_COLOR_T
-# define LIBTBAG_CURSES_PAIRS   NCURSES_PAIRS_T
-#else // defined(USE_NCURSES)
-struct DUMMY_SCREEN { __EMPTY_BLOCK__ };
-struct DUMMY_WINDOW { __EMPTY_BLOCK__ };
-# define LIBTBAG_CURSES_SCREEN  DUMMY_SCREEN
-# define LIBTBAG_CURSES_WINDOW  DUMMY_WINDOW
-# define LIBTBAG_CURSES_CHAR    int
-# define LIBTBAG_CURSES_ATTR    int
-# define LIBTBAG_CURSES_COLOR   short
-# define LIBTBAG_CURSES_PAIRS   short
-#endif // defined(USE_NCURSES)
-
-// NCurses variables.
-#if defined(USE_NCURSES)
-# define LIBTBAG_CURSES_STDSCR  stdscr
-# define LIBTBAG_CURSES_COLS    COLS
-# define LIBTBAG_CURSES_LINES   LINES
-#else // defined(USE_NCURSES)
-# define LIBTBAG_CURSES_STDSCR  (-1)
-# define LIBTBAG_CURSES_COLS    (-1)
-# define LIBTBAG_CURSES_LINES   (-1)
-#endif // defined(USE_NCURSES)
-
-// NCurses stdscr methods.
-#if defined(USE_NCURSES)
-# define LIBTBAG_CURSES_INITSCR()                ::initscr() // RETURN WINDOW*
-# define LIBTBAG_CURSES_ENDWIN()                 ::endwin()
-# define LIBTBAG_CURSES_REFRESH()                ::refresh()
-# define LIBTBAG_CURSES_PRINTW(...)              ::printw(__VA_ARGS__)
-# define LIBTBAG_CURSES_MVPRINTW(y, x, ...)      ::mvprintw(y, x, __VA_ARGS__)
-# define LIBTBAG_CURSES_GETCH()                  ::getch()
-# define LIBTBAG_CURSES_GETSTR(str)              ::getstr(str);
-# define LIBTBAG_CURSES_WGETNSTR(win, str, size) ::wgetnstr(win, str, size);
-# define LIBTBAG_CURSES_CLEAR()                  ::clear()
-#else // defined(USE_NCURSES)
-# define LIBTBAG_CURSES_INITSCR()                (nullptr)
-# define LIBTBAG_CURSES_ENDWIN()                 (-1)
-# define LIBTBAG_CURSES_REFRESH()                (-1)
-# define LIBTBAG_CURSES_PRINTW(...)              (-1)
-# define LIBTBAG_CURSES_MVPRINTW(y, x, ...)      (-1)
-# define LIBTBAG_CURSES_GETCH()                  (-1)
-# define LIBTBAG_CURSES_GETSTR(str)              (-1)
-# define LIBTBAG_CURSES_WGETNSTR(win, str, size) (-1)
-# define LIBTBAG_CURSES_CLEAR()                  (-1)
-#endif // defined(USE_NCURSES)
-
-// NCurses window methods.
-#if defined(USE_NCURSES)
-# define LIBTBAG_CURSES_NEWWIN(h, w, y, x)                            ::newwin(h, w, y, x) // RETURN WINDOW*
-# define LIBTBAG_CURSES_DELWIN(win)                                   ::delwin(win)
-# define LIBTBAG_CURSES_WREFRESH(win)                                 ::wrefresh(win)
-# define LIBTBAG_CURSES_BOX(win, vertical, horizontal)                ::box(win, vertical, horizontal)
-# define LIBTBAG_CURSES_WBORDER(win, ls, rs, ts, bs, tl, tr, bl, br)  ::wborder(win, ls, rs, ts, bs, tl, tr, bl, br)
-# define LIBTBAG_CURSES_MVWPRINTW(win, y, x, ...)                     ::mvwprintw(win, y, x, __VA_ARGS__)
-#else // defined(USE_NCURSES)
-# define LIBTBAG_CURSES_NEWWIN(h, w, y, x)                            (nullptr)
-# define LIBTBAG_CURSES_DELWIN(win)                                   (-1)
-# define LIBTBAG_CURSES_WREFRESH(win)                                 (-1)
-# define LIBTBAG_CURSES_BOX(win, vertical, horizontal)                (-1)
-# define LIBTBAG_CURSES_WBORDER(win, ls, rs, ts, bs, tl, tr, bl, br)  (-1)
-# define LIBTBAG_CURSES_MVWPRINTW(win, y, x, ...)                     (-1)
-#endif // defined(USE_NCURSES)
-
-// NCurses color methods.
-#if defined(USE_NCURSES)
-# define LIBTBAG_CURSES_START_COLOR()           ::start_color()
-# define LIBTBAG_CURSES_INIT_COLOR(c, r, g, b)  ::init_color(c, r, g, b)
-# define LIBTBAG_CURSES_INIT_PAIR(pair, f, b)   ::init_pair(pair, f, b)
-#else // defined(USE_NCURSES)
-# define LIBTBAG_CURSES_START_COLOR()           (-1)
-# define LIBTBAG_CURSES_INIT_COLOR(c, r, g, b)  (-1)
-# define LIBTBAG_CURSES_INIT_PAIR(pair, f, b)   (-1)
-#endif // defined(USE_NCURSES)
-
-// NCurses terminal methods.
-#if defined(USE_NCURSES)
-# define LIBTBAG_CURSES_RAW()           ::raw()   // Line buffering disable.
-# define LIBTBAG_CURSES_CBREAK()        ::cbreak()
-# define LIBTBAG_CURSES_ECHO()          ::echo()
-# define LIBTBAG_CURSES_NOECHO()        ::noecho()
-# define LIBTBAG_CURSES_ATTRON(v)       ::attron(v)
-# define LIBTBAG_CURSES_ATTROFF(v)      ::attroff(v)
-# define LIBTBAG_CURSES_KEYPAD(win, b)  ::keypad(win, b)
-# define LIBTBAG_CURSES_CURS_SET(flag)  ::curs_set(flag)
-#else // defined(USE_NCURSES)
-# define LIBTBAG_CURSES_RAW()           (-1)
-# define LIBTBAG_CURSES_CBREAK()        (-1)
-# define LIBTBAG_CURSES_ECHO()          (-1)
-# define LIBTBAG_CURSES_NOECHO()        (-1)
-# define LIBTBAG_CURSES_ATTRON(v)       (-1)
-# define LIBTBAG_CURSES_ATTROFF(v)      (-1)
-# define LIBTBAG_CURSES_KEYPAD(win, b)  (-1)
-# define LIBTBAG_CURSES_CURS_SET(flag)  (-1)
-#endif // defined(USE_NCURSES)
-
-// NCurses query methods.
-#if defined(USE_NCURSES)
-# define LIBTBAG_CURSES_GETX(win)           getcurx(win)
-# define LIBTBAG_CURSES_GETY(win)           getcury(win)
-# define LIBTBAG_CURSES_GETBEGX(win)        getbegx(win)
-# define LIBTBAG_CURSES_GETBEGY(win)        getbegy(win)
-# define LIBTBAG_CURSES_GETMAXX(win)        getmaxx(win)
-# define LIBTBAG_CURSES_GETMAXY(win)        getmaxy(win)
-# define LIBTBAG_CURSES_GETPARX(win)        getparx(win)
-# define LIBTBAG_CURSES_GETPARY(win)        getpary(win)
-# define LIBTBAG_CURSES_GETYX(win, y, x)    getyx   (win, y, x)
-# define LIBTBAG_CURSES_GETBEGYX(win, y, x) getbegyx(win, y, x)
-# define LIBTBAG_CURSES_GETMAXYX(win, y, x) getmaxyx(win, y, x)
-# define LIBTBAG_CURSES_GETPARYX(win, y, x) getparyx(win, y, x)
-#else // defined(USE_NCURSES)
-# define LIBTBAG_CURSES_GETX(win)           (-1)
-# define LIBTBAG_CURSES_GETY(win)           (-1)
-# define LIBTBAG_CURSES_GETBEGX(win)        (-1)
-# define LIBTBAG_CURSES_GETBEGY(win)        (-1)
-# define LIBTBAG_CURSES_GETMAXX(win)        (-1)
-# define LIBTBAG_CURSES_GETMAXY(win)        (-1)
-# define LIBTBAG_CURSES_GETPARX(win)        (-1)
-# define LIBTBAG_CURSES_GETPARY(win)        (-1)
-# define LIBTBAG_CURSES_GETYX(win, y, x)    (y = -1, x = -1)
-# define LIBTBAG_CURSES_GETBEGYX(win, y, x) (y = -1, x = -1)
-# define LIBTBAG_CURSES_GETMAXYX(win, y, x) (y = -1, x = -1)
-# define LIBTBAG_CURSES_GETPARYX(win, y, x) (y = -1, x = -1)
-#endif // defined(USE_NCURSES)
-
-#if defined(USE_NCURSES)
-# define LIBTBAG_CURSES_COLOR_PAIR(n)  COLOR_PAIR(n)
-# define LIBTBAG_CURSES_COLOR_BLACK    COLOR_BLACK
-# define LIBTBAG_CURSES_COLOR_RED      COLOR_RED
-# define LIBTBAG_CURSES_COLOR_GREEN    COLOR_GREEN
-# define LIBTBAG_CURSES_COLOR_YELLOW   COLOR_YELLOW
-# define LIBTBAG_CURSES_COLOR_BLUE     COLOR_BLUE
-# define LIBTBAG_CURSES_COLOR_MAGENTA  COLOR_MAGENTA
-# define LIBTBAG_CURSES_COLOR_CYAN     COLOR_CYAN
-# define LIBTBAG_CURSES_COLOR_WHITE    COLOR_WHITE
-#else // defined(USE_NCURSES)
-# define LIBTBAG_CURSES_COLOR_PAIR(n)   (-1)
-# define LIBTBAG_CURSES_COLOR_BLACK     0
-# define LIBTBAG_CURSES_COLOR_RED       1
-# define LIBTBAG_CURSES_COLOR_GREEN     2
-# define LIBTBAG_CURSES_COLOR_YELLOW    3
-# define LIBTBAG_CURSES_COLOR_BLUE      4
-# define LIBTBAG_CURSES_COLOR_MAGENTA   5
-# define LIBTBAG_CURSES_COLOR_CYAN      6
-# define LIBTBAG_CURSES_COLOR_WHITE     7
-#endif // defined(USE_NCURSES)
 
 // -------------------
 NAMESPACE_LIBTBAG_OPEN
@@ -199,12 +44,25 @@ constexpr bool isEnableNcurses() noexcept
 class Context : public Noncopyable
 {
 public:
-    using Screen        = LIBTBAG_CURSES_SCREEN;
-    using Window        = LIBTBAG_CURSES_WINDOW;
-    using CharType      = LIBTBAG_CURSES_CHAR;
-    using ColorType     = LIBTBAG_CURSES_COLOR;
-    using PairsType     = LIBTBAG_CURSES_PAIRS;
-    using AttributeType = LIBTBAG_CURSES_ATTR;
+    using Screen        = SCREEN;
+    using Window        = WINDOW;
+    using CharType      = chtype;
+    using AttributeType = attr_t;
+    using ColorType     = NCURSES_COLOR_T;
+    using PairType      = NCURSES_PAIRS_T;
+
+public:
+    enum class ColorTable : ColorType
+    {
+        BLACK   = COLOR_BLACK   ,
+        RED     = COLOR_RED     ,
+        GREEN   = COLOR_GREEN   ,
+        YELLOW  = COLOR_YELLOW  ,
+        BLUE    = COLOR_BLUE    ,
+        MAGENTA = COLOR_MAGENTA ,
+        CYAN    = COLOR_CYAN    ,
+        WHITE   = COLOR_WHITE   ,
+    };
 
 public:
     /**
@@ -235,163 +93,204 @@ public:
 
 // NCurses stdscr methods.
 public:
+    inline Window * getStandardWindow() {
+        return stdscr;
+    }
+
     Window * initialize() {
-        return LIBTBAG_CURSES_INITSCR();
+        return ::initscr();
     }
 
     int release() {
-        return LIBTBAG_CURSES_ENDWIN();
+        return ::endwin();
     }
 
     template <typename ... Args>
     int print(std::string const & format, Args ... args) {
-        return LIBTBAG_CURSES_PRINTW(format.c_str(), args...);
+        return ::printw(format.c_str(), args...);
+    }
+
+    template <typename ... Args>
+    int movePrint(int x, int y, std::string const & format, Args ... args) {
+        return ::mvprintw(y, x, format.c_str(), args...);
     }
 
     int getChar() {
-        return LIBTBAG_CURSES_GETCH();
+        return ::getch();
     }
 
     int getString(char * result, std::size_t buffer_size) {
-        return LIBTBAG_CURSES_WGETNSTR(stdscr, result, buffer_size);
+        return ::wgetnstr(getStandardWindow(), result, buffer_size);
     }
 
     int clear() {
-        return LIBTBAG_CURSES_CLEAR();
+        return ::clear();
     }
 
 // Window new/del methods.
 public:
     Window * createWindow(int x, int y, int width, int height) {
-        return LIBTBAG_CURSES_NEWWIN(height, width, y, x);
+        return ::newwin(height, width, y, x);
     }
 
     int destroyWindow(Window * window) {
-        return LIBTBAG_CURSES_DELWIN(window);
+        return ::delwin(window);
     }
 
 // Update methods.
 public:
     int refresh() {
-        return LIBTBAG_CURSES_REFRESH();
+        return ::refresh();
     }
 
     int refresh(Window * window) {
-        return LIBTBAG_CURSES_WREFRESH(window);
+        return ::wrefresh(window);
     }
 
 // Window style.
 public:
     int setBox(Window * window, CharType vertical, CharType horizontal) {
-        return LIBTBAG_CURSES_BOX(window, vertical, horizontal);
+        return ::box(window, vertical, horizontal);
+    }
+
+    int setBox(Window * window, char vertical, char horizontal) {
+        return setBox(window
+                    , static_cast<CharType>(vertical)
+                    , static_cast<CharType>(horizontal));
     }
 
     /**
-     * @param ls [in] left side.
-     * @param rs [in] right side.
-     * @param ts [in] top side.
-     * @param bs [in] bottom side.
-     * @param tl [in] top left-hand corner.
-     * @param tr [in] top right-hand corner.
-     * @param bl [in] bottom left-hand corner.
-     * @param br [in] bottom right-hand corner.
+     * @param window [in] Window pointer.
+     * @param     ls [in] left side.
+     * @param     rs [in] right side.
+     * @param     ts [in] top side.
+     * @param     bs [in] bottom side.
+     * @param     tl [in] top left-hand corner.
+     * @param     tr [in] top right-hand corner.
+     * @param     bl [in] bottom left-hand corner.
+     * @param     br [in] bottom right-hand corner.
      */
-    int setBorder(Window * window
-                       , CharType ls, CharType rs
-                       , CharType ts, CharType bs
-                       , CharType tl, CharType tr
-                       , CharType bl, CharType br) {
-        return LIBTBAG_CURSES_WBORDER(window, ls, rs, ts, bs, tl, tr, bl, br);
+    template <typename T>
+    int setBorder(Window * window, T ls, T rs
+                                 , T ts, T bs
+                                 , T tl, T tr
+                                 , T bl, T br) {
+        return ::wborder(window, static_cast<CharType>(ls), static_cast<CharType>(rs)
+                               , static_cast<CharType>(ts), static_cast<CharType>(bs)
+                               , static_cast<CharType>(tl), static_cast<CharType>(tr)
+                               , static_cast<CharType>(bl), static_cast<CharType>(br));
+    }
+
+public:
+    template <typename ... Args>
+    int movePrintWithWindow(Window * window, int x, int y, std::string const & format, Args ... args) {
+        return ::mvwprintw(window, y, x, format.c_str(), args...);
     }
 
 // Color methods.
 public:
     int startColorMode() {
-        return LIBTBAG_CURSES_START_COLOR();
+        return ::start_color();
     }
 
     int initColor(ColorType color, ColorType r, ColorType g, ColorType b) {
-        return LIBTBAG_CURSES_INIT_COLOR(color, r, g, b);
+        return ::init_color(color, r, g, b);
     }
 
-    int initPair(PairsType pair, ColorType foreground, ColorType background) {
-        return LIBTBAG_CURSES_INIT_PAIR(pair, foreground, background);
+    PairType getColorPair(int number) {
+        return COLOR_PAIR(number);
+    }
+
+    template <typename T>
+    int initPair(PairType pair, T foreground, T background) {
+        return ::init_pair(pair
+                         , static_cast<ColorType>(foreground)
+                         , static_cast<ColorType>(background));
     }
 
 // Terminal methods.
 public:
     int setRaw() {
-        return LIBTBAG_CURSES_RAW();
+        return ::raw();
     }
 
     int setCbreak() {
-        return LIBTBAG_CURSES_CBREAK();
+        return ::cbreak();
     }
 
     int setEcho() {
-        return LIBTBAG_CURSES_ECHO();
+        return ::echo();
     }
 
     int setNoEcho() {
-        return LIBTBAG_CURSES_NOECHO();
+        return ::noecho();
     }
 
 // Attribute methods.
 public:
     int onAttribute(AttributeType flags) {
-        return LIBTBAG_CURSES_ATTRON(flags);
+        return ::attron(flags);
     }
 
     int offAttribute(AttributeType flags) {
-        return LIBTBAG_CURSES_ATTROFF(flags);
+        return ::attroff(flags);
     }
 
 public:
-    int offAttribute(Window * window, bool enable_function_key) {
-        return LIBTBAG_CURSES_KEYPAD(window, enable_function_key);
+    int onAttributeWithColorPair(PairType pair) {
+        return ::attron(pair);
+    }
+
+    int offAttributeWithColorPair(PairType pair) {
+        return ::attron(pair);
+    }
+
+public:
+    int setKeypad(Window * window, bool enable_function_key) {
+        return ::keypad(window, (enable_function_key ? TRUE : FALSE));
     }
 
     int setCursor(int flag) {
-        return LIBTBAG_CURSES_CURS_SET(flag);
+        return ::curs_set(flag);
     }
 
 // Query methods.
 public:
     int getCursorX(Window * window) {
-        return LIBTBAG_CURSES_GETX(window);
+        return getcurx(window);
     };
     int getCursorY(Window * window) {
-        return LIBTBAG_CURSES_GETY(window);
+        return getcury(window);
     };
 
     int getBeginningX(Window * window) {
-        return LIBTBAG_CURSES_GETBEGX(window);
+        return getbegx(window);
     };
     int getBeginningY(Window * window) {
-        return LIBTBAG_CURSES_GETBEGY(window);
+        return getbegy(window);
     };
 
     int getMaxX(Window * window) {
-        return LIBTBAG_CURSES_GETMAXX(window);
+        return getmaxx(window);
     };
     int getMaxY(Window * window) {
-        return LIBTBAG_CURSES_GETMAXY(window);
+        return getmaxy(window);
     };
 
     int getParentRelativeX(Window * window) {
-        return LIBTBAG_CURSES_GETPARX(window);
+        return getparx(window);
     };
     int getParentRelativeY(Window * window) {
-        return LIBTBAG_CURSES_GETPARY(window);
+        return getpary(window);
     };
 
 public:
-    std::size_t getColSize() {
-        return static_cast<std::size_t>(LIBTBAG_CURSES_COLS);
+    inline int getTerminalWidth() {
+        return COLS;
     }
 
-    std::size_t getRowSize() {
-        return static_cast<std::size_t>(LIBTBAG_CURSES_LINES);
+    inline int getTerminalHeight() {
+        return LINES;
     }
 };
 
