4d9d1f31ef2923978ea4dd2415b84019baec6e70
zer0 <osom8979@gmail.com>
2016-04-29 17:58:57 +0900

    Create & Test SqliteContext::prepare() method.

--- libtbag/database/SqliteContext.hpp
+++ libtbag/database/SqliteContext.hpp
@@ -17,6 +17,9 @@
 #include <libtbag/Noncopyable.hpp>
 
 #include <string>
+#include <vector>
+#include <tuple>
+#include <functional>
 
 #include <sqlite3.h>
 
@@ -34,6 +37,66 @@ namespace database {
  */
 class SqliteContext : public Noncopyable
 {
+public:
+    /**
+     * Sqlite transaction class prototype.
+     *
+     * @author zer0
+     * @date   2016-04-29
+     */
+    class Transaction : public Noncopyable
+    {
+    private:
+        SqliteContext & _context;
+
+    public:
+        Transaction(SqliteContext & context) : _context(context) {
+            _context.beginTransaction();
+        }
+
+        ~Transaction() {
+            _context.endTransaction();
+        }
+    };
+
+    /**
+     * Sqlite prepare statement class prototype.
+     *
+     * @author zer0
+     * @date   2016-04-29
+     */
+    class Statement : public Noncopyable
+    {
+    private:
+        sqlite3_stmt * _statement;
+
+    public:
+        Statement(sqlite3_stmt * statement) : _statement(statement) {
+            __EMPTY_BLOCK__
+        }
+
+        ~Statement() {
+            __EMPTY_BLOCK__
+        }
+
+    public:
+        int getByte(int index) const {
+            return sqlite3_column_bytes(_statement, index);
+        }
+
+        int getDouble(int index) const {
+            return sqlite3_column_double(_statement, index);
+        }
+
+        int getInt(int index) const {
+            return sqlite3_column_int(_statement, index);
+        }
+
+        std::string getString(int index) const {
+            return std::string((char const *)sqlite3_column_text(_statement, index));
+        }
+    };
+
 private:
     sqlite3 * _db;
 
@@ -48,7 +111,7 @@ public:
 
 public:
     bool open(std::string const & path) {
-        if (sqlite3_open(path.c_str(), &_db) != 0) {
+        if (sqlite3_open(path.c_str(), &_db) != SQLITE_OK) {
             return false;
         }
         return true;
@@ -56,6 +119,63 @@ public:
 
     void close() {
         sqlite3_close(_db);
+    }
+
+    inline bool isOpen() const noexcept {
+        return (_db != nullptr ? true : false);
+    }
+
+public:
+    bool execute(std::string const & query) {
+        if (!isOpen()) {
+            return false;
+        }
+
+        char * error_message = nullptr;
+        if (sqlite3_exec(_db, query.c_str(), nullptr, nullptr, &error_message) != SQLITE_OK) {
+            return false;
+        }
+        return true;
+    }
+
+    template <typename Tuple>
+    std::vector<Tuple> prepare(std::string const & query
+                             , std::function<Tuple(Statement const &)> const & callback) {
+        if (!isOpen()) {
+            return std::vector<Tuple>();
+        }
+
+        std::vector<Tuple> result;
+
+        Transaction guard(*this);
+        sqlite3_stmt * stmt = nullptr;
+
+        if (sqlite3_prepare(_db, query.c_str(), query.size(), &stmt, nullptr) == SQLITE_OK) {
+            while (sqlite3_step(stmt) == SQLITE_ROW) {
+                result.push_back(callback(Statement(stmt)));
+            }
+        }
+
+        sqlite3_finalize(stmt);
+        return result;
+    }
+
+public:
+    bool beginTransaction() {
+        return this->execute("BEGIN;");
+    }
+
+    bool endTransaction() {
+        return this->execute("END;");
+    }
+
+public:
+    int getErrorCode() const noexcept {
+        return sqlite3_errcode(this->_db);
+    }
+
+    std::string getErrorMessage() const noexcept {
+        return std::string(sqlite3_errmsg(this->_db));
     }
 };
 
--- tester/database/SqliteContextTest.cpp
+++ tester/database/SqliteContextTest.cpp
@@ -15,7 +15,39 @@ TEST(SqliteContextTest, Default)
 {
     std::string const PATH = "__database_test_sqlite3.sqlite";
     SqliteContext db;
+
+    std::string const CREATE_TABLE =
+            "CREATE TABLE IF NOT EXISTS test (id INT PRIMARY KEY, name CHAR(20));";
+    std::string const INSERT_ITEM1 =
+            "INSERT INTO test(id, name) VALUES(1, 'temp_name1');";
+    std::string const INSERT_ITEM2 =
+            "INSERT INTO test(id, name) VALUES(2, 'temp_name2');";
+    std::string const SELECT_ITEM =
+            "SELECT * FROM test;";
+    std::string const DELETE_ITEM =
+            "DELETE FROM test WHERE id > 0;";
+
     ASSERT_TRUE(db.open(PATH));
+    ASSERT_EQ(db.getErrorCode(), 0);
+    ASSERT_GT(db.getErrorMessage().size(), 0);
+
+    ASSERT_TRUE(db.execute(CREATE_TABLE));
+    ASSERT_TRUE(db.execute(INSERT_ITEM1));
+    ASSERT_TRUE(db.execute(INSERT_ITEM2));
+
+    using RowType = std::tuple<int, std::string>;
+    auto callback = [](SqliteContext::Statement const & statement) -> RowType {
+        return RowType(statement.getInt(0), statement.getString(1));
+    };
+    auto result = db.prepare<RowType>(SELECT_ITEM, callback);
+
+    ASSERT_EQ(result.size(), 2);
+    ASSERT_EQ(std::get<0>(result[0]), 1);
+    ASSERT_EQ(std::get<0>(result[1]), 2);
+    ASSERT_EQ(std::get<1>(result[0]), std::string("temp_name1"));
+    ASSERT_EQ(std::get<1>(result[1]), std::string("temp_name2"));
+    ASSERT_TRUE(db.execute(DELETE_ITEM));
+
     db.close();
 }
 
