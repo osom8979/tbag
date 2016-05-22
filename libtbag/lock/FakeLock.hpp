f31e6cf516ba859c74e863db36337a5b0626f7f0
zer0 <osom8979@gmail.com>
2016-05-22 15:25:04 +0900

    Create lock::FakeLock class.

---
+++ libtbag/lock/FakeLock.hpp
@@ -0,0 +1,58 @@
+/**
+ * @file   FakeLock.hpp
+ * @brief  FakeLock class prototype.
+ * @author zer0
+ * @date   2016-05-22
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_LOCK_FAKELOCK_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_LOCK_FAKELOCK_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+#include <libtbag/Noncopyable.hpp>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace lock {
+
+/**
+ * FakeLock class prototype.
+ *
+ * @author zer0
+ * @date   2016-05-22
+ */
+class FakeLock : public Noncopyable
+{
+public:
+    FakeLock() = default;
+    ~FakeLock() = default;
+
+public:
+    void lock() noexcept {
+        __EMPTY_BLOCK__
+    }
+
+    bool try_lock() noexcept {
+        return false;
+    }
+
+    void unlock() noexcept {
+        __EMPTY_BLOCK__
+    }
+};
+
+} // namespace lock
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_LOCK_FAKELOCK_HPP__
+
---
+++ tester/lock/FakeLockTest.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file   FakeLockTest.cpp
+ * @brief  FakeLock class tester.
+ * @author zer0
+ * @date   2016-05-22
+ */
+
+#include <gtest/gtest.h>
+#include <libtbag/lock/FakeLock.hpp>
+
+#include <mutex>
+
+using namespace libtbag;
+using namespace libtbag::lock;
+
+TEST(FakeLockTest, Default)
+{
+    FakeLock lock;
+    lock.lock();
+    lock.unlock();
+    ASSERT_FALSE(lock.try_lock());
+}
+
+TEST(FakeLockTest, lock_guard)
+{
+    FakeLock lock;
+    std::lock_guard<FakeLock> guard(lock);
+}
+
