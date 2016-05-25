082abeea2faa6e1fe75a8354a9c622f97370e9b7
zer0 <osom8979@gmail.com>
2016-05-25 16:28:27 +0900

    Create TbagDebug cmake file.

---
+++ cmake/modules/TbagDebug.cmake
@@ -0,0 +1,30 @@
+#/// @file   TbagDebug.cmake
+#/// @brief  Debugging helper.
+#/// @author zer0
+#/// @date   2016-05-25
+
+#/// Print debug message.
+function (tbag_debug_message)
+    message (STATUS "@ " ${ARGN})
+endfunction ()
+
+#/// Print debug message with list.
+function (tbag_debug_message_list)
+    foreach (__list_cursor ${ARGN})
+        tbag_debug_message (${__list_cursor})
+    endforeach ()
+endfunction ()
+
+#/// LOG function.
+#///
+#/// @param __tag [in] tag name.
+#///
+#/// @remarks
+#///  Check the global variables:
+#///  - TBAG_DEBUG_TAG: Regex conditions of print.
+function (tbag_debug __tag)
+    if (TBAG_DEBUG_TAG AND ("${__tag}" MATCHES "${TBAG_DEBUG_TAG}"))
+        tbag_debug_message ("[${__tag}] ${ARGN}")
+    endif ()
+endfunction ()
+
