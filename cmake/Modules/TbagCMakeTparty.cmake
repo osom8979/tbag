cc308368a0913f3b2ae345ec9e2c4ec6f78cdd36
zer0 <osom8979@gmail.com>
2016-05-27 10:27:03 +0900

    Fix the THIRD_PREFIX variable error.

--- cmake/Modules/TbagCMakeTparty.cmake
+++ cmake/Modules/TbagCMakeTparty.cmake
@@ -5,34 +5,29 @@
 
 include (TbagFilesystem) # For the PATH_SEPARATOR variable.
 
+#/// Initialize third-party library variables.
 macro (tbag_tparty_init)
-    # 3rd-party library prefix.
-    set (THIRD_HOME   "$ENV{TPARTY_HOME}" CACHE STRING "3rd-party library prefix")
+    # Third-party library directory.
+    set (THIRD_PREFIX "$ENV{TPARTY_HOME}/local" CACHE STRING "3rd-party library prefix")
 
-    if (IS_DIRECTORY ${THIRD_HOME})
-        set (THIRD_PREFIX "${TPARTY_HOME}/local")
+    set (THIRD_BIN "${THIRD_PREFIX}/bin")
+    set (THIRD_INC "${THIRD_PREFIX}/include")
+    set (THIRD_LIB "${THIRD_PREFIX}/lib")
 
-        # Third-party libraries.
-        set (THIRD_BIN "${THIRD_PREFIX}/bin")
-        set (THIRD_INC "${THIRD_PREFIX}/include")
-        set (THIRD_LIB "${THIRD_PREFIX}/lib")
-
-        if (IS_DIRECTORY ${THIRD_BIN})
-            set (ENV{PATH} "${THIRD_BIN}${PATH_SEPARATOR}$ENV{PATH}")
-        endif ()
-
-        if (IS_DIRECTORY ${THIRD_INC})
-            set (ENV{CPATH} "${THIRD_INC}${PATH_SEPARATOR}$ENV{CPATH}")
-            include_directories (${THIRD_INC})
-        endif ()
-
-        if (IS_DIRECTORY ${THIRD_LIB})
-            set (ENV{LIBRARY_PATH} "${THIRD_LIB}${PATH_SEPARATOR}$ENV{LIBRARY_PATH}")
-            link_directories (${THIRD_LIB})
-        endif ()
-
-        list (INSERT CMAKE_PROGRAM_PATH 0 "${THIRD_BIN}")
-        list (INSERT CMAKE_LIBRARY_PATH 0 "${THIRD_LIB}")
+    if (IS_DIRECTORY ${THIRD_BIN})
+        set (ENV{PATH} "${THIRD_BIN}${PATH_SEPARATOR}$ENV{PATH}")
     endif ()
+    if (IS_DIRECTORY ${THIRD_INC})
+        set (ENV{CPATH} "${THIRD_INC}${PATH_SEPARATOR}$ENV{CPATH}")
+    endif ()
+    if (IS_DIRECTORY ${THIRD_LIB})
+        set (ENV{LIBRARY_PATH} "${THIRD_LIB}${PATH_SEPARATOR}$ENV{LIBRARY_PATH}")
+    endif ()
+
+    list (INSERT CMAKE_PROGRAM_PATH 0 "${THIRD_BIN}")
+    list (INSERT CMAKE_LIBRARY_PATH 0 "${THIRD_LIB}")
+
+    include_directories (${THIRD_INC})
+    link_directories (${THIRD_LIB})
 endmacro ()
 
