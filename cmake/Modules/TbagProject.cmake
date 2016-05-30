e133a3ac6b4bb833bc81d477d123c2d7993fa161
zer0 <osom8979@gmail.com>
2016-05-30 15:28:57 +0900

    Remove TbagProjectCommon cmake file.

--- cmake/Modules/TbagProject.cmake
+++ cmake/Modules/TbagProject.cmake
@@ -4,9 +4,51 @@
 #/// @date   2016-05-27
 
 include (TbagUtils)
-include (TbagProjectCommon)
-include (TbagFindObject)
 include (TbagModules)
+include (TbagFindObject)
+
+## -----------------------
+## Project type operators.
+## -----------------------
+
+set (TBAG_PROJECT_FILE_PREFIX  "project")
+set (TBAG_PROJECT_FILE_SUFFIX  "cmake")
+
+set (TBAG_PROJECT_LIBRARY_PREFIX     "lib")
+set (TBAG_PROJECT_EXECUTABLE_PREFIX  "")
+
+set (TBAG_PROJECT_FILE_NAME    "${TBAG_PROJECT_FILE_PREFIX}.${TBAG_PROJECT_FILE_SUFFIX}")
+set (TBAG_PROJECT_FILE_REGEX   "[^/]+/${TBAG_PROJECT_FILE_PREFIX}\\.${TBAG_PROJECT_FILE_SUFFIX}")
+
+#/// Obtain the project type.
+#///
+#/// @param __result           [out] value name of result type string.
+#/// @param __project_dir_name [in]  project directory name.
+function (tbag_project_common__get_type __result __project_dir_name)
+    set (${__result})
+
+    if (${__project_dir_name} MATCHES "^${TBAG_PROJECT_LIBRARY_PREFIX}.+")
+        set (${__result} "${TBAG_PROJECT_LIBRARY_PREFIX}" PARENT_SCOPE)
+    else ()
+        set (${__result} "${TBAG_PROJECT_EXECUTABLE_PREFIX}" PARENT_SCOPE)
+    endif ()
+endfunction()
+
+#/// Obtain the project name.
+#///
+#/// @param __result           [out] value name of result type string.
+#/// @param __project_dir_name [in]  project directory name.
+function (tbag_project_common__get_name __result __project_dir_name)
+    set (${__result})
+    tbag_project_common__get_type (__project_type "${__project_dir_name}")
+
+    if ("${__project_type}" STREQUAL "${TBAG_PROJECT_LIBRARY_PREFIX}")
+        string (REPLACE "${TBAG_PROJECT_LIBRARY_PREFIX}" "" __temp_name "${__project_dir_name}")
+        set (${__result} "${__temp_name}" PARENT_SCOPE)
+    else ()
+        set (${__result} "${__project_dir_name}" PARENT_SCOPE)
+    endif ()
+endfunction ()
 
 ## --------------------------
 ## Private modules operators.
