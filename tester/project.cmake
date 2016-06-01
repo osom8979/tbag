bd98814d0a5a2135aca0a9b5156e3fcdccf57696
zer0 <osom8979@gmail.com>
2016-06-01 17:02:08 +0900

    Fix the compile error: include could not find load file TbagProjectSQlite3.

--- tester/project.cmake
+++ tester/project.cmake
@@ -6,7 +6,7 @@ tbag_modules__include_project (GTest)
 tbag_modules__include_project (UV)
 tbag_modules__include_project (TinyXML2)
 tbag_modules__include_project (Spdlog)
-tbag_modules__include_project (SQlite3)
+tbag_modules__include_project (SQLite3)
 
 #tbag_modules__include_project (Preview)
 
