6d38f1ec373b4a6196e04bc958c07cd0fc0a1d70
zer0 <osom8979@gmail.com>
2016-06-02 18:01:14 +0900

    Fix the error of 'americano_python target does not exist'.

--- cmake/TbagModule/TbagObjectProtobufPython.cmake
+++ cmake/TbagModule/TbagObjectProtobufPython.cmake
@@ -13,6 +13,6 @@ if (PROTOBUF_FOUND)
     string (MD5 __proto_srcs_hash "${__proto_srcs}")
     set (__protobuf_python_target_name "${TBAG_PROJECT_CONST_NAME}_protobuf_python_${__proto_srcs_hash}")
     add_custom_target ("${__protobuf_python_target_name}" ALL DEPENDS ${__proto_srcs})
-    list (APPEND TBAG_OBJECT_DEPENDENCIES ${TBAG_PROJECT_CONST_NAME}_python)
+    list (APPEND TBAG_OBJECT_DEPENDENCIES "${__protobuf_python_target_name}")
 endif ()
 
