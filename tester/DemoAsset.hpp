96a30792649e44affcfa8361b227de82fcd25c48
zer0 <osom8979@gmail.com>
2016-06-30 13:29:58 +0900

    Create & Test DemoAsset class.

---
+++ tester/DemoAsset.hpp
@@ -0,0 +1,55 @@
+/**
+ * @file   DemoAsset.hpp
+ * @brief  DemoAsset class prototype.
+ * @author zer0
+ * @date   2016-06-30
+ */
+
+#ifndef __INCLUDE_LIBTBAG__TESTER_DEMOASSET_HPP__
+#define __INCLUDE_LIBTBAG__TESTER_DEMOASSET_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+#include <libtbag/Asset.hpp>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+/**
+ * DemoAsset class prototype.
+ *
+ * @author zer0
+ * @date   2016-06-30
+ */
+class DemoAsset : public libtbag::Asset
+{
+public:
+    DemoAsset() {
+        this->insert_tester_dir();
+    }
+
+    ~DemoAsset() {
+        // EMPTY.
+    }
+
+public:
+    static Path getTesterAssetDir() {
+        return (Path(__FILE__).getParent() / "asset");
+    }
+
+public:
+    CREATE_ASSET_PATH(tester_dir, DemoAsset::getTesterAssetDir());
+    CREATE_ASSET_PATH_SUB(tester_dir, image, "image");
+};
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__TESTER_DEMOASSET_HPP__
+
---
+++ tester/DemoAssetTest.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file   DemoAssetTest.cpp
+ * @brief  DemoAsset class tester.
+ * @author zer0
+ * @date   2016-06-30
+ */
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include <tester/DemoAsset.hpp>
+
+using namespace libtbag;
+
+TEST(DemoAssetTest, Exists)
+{
+    DemoAsset asset;
+
+    std::cout << "Tester/Asset directory: " << asset.get_tester_dir() << std::endl;
+    std::cout << "Tester/Image directory: " << asset.get_tester_dir_image() << std::endl;
+
+    ASSERT_TRUE(asset.exists_tester_dir());
+    ASSERT_TRUE(asset.exists_tester_dir_image());
+
+    auto image_list = asset.scan_tester_dir_image();
+    ASSERT_EQ(image_list.size(), 1U);
+}
+
