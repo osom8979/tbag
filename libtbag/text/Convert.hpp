3463ccd710b06e2f201439958f07ddebdb9bb48a
zer0 <osom8979@gmail.com>
2016-07-03 20:46:59 +0900

    Implement libtbag::text::convertToUtf8() function.

--- libtbag/text/Convert.hpp
+++ libtbag/text/Convert.hpp
@@ -18,6 +18,7 @@
 
 #include <cassert>
 #include <string>
+#include <iostream>
 
 #include <unicode/ucnv.h>
 #include <unicode/udat.h>
@@ -101,7 +102,41 @@ std::string convertFromUtf8(std::string const & utf8, std::string const & to_cha
 
 std::string convertToUtf8(std::string const & from_string, std::string const & from_charset) throw (ConvertException)
 {
-    return "";
+    UErrorCode status = U_ZERO_ERROR;
+    UConverter * converter = ucnv_open(from_charset.c_str(), &status);
+
+    if (U_FAILURE(status)) {
+        throw ConvertException(u_errorName(status));
+    }
+
+    assert(converter != nullptr);
+    status = U_ZERO_ERROR;
+
+    int32_t result_size = ucnv_toUChars(converter, nullptr, 0, from_string.c_str(), from_string.size(), &status);
+
+    if (status != U_BUFFER_OVERFLOW_ERROR) {
+        throw ConvertException(u_errorName(status));
+    }
+
+    assert(result_size > 0);
+    status = U_ZERO_ERROR;
+
+    std::vector<UChar> unicode_buffer;
+    unicode_buffer.resize(result_size + 1);
+    unicode_buffer.at(result_size) = '\0';
+    result_size = ucnv_toUChars(converter, &unicode_buffer[0], unicode_buffer.size(), from_string.c_str(), from_string.size(), &status);
+
+    if (U_FAILURE(status)) {
+        throw ConvertException(u_errorName(status));
+    }
+
+    ucnv_close(converter);
+
+    icu::UnicodeString unicode(&unicode_buffer[0]);
+    std::string result;
+    unicode.toUTF8String(result);
+
+    return result;
 }
 
 } // namespace text
--- tester/text/ConvertTest.cpp
+++ tester/text/ConvertTest.cpp
@@ -21,5 +21,6 @@ TEST(ConvertTest, Default)
 
     ASSERT_EQ(convertFromUtf8(utf8_string, "cp949"), cp949_string);
     ASSERT_THROW(text::convertFromUtf8(utf8_string, "unknown"), ConvertException);
+    ASSERT_EQ(convertToUtf8(cp949_string, "cp949"), utf8_string);
 }
 
