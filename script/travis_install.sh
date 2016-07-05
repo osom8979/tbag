0e48cb1fba9225ecc5a3ea5e4ec9bd1a34d0dd26
zer0 <osom8979@gmail.com>
2016-07-05 09:40:40 +0900

    Update Travis apt packages.

--- script/travis_install.sh
+++ script/travis_install.sh
@@ -25,11 +25,6 @@ fi
 # Install.
 cd "$TPARTY_HOME"
 echo 'gtest.sh'     > cache
-echo 'tinyxml2.sh' >> cache
-echo 'spdlog.sh'   >> cache
-echo 'sqlite3.sh'  >> cache
-echo 'libuv.sh'    >> cache
-echo 'ncurses.sh'  >> cache
 echo 'lua.sh'      >> cache
 ./install.sh -y
 
