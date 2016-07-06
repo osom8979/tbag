f88d9071acb3afedf3f70dc763cda223664c2164
zer0 <osom8979@gmail.com>
2016-07-06 17:51:17 +0900

    Create text::convert namespace.

--- libtbag/text/Convert.hpp
+++ libtbag/text/Convert.hpp
@@ -14,21 +14,16 @@
 #endif
 
 #include <libtbag/config.h>
-#include <libtbag/Exception.hpp>
-
-#include <cassert>
+#include <exception>
 #include <string>
-#include <iostream>
-
-#include <unicode/ucnv.h>
-#include <unicode/udat.h>
-#include <unicode/udata.h>
+#include <vector>
 
 // -------------------
 NAMESPACE_LIBTBAG_OPEN
 // -------------------
 
-namespace text {
+namespace text    {
+namespace convert {
 
 /**
  * Convert exception class.
@@ -39,106 +34,42 @@ namespace text {
 class ConvertException : public std::exception
 {
 private:
-    std::string message;
+    std::string _message;
 
 public:
-    ConvertException(std::string const & msg) : message(msg) {
-        // EMPTY.
-    }
-
-    virtual ~ConvertException() {
+    ConvertException(std::string const & msg) : _message(msg)
+    {
         // EMPTY.
     }
 
 public:
-    virtual const char * what() const noexcept override {
-        return message.c_str();
+    virtual ~ConvertException()
+    {
+        // EMPTY.
+    }
+
+public:
+    virtual const char * what() const noexcept override
+    {
+        return _message.c_str();
     }
 };
 
-inline std::vector<std::string> getAvailableConverterNames()
-{
-    std::vector<std::string> result;
-    int32_t count = ucnv_countAvailable();
-    for (int i = 0; i < count; ++i) {
-        result.push_back(ucnv_getAvailableName(i));
-    }
-    return result;
-}
+std::vector<std::string> getAvailableConverterNames();
 
-inline std::string convertFromUtf8(std::string const & utf8, std::string const & to_charset) throw (ConvertException)
-{
-    UErrorCode status = U_ZERO_ERROR;
-    UConverter * converter = ucnv_open(to_charset.c_str(), &status);
+/**
+ * Convert from UTF-8 string.
+ */
+std::string convertFromUtf8(std::string const & utf8
+                          , std::string const & to_charset) throw (ConvertException);
 
-    if (U_FAILURE(status)) {
-        throw ConvertException(u_errorName(status));
-    }
+/**
+ * Convert to UTF-8 string.
+ */
+std::string convertToUtf8(std::string const & from_string
+                        , std::string const & from_charset) throw (ConvertException);
 
-    assert(converter != nullptr);
-    status = U_ZERO_ERROR;
-
-    icu::UnicodeString unicode = icu::UnicodeString::fromUTF8(icu::StringPiece(utf8.c_str()));
-    int32_t result_size = ucnv_fromUChars(converter, nullptr, 0, unicode.getBuffer(), unicode.length(), &status);
-
-    if (status != U_BUFFER_OVERFLOW_ERROR) {
-        throw ConvertException(u_errorName(status));
-    }
-
-    assert(result_size > 0);
-    status = U_ZERO_ERROR;
-
-    std::string result;
-    result.resize(result_size);
-    result_size = ucnv_fromUChars(converter, &result[0], result.size(), unicode.getBuffer(), unicode.length(), &status);
-
-    if (U_FAILURE(status)) {
-        throw ConvertException(u_errorName(status));
-    }
-
-    ucnv_close(converter);
-    return result;
-}
-
-inline std::string convertToUtf8(std::string const & from_string, std::string const & from_charset) throw (ConvertException)
-{
-    UErrorCode status = U_ZERO_ERROR;
-    UConverter * converter = ucnv_open(from_charset.c_str(), &status);
-
-    if (U_FAILURE(status)) {
-        throw ConvertException(u_errorName(status));
-    }
-
-    assert(converter != nullptr);
-    status = U_ZERO_ERROR;
-
-    int32_t result_size = ucnv_toUChars(converter, nullptr, 0, from_string.c_str(), from_string.size(), &status);
-
-    if (status != U_BUFFER_OVERFLOW_ERROR) {
-        throw ConvertException(u_errorName(status));
-    }
-
-    assert(result_size > 0);
-    status = U_ZERO_ERROR;
-
-    std::vector<UChar> unicode_buffer;
-    unicode_buffer.resize(result_size + 1);
-    unicode_buffer.at(result_size) = '\0';
-    result_size = ucnv_toUChars(converter, &unicode_buffer[0], unicode_buffer.size(), from_string.c_str(), from_string.size(), &status);
-
-    if (U_FAILURE(status)) {
-        throw ConvertException(u_errorName(status));
-    }
-
-    ucnv_close(converter);
-
-    icu::UnicodeString unicode(&unicode_buffer[0]);
-    std::string result;
-    unicode.toUTF8String(result);
-
-    return result;
-}
-
+} // namespace convert
 } // namespace text
 
 // --------------------
--- tester/text/ConvertTest.cpp
+++ tester/text/ConvertTest.cpp
@@ -8,11 +8,9 @@
 #include <gtest/gtest.h>
 #include <libtbag/text/Convert.hpp>
 
-#include <string>
-#include <vector>
-
 using namespace libtbag;
 using namespace libtbag::text;
+using namespace libtbag::text::convert;
 
 TEST(ConvertTest, Default)
 {
@@ -20,7 +18,7 @@ TEST(ConvertTest, Default)
     std::string cp949_string = "\xb0\xa1\xb3\xaa\xb4\xd9";
 
     ASSERT_EQ(convertFromUtf8(utf8_string, "cp949"), cp949_string);
-    ASSERT_THROW(text::convertFromUtf8(utf8_string, "unknown"), ConvertException);
+    ASSERT_THROW(convertFromUtf8(utf8_string, "unknown"), ConvertException);
     ASSERT_EQ(convertToUtf8(cp949_string, "cp949"), utf8_string);
 }
 
