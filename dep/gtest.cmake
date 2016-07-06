d91debb7860d151e6ad97ba22f1322cdc106468d
zer0 <osom8979@gmail.com>
2016-07-06 17:36:27 +0900

    Refactoring libtbag-gtest cmake script.

--- dep/gtest.cmake
+++ dep/gtest.cmake
@@ -6,22 +6,26 @@
 #/// @remarks
 #///  The following are set after configuration is done:
 #///   - ${TBAG_DEP_GTEST_NAME}
+#///   - ${TBAG_DEP_GTEST_LOG_PREFIX}
 #///   - ${TBAG_DEP_GTEST_INCLUDE_DIRS}
-#///   - ${TBAG_DEP_GTEST_LIBRARIES}
+#///   - ${TBAG_DEP_GTEST_ARCHIVES}
+#///   - ${TBAG_DEP_GTEST_DEPENDENCIES}
+#///   - ${TBAG_DEP_GTEST_LIBRARIES}: Archives + Dependencies
 
 set (TBAG_DEP_GTEST_NAME          "libtbag-gtest")
-set (TBAG_DEP_GTEST_INCLUDE_DIRS  "${PROJECT_SOURCE_DIR}/dep/gtest/include")
 set (TBAG_DEP_GTEST_LOG_PREFIX    "${PROJECT_BINARY_DIR}/CMakeFiles/${TBAG_DEP_GTEST_NAME}.dir/build.log")
-
-set (TBAG_DEP_GTEST_LIBRARIES_BUILD "${PROJECT_SOURCE_DIR}/dep/gtest/make/gtest.a"
-                                    "${PROJECT_SOURCE_DIR}/dep/gtest/make/gtest_main.a")
-set (TBAG_DEP_GTEST_LIBRARIES       ${TBAG_DEP_GTEST_LIBRARIES_BUILD} -lpthread)
+set (TBAG_DEP_GTEST_INCLUDE_DIRS  "${PROJECT_SOURCE_DIR}/dep/gtest/include")
+set (TBAG_DEP_GTEST_ARCHIVES      "${PROJECT_SOURCE_DIR}/dep/gtest/make/gtest.a"
+                                  "${PROJECT_SOURCE_DIR}/dep/gtest/make/gtest_main.a")
+set (TBAG_DEP_GTEST_DEPENDENCIES  -lpthread)
+set (TBAG_DEP_GTEST_LIBRARIES     ${TBAG_DEP_GTEST_ARCHIVES}
+                                  ${TBAG_DEP_GTEST_DEPENDENCIES})
 
 ## Google-gtest custom target.
-add_custom_target (${TBAG_DEP_GTEST_NAME} ALL DEPENDS ${TBAG_DEP_GTEST_LIBRARIES_BUILD})
+add_custom_target (${TBAG_DEP_GTEST_NAME} ALL DEPENDS ${TBAG_DEP_GTEST_ARCHIVES})
 
 add_custom_command (
-        OUTPUT ${TBAG_DEP_GTEST_LIBRARIES_BUILD}
+        OUTPUT ${TBAG_DEP_GTEST_ARCHIVES}
         COMMAND make CXXFLAGS=-fPIC gtest.a gtest_main.a > "${TBAG_DEP_GTEST_LOG_PREFIX}" 2>&1
         DEPENDS "${PROJECT_SOURCE_DIR}/dep/gtest/make/Makefile"
         WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/dep/gtest/make/")
--- tester/project.cmake
+++ tester/project.cmake
@@ -1,20 +1,10 @@
 ## Tbag CMake project setting.
-#
-# List of project modules:
-#  tbag_module__print_projects ()
-#
-# List of project module properties:
-#  tbag_modules__include_project (Preview)
-#
-# Default project setting:
-#  tbag_modules__include_project (Default)
 
 tbag_modules__include_project (Default)
 
 ## TODO Remove this code.
 list (APPEND TBAG_PROJECT_INCLUDE_DIRS  ${TBAG_DEP_UV_INCLUDE_DIRS})
 list (APPEND TBAG_PROJECT_INCLUDE_DIRS  ${TBAG_DEP_SPDLOG_INCLUDE_DIRS})
-list (APPEND TBAG_PROJECT_INCLUDE_DIRS  ${TBAG_DEP_SQLITE3_INCLUDE_DIRS})
 list (APPEND TBAG_PROJECT_INCLUDE_DIRS  ${TBAG_DEP_TINYXML2_INCLUDE_DIRS})
 list (APPEND TBAG_PROJECT_INCLUDE_DIRS  ${TBAG_DEP_ICU_INCLUDE_DIRS})
 list (APPEND TBAG_PROJECT_INCLUDE_DIRS  ${TBAG_DEP_LUA_INCLUDE_DIRS})
