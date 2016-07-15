da7e98e0b226f379892f777ef3db9649bcd2eb72
zer0 <osom8979@gmail.com>
2016-07-15 22:49:36 +0900

    Create some LOG macros.

--- libtbag/log/Log.hpp
+++ libtbag/log/Log.hpp
@@ -16,6 +16,7 @@
 #include <libtbag/config.h>
 #include <libtbag/Noncopyable.hpp>
 #include <libtbag/pattern/Singleton.hpp>
+#include <libtbag/log/details/Severity.hpp>
 
 #include <atomic>
 
@@ -29,6 +30,8 @@ namespace log {
 //constexpr char const * const DEFAULT_LOGGER_OF_FILE = "_F";
 //constexpr char const * const DEFAULT_LOGGER_FORMAT = "%L [%Y-%m-%dT%H:%M:%S.%e] [%t] %v";
 
+constexpr char const * const TBAG_DEFAULT_LOGGER_NAME = "__tbag_default_logger__";
+
 constexpr bool isAsynchronousLogging() noexcept
 {
     return true;
@@ -39,11 +42,43 @@ constexpr bool isMultithreadLogging() noexcept
     return false;
 }
 
-} // namespace log
+/**
+ * Log class.
+ */
+class Log
+{
+};
+
+}; // namespace log
 
 // --------------------
 NAMESPACE_LIBTBAG_CLOSE
 // --------------------
+
+#define tbEMERGENCY LOG_SEVERITY_EMERGENCY
+#define tbALERT     LOG_SEVERITY_ALERT
+#define tbCRITICAL  LOG_SEVERITY_CRITICAL
+#define tbERROR     LOG_SEVERITY_ERROR
+#define tbWARNING   LOG_SEVERITY_WARNING
+#define tbNOTICE    LOG_SEVERITY_NOTICE
+#define tbINFO      LOG_SEVERITY_INFORMATIONAL
+#define tbDEBUG     LOG_SEVERITY_DEBUG
+#define tbOFF       LOG_SEVERITY_OFF
+
+#if defined(DISABLE_TBAG_LOG)
+# define tbLOG(level, msg, ...)
+#else
+# define tbLOG(level, msg, ...)
+#endif
+
+#define tbLOGE(msg, ...)  tbLOG(tbEMERGENCY , msg, __VA_ARGS__)
+#define tbLOGA(msg, ...)  tbLOG(tbALERT     , msg, __VA_ARGS__)
+#define tbLOGC(msg, ...)  tbLOG(tbCRITICAL  , msg, __VA_ARGS__)
+#define tbLOGR(msg, ...)  tbLOG(tbERROR     , msg, __VA_ARGS__)
+#define tbLOGW(msg, ...)  tbLOG(tbWARNING   , msg, __VA_ARGS__)
+#define tbLOGN(msg, ...)  tbLOG(tbNOTICE    , msg, __VA_ARGS__)
+#define tbLOGI(msg, ...)  tbLOG(tbINFO      , msg, __VA_ARGS__)
+#define tbLOGD(msg, ...)  tbLOG(tbDEBUG     , msg, __VA_ARGS__)
 
 #endif // __INCLUDE_LIBTBAG__LIBTBAG_LOG_LOG_HPP__
 
