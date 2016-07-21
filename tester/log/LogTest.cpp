163a2a998aaa22a09a58f9566512f6c0af3216da
zer0 <osom8979@gmail.com>
2016-07-21 11:44:05 +0900

    Add some logging macros.

--- tester/log/LogTest.cpp
+++ tester/log/LogTest.cpp
@@ -18,19 +18,19 @@ TEST(LogTest, Console)
     log::createDefaultColorConsoleLogger();
     log::setDefaultLevel(log::LogLevel::LEVEL_NOTICE);
 
-    tDLogE("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "emergency", 0.1, 'T');
-    tDLogA("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "alert", 0.1, 'T');
-    tDLogC("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "critical", 0.1, 'T');
-    tDLogR("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "error", 0.1, 'T');
+    tDLogEF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "emergency", 0.1, 'T');
+    tDLogAF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "alert", 0.1, 'T');
+    tDLogCF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "critical", 0.1, 'T');
+    tDLogRF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "error", 0.1, 'T');
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    tDLogW("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "warning", 0.1, 'T');
-    tDLogN("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "notice", 0.1, 'T');
-    tDLogI("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "info", 0.1, 'T');
-    tDLogD("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "debug", 0.1, 'T');
+    tDLogWF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "warning", 0.1, 'T');
+    tDLogNF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "notice", 0.1, 'T');
+    tDLogIF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "info", 0.1, 'T');
+    tDLogDF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "debug", 0.1, 'T');
 
     log::setDefaultLevel(log::LogLevel::LEVEL_DEBUG);
-    tDLogI("CHANGE LOG LEVEL: {}", "info");
-    tDLogD("CHANGE LOG LEVEL: {}", "debug");
+    tDLogI("CHANGE LOG LEVEL: info.");
+    tDLogD("CHANGE LOG LEVEL: debug.");
 
     log::removeDefaultLogger();
 }
@@ -40,24 +40,14 @@ TEST(LogTest, File)
     log::createDefaultFileLogger("__log_test_file");
     log::setDefaultLevel(log::LogLevel::LEVEL_WARNING);
 
-    log::emergency("LogTest: emergency");
-    log::alert    ("LogTest: alert");
-    log::critical ("LogTest: critical");
-    log::error    ("LogTest: error");
-    log::warning  ("LogTest: warning");
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    log::notice   ("LogTest: notice");
-    log::info     ("LogTest: info");
-    log::debug    ("LogTest: debug");
-
-    tDLogE("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "TEST", 0.1, 'T');
-    tDLogA("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "TEST", 0.1, 'T');
-    tDLogC("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "TEST", 0.1, 'T');
-    tDLogR("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "TEST", 0.1, 'T');
-    tDLogW("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "TEST", 0.1, 'T');
-    tDLogN("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "TEST", 0.1, 'T');
-    tDLogI("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "TEST", 0.1, 'T');
-    tDLogD("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "TEST", 0.1, 'T');
+    tDLogE("LogTest: emergency");
+    tDLogA("LogTest: alert");
+    tDLogC("LogTest: critical");
+    tDLogR("LogTest: error");
+    tDLogW("LogTest: warning");
+    tDLogN("LogTest: notice");
+    tDLogI("LogTest: info");
+    tDLogD("LogTest: debug");
 
     log::removeDefaultLogger();
 }
