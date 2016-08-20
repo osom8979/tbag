0346559436d7f57edacb3a2a6f2eef8edb79332e
zer0 <osom8979@gmail.com>
2016-08-20 17:38:07 +0900

    Add extension CXX flags in TbagProjectPCH cmake file.

--- cmake/TbagModule/TbagProjectPCH.cmake
+++ cmake/TbagModule/TbagProjectPCH.cmake
@@ -19,7 +19,20 @@ else ()
     set (TbagProjectPCH_RESULT_PATH "${TbagProjectPCH_COPY_PATH}.pch")
 endif ()
 
-string (REPLACE " " ";" TbagProjectPCH_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+## Extension CXX flags.
+if ("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+    set (TbagProjectPCH_CXX_FLAGS_EX ${CMAKE_C_FLAGS_DEBUG})
+elseif ("${CMAKE_BUILD_TYPE}" STREQUAL "Release")
+    set (TbagProjectPCH_CXX_FLAGS_EX ${CMAKE_C_FLAGS_RELEASE})
+elseif ("${CMAKE_BUILD_TYPE}" STREQUAL "RelWithDebInfo")
+    set (TbagProjectPCH_CXX_FLAGS_EX ${CMAKE_C_FLAGS_RELWITHDEBINFO})
+elseif ("${CMAKE_BUILD_TYPE}" STREQUAL "MinSizeRel")
+    set (TbagProjectPCH_CXX_FLAGS_EX ${CMAKE_C_FLAGS_MINSIZEREL})
+endif()
+
+string (REPLACE " " ";" TbagProjectPCH_CXX_FLAGS "${CMAKE_CXX_FLAGS};${TbagProjectPCH_CXX_FLAGS_EX}")
+
+## Definitions.
 foreach (__define_cursor ${TBAG_PROJECT_DEFINITIONS})
     if ("${__define_cursor}" MATCHES "^-D")
         list (APPEND TbagProjectPCH_CXX_FLAGS "${__define_cursor}")
@@ -27,6 +40,8 @@ foreach (__define_cursor ${TBAG_PROJECT_DEFINITIONS})
         list (APPEND TbagProjectPCH_CXX_FLAGS "-D${__define_cursor}")
     endif ()
 endforeach ()
+
+## Include directories.
 foreach (__include_cursor ${TBAG_PROJECT_INCLUDE_DIRS})
     if ("${__include_cursor}" MATCHES "^-I")
         list (APPEND TbagProjectPCH_CXX_FLAGS "${__include_cursor}")
