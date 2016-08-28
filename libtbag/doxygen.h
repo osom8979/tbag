8507a0278fa0be39d08b212efe891bf69fa8726b
zer0 <osom8979@gmail.com>
2016-08-28 16:45:41 +0900

    Create doxygen main-page.

---
+++ libtbag/doxygen.h
@@ -0,0 +1,31 @@
+/**
+ * @file   doxygen.h
+ * @brief  Doxygen configuration header.
+ * @author zer0
+ * @date   2016-08-28
+ *
+ * @warning
+ *  Don't use the main project.
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_DOXYGEN_H__
+#define __INCLUDE_LIBTBAG__LIBTBAG_DOXYGEN_H__
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+/**
+ * @mainpage TEA-BAG
+ *
+ * @section ABSTRACT Abstract
+ *
+ *  THIRD-PARTY EXTENSION UTILITY PROJECT.
+ *
+ * @section CONTRIBUTOR Contributor.
+ *
+ *  - zer0 (osom8979@gmail.com)
+ */
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_DOXYGEN_H__
+
