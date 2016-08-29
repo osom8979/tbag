66622dcf1ba2ff35c2dcec2bbce09f8faceb5613
zer0 <osom8979@gmail.com>
2016-08-29 14:13:13 +0900

    Fix the ASSERT_NEAR() parameter bug.

--- tester/math/equationTest.cpp
+++ tester/math/equationTest.cpp
@@ -18,12 +18,19 @@ TEST(equationTest, getLinearEquationWithTwoPoint)
     ASSERT_EQ(0, e.b);
 }
 
+TEST(equationTest, isParallelWithTwoLinearEquation)
+{
+    LinearEquation<double> e1{2.4, 1};
+    LinearEquation<double> e2{2.4, 2};
+    ASSERT_TRUE(isParallelWithTwoLinearEquation(e1, e2));
+}
+
 TEST(equationTest, getIntersectionWithTwoLinearEquation)
 {
-    LinearEquation<double> e1{ 2,  0};
-    LinearEquation<double> e2{-2,  2};
+    LinearEquation<double> e1{ 2, 0};
+    LinearEquation<double> e2{-2, 2};
     auto p = getIntersectionWithTwoLinearEquation(e1, e2);
-    ASSERT_NEAR(0.5, p.x, 1);
-    ASSERT_NEAR(1.0, p.y, 1);
+    ASSERT_NEAR(0.5, p.x, 0.1);
+    ASSERT_NEAR(1.0, p.y, 0.1);
 }
 
