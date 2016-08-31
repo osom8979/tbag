0e4ed2338fac1b620f070e4b9a9718ec6305eacf
zer0 <osom8979@gmail.com>
2016-08-31 11:46:47 +0900

    Create & Test libtbag::container::DependencyWorker class.

---
+++ libtbag/container/DependencyWorker.hpp
@@ -0,0 +1,162 @@
+/**
+ * @file   DependencyWorker.hpp
+ * @brief  DependencyWorker class prototype.
+ * @author zer0
+ * @date   2016-08-31
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_CONTAINER_DEPENDENCYWORKER_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_CONTAINER_DEPENDENCYWORKER_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+
+#include <functional>
+#include <vector>
+#include <set>
+#include <unordered_map>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace container {
+
+/**
+ * DependencyWorker class prototype.
+ *
+ * @author zer0
+ * @date   2016-08-31
+ */
+template <typename KeyType>
+class DependencyWorker
+{
+public:
+    static constexpr std::size_t DEFAULT_MAX_RECURSIVE = 10;
+
+public:
+    using Key          = KeyType;
+    using Worker       = std::function<void(void)>;
+    using Dependencies = std::vector<Key>;
+
+public:
+    struct WorkingProperty
+    {
+        Dependencies dependencies;
+        Worker worker;
+    };
+
+public:
+    using WorkingMap  = std::unordered_map<Key, WorkingProperty>;
+    using WorkingPair = typename WorkingMap::value_type;
+
+private:
+    std::size_t _max_recursive = DEFAULT_MAX_RECURSIVE;
+    WorkingMap  _workers;
+
+public:
+    DependencyWorker() = default;
+    ~DependencyWorker() = default;
+
+public:
+    DependencyWorker(std::size_t max_recursive) : _max_recursive(max_recursive)
+    {
+        // EMPTY.
+    }
+
+public:
+    DependencyWorker(DependencyWorker const & obj) = default;
+    DependencyWorker(DependencyWorker && obj) = default;
+
+public:
+    DependencyWorker & operator =(DependencyWorker const & obj) = default;
+    DependencyWorker & operator =(DependencyWorker && obj) = default;
+
+public:
+    inline std::size_t getMaxRecursive() const noexcept
+    { return _max_recursive; }
+    inline void setMaxRecursive(std::size_t count) noexcept
+    { _max_recursive = count; }
+    inline bool empty() const noexcept
+    { return _workers.empty(); }
+    inline std::size_t size() const noexcept
+    { return _workers.size(); }
+
+public:
+    void clear()
+    {
+        _workers.clear();
+    }
+
+    bool insert(Key key, WorkingProperty const & worker)
+    {
+        return _workers.insert(std::make_pair(key, worker)).second;
+    }
+
+    bool insert(Key key, WorkingProperty && worker)
+    {
+        return _workers.insert(std::make_pair(key, worker)).second;
+    }
+
+    bool insert(Key key, Dependencies const & dependencies, Worker const & worker)
+    {
+        return insert(key, WorkingProperty{dependencies, worker});
+    }
+
+    bool insert(Key key, Dependencies && dependencies, Worker && worker)
+    {
+        return insert(key, WorkingProperty{dependencies, worker});
+    }
+
+private:
+    bool run(Key const & key, Dependencies & completed_keys, std::size_t recursive_count)
+    {
+        // Endless loop guard.
+        if (recursive_count == 0) {
+            return false;
+        }
+        --recursive_count;
+
+        // Exists worker.
+        auto find_itr = _workers.find(key);
+        if (find_itr == _workers.end()) {
+            return false;
+        }
+
+        // Children dependencies.
+        for (auto & dependency : find_itr->second.dependencies) {
+            if (run(dependency, completed_keys, recursive_count) == false) {
+                return false;
+            }
+        }
+
+        // Exists key.
+        bool not_found_key = std::find(completed_keys.begin(), completed_keys.end(), key) == completed_keys.end();
+        if (static_cast<bool>(find_itr->second.worker) && not_found_key) {
+            // WORKING!
+            find_itr->second.worker();
+            completed_keys.push_back(key);
+        }
+        return true;
+    }
+
+public:
+    bool run(Key const & key)
+    {
+        Dependencies completed_keys;
+        return run(key, completed_keys, _max_recursive);
+    }
+};
+
+} // namespace container
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_CONTAINER_DEPENDENCYWORKER_HPP__
+
---
+++ tester/container/DependencyWorkerTest.cpp
@@ -0,0 +1,62 @@
+/**
+ * @file   DependencyWorkerTest.cpp
+ * @brief  DependencyWorker class tester.
+ * @author zer0
+ * @date   2016-08-31
+ */
+
+#include <gtest/gtest.h>
+#include <libtbag/container/DependencyWorker.hpp>
+
+using namespace libtbag;
+using namespace libtbag::container;
+
+TEST(DependencyWorkerTest, Constructor)
+{
+    DependencyWorker<int> w0;
+
+    DependencyWorker<int> w1(1);
+    ASSERT_EQ(1, w1.getMaxRecursive());
+
+    DependencyWorker<int> w2 = w1;
+    ASSERT_EQ(1, w2.getMaxRecursive());
+
+    DependencyWorker<int> temp1(2);
+    ASSERT_EQ(2, temp1.getMaxRecursive());
+
+    DependencyWorker<int> temp2 = std::move(temp1);
+    ASSERT_EQ(2, temp2.getMaxRecursive());
+
+    w1 = w0;
+    ASSERT_EQ(w0.getMaxRecursive(), w1.getMaxRecursive());
+
+    w2 = std::move(temp2);
+    ASSERT_EQ(2, w2.getMaxRecursive());
+}
+
+TEST(DependencyWorkerTest, OnlyCoverage)
+{
+    DependencyWorker<int> worker;
+    worker.empty();
+    worker.size();
+    worker.getMaxRecursive();
+    worker.setMaxRecursive(10U);
+    worker.clear();
+}
+
+TEST(DependencyWorkerTest, EndlessLoopGuard)
+{
+    DependencyWorker<int> worker;
+
+    bool result1 = false;
+    bool result2 = false;
+
+    ASSERT_TRUE(worker.insert(1, DependencyWorker<int>::Dependencies{2}, [&](){ result1 = true; }));
+    ASSERT_TRUE(worker.insert(2, DependencyWorker<int>::Dependencies{1}, [&](){ result2 = true; }));
+
+    ASSERT_FALSE(worker.run(1));
+    ASSERT_FALSE(worker.run(2));
+    ASSERT_FALSE(result1);
+    ASSERT_FALSE(result2);
+}
+
