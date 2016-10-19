6736508e0abf063233b4c0fe98b29164ea13d37d
zer0 <osom8979@gmail.com>
2016-10-19 17:55:05 +0900

    Fix the LDFLAGS order.

--- libtbag/project.cmake
+++ libtbag/project.cmake
@@ -21,16 +21,6 @@ list (APPEND TBAG_PROJECT_INCLUDE_DIRS  ${CMAKE_SOURCE_DIR}/dep/lua/include)
 ## dep/uv
 list (APPEND TBAG_PROJECT_DEPENDENCIES  uv)
 list (APPEND TBAG_PROJECT_INCLUDE_DIRS  ${CMAKE_SOURCE_DIR}/dep/uv/include)
-if (UNIX)
-    list (APPEND TBAG_PROJECT_LDFLAGS -lpthread)
-    if (APPLE)
-        list (APPEND TBAG_PROJECT_LDFLAGS -ldl)
-    else ()
-        list (APPEND TBAG_PROJECT_LDFLAGS -lrt -lnsl -ldl)
-    endif ()
-else (WIN32)
-    list (APPEND TBAG_PROJECT_LDFLAGS ws2_32.lib iphlpapi.lib winmm.lib psapi.lib userenv.lib)
-endif ()
 
 ## dep/icu
 list (APPEND TBAG_PROJECT_DEPENDENCIES  icuuc icui18n)
@@ -42,6 +32,18 @@ tbag_project__set_whole_archive ($<TARGET_FILE:lua>)
 tbag_project__set_whole_archive ($<TARGET_FILE:uv>)
 tbag_project__set_whole_archive ($<TARGET_FILE:icuuc>)
 tbag_project__set_whole_archive ($<TARGET_FILE:icui18n>)
+
+## external libraries.
+if (UNIX)
+    list (APPEND TBAG_PROJECT_LDFLAGS -lpthread)
+    if (APPLE)
+        list (APPEND TBAG_PROJECT_LDFLAGS -ldl)
+    else ()
+        list (APPEND TBAG_PROJECT_LDFLAGS -lnsl -ldl -lrt)
+    endif ()
+else (WIN32)
+    list (APPEND TBAG_PROJECT_LDFLAGS ws2_32.lib iphlpapi.lib winmm.lib psapi.lib userenv.lib)
+endif ()
 
 #tbag_modules__include_project (PCH ${CMAKE_SOURCE_DIR}/libtbag/config.h)
 
