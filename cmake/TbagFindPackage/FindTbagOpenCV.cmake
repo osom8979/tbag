b3b6fadf84de2da4beff9994a181c80b439012ee
zer0 <osom8979@gmail.com>
2016-10-20 17:36:31 +0900

    Refactoring FindTbagOpenCV cmake file.

--- cmake/TbagFindPackage/FindTbagOpenCV.cmake
+++ cmake/TbagFindPackage/FindTbagOpenCV.cmake
@@ -7,27 +7,34 @@
 #///  The following variables are optionally searched for defaults:
 #///    - ${OpenCV_ROOT}
 
-if (NOT DEFINED OpenCV_ROOT)
-    if (IS_DIRECTORY "${THIRD_PREFIX}")
-        set (OpenCV_ROOT "${THIRD_PREFIX}")
-    else ()
-        set (OpenCV_ROOT "/usr/local")
-    endif ()
+## Find opencv-cmake-module.
+if (NOT DEFINED OpenCV_DIR)
+    message (STATUS "Looking for opencv-cmake-module.")
+    find_path (OpenCV_DIR
+               NAMES "OpenCVConfig.cmake"
+               PATHS "${PROJECT_SOURCE_DIR}/cmake"
+                     "${OpenCV_ROOT}"
+                     "${OpenCV_ROOT}/build"
+                     "${OpenCV_ROOT}/share/OpenCV"
+                     "${THIRD_PREFIX}/build"
+                     "${THIRD_PREFIX}/share/OpenCV"
+                     "$ENV{OPENCV_HOME}"
+                     "$ENV{OPENCV_HOME}/build"
+                     "$ENV{OPENCV_HOME}/share/OpenCV"
+                     "$ENV{TPARTY_HOME}/local/share/OpenCV"
+                     "/usr/local/share/OpenCV")
 endif ()
 
-set (__tbag_opencv_module_dir "${OpenCV_ROOT}/share/OpenCV")
-if (NOT IS_DIRECTORY "${__tbag_opencv_module_dir}")
-    if (IS_DIRECTORY "/usr/local/share/OpenCV")
-        set (__tbag_opencv_module_dir "/usr/local/share/OpenCV")
-    else ()
-        #message (WARNING "Not found OpenCV Modules directory: ${__tbag_opencv_module_dir}")
-    endif ()
+if (IS_DIRECTORY "${OpenCV_DIR}")
+    message (STATUS "Find OpenCV Modules directory: ${OpenCV_DIR}")
+else ()
+    message (WARNING "Not found OpenCV Modules directory: ${OpenCV_DIR}")
 endif ()
 
 if (NOT OpenCV_FOUND)
-    list (FIND "${CMAKE_MODULE_PATH}" "${__tbag_opencv_module_dir}" __opencv_module_index)
+    list (FIND "${CMAKE_MODULE_PATH}" "${OpenCV_DIR}" __opencv_module_index)
     if (__opencv_module_index EQUAL -1)
-        list (APPEND CMAKE_MODULE_PATH "${__tbag_opencv_module_dir}")
+        list (APPEND CMAKE_MODULE_PATH "${OpenCV_DIR}")
     endif ()
 
     if (TbagOpenCV_FIND_QUIETLY)
