89064795955fbaf90e776fbe3f0284c8c29615ed
zer0 <osom8979@gmail.com>
2016-10-30 14:25:40 +0900

    Create libtbag::archive::Lzma class prototype.

---
+++ libtbag/archive/Lzma.cpp
@@ -0,0 +1,71 @@
+/**
+ * @file   Lzma.cpp
+ * @brief  Lzma class implementation.
+ * @author zer0
+ * @date   2016-10-30
+ */
+
+#include <libtbag/archive/Lzma.hpp>
+
+#include <Alloc.h>
+#include <7zFile.h>
+#include <7zVersion.h>
+#include <LzmaDec.h>
+#include <LzmaEnc.h>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace archive {
+
+/**
+ * Pointer to implementation of LZMA SDK.
+ *
+ * @author zer0
+ * @date   2016-10-30
+ */
+struct Lzma::LzmaPimpl
+{
+private:
+
+public:
+    LzmaPimpl()
+    {
+    }
+
+    ~LzmaPimpl()
+    {
+    }
+};
+
+// --------------------
+// Lzma implementation.
+// --------------------
+
+Lzma::Lzma() : _lzma(new LzmaPimpl)
+{
+    // EMPTY.
+}
+
+Lzma::~Lzma()
+{
+    // EMPTY.
+}
+
+bool Lzma::write(std::string const & path)
+{
+    return false;
+}
+
+bool Lzma::read(std::string const & path)
+{
+    return false;
+}
+
+} // namespace archive
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
---
+++ libtbag/archive/Lzma.hpp
@@ -0,0 +1,59 @@
+/**
+ * @file   Lzma.hpp
+ * @brief  Lzma class prototype.
+ * @author zer0
+ * @date   2016-10-30
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_ARCHIVE_LZMA_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_ARCHIVE_LZMA_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+#include <libtbag/Noncopyable.hpp>
+
+#include <string>
+#include <memory>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace archive {
+
+/**
+ * Lzma class prototype.
+ *
+ * @author zer0
+ * @date   2016-10-30
+ */
+class TBAG_EXPORTS Lzma : public Noncopyable
+{
+public:
+    struct LzmaPimpl;
+    using UniqueLzmaPimpl = std::unique_ptr<LzmaPimpl>;
+
+private:
+    UniqueLzmaPimpl _lzma;
+
+public:
+    Lzma();
+    ~Lzma();
+
+public:
+    bool write(std::string const & path);
+    bool read(std::string const & path);
+};
+
+} // namespace archive
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_ARCHIVE_LZMA_HPP__
+
