2b9cc2388c8b20c571b2f7ccfabd949516d34d9f
zer0 <osom8979@gmail.com>
2016-11-01 16:37:37 +0900

    Apply USE_SHARED_DEPS option.

--- tester/project.cmake
+++ tester/project.cmake
@@ -16,6 +16,21 @@ if (NOT BUILD_SHARED_LIBS)
     list (APPEND TBAG_PROJECT_DEFINITIONS TBAG_STATIC_API)
 endif ()
 
+## Dependencies.
+if (USE_SHARED_DEPS)
+    tbag_modules__apply_dep_icu     ()
+    tbag_modules__apply_dep_lmdb    ()
+    tbag_modules__apply_dep_lzma    ()
+    tbag_modules__apply_dep_sqlite3 ()
+    tbag_modules__apply_dep_uv      ()
+    if (USE_LUA)
+        tbag_modules__apply_dep_lua ()
+    endif ()
+    if (USE_JSONCPP)
+        tbag_modules__apply_dep_jsoncpp ()
+    endif ()
+endif ()
+
 ## Test project.
 list (APPEND TBAG_PROJECT_DEPENDENCIES tbshare_copy)
 list (APPEND TBAG_PROJECT_DEPENDENCIES tbproc_copy)
