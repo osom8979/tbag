3a3d968b06b22b9c3a59dca91a04ea525962651e
zer0 <osom8979@gmail.com>
2016-11-01 15:12:32 +0900

    Create gtest entry-point in tester project.

---
+++ tester/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file   main.cpp
+ * @brief  Tester project entry-point.
+ * @author zer0
+ * @date   2016-11-01
+ */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
