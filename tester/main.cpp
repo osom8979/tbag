cfe1be66df5b9905ac6315ba2858d7cbcca7f7e6
zer0 <osom8979@gmail.com>
2016-11-02 21:34:38 +0900

    Create server & client test code.

--- tester/main.cpp
+++ tester/main.cpp
@@ -8,11 +8,68 @@
 #include <gtest/gtest.h>
 #include <libtbag/libtbag.h>
 
+#include <libtbag/string/Commander.hpp>
+#include <libtbag/filesystem/Path.hpp>
+#include <libtbag/network/socket/Server.hpp>
+#include <libtbag/network/socket/Client.hpp>
+
+#include <iostream>
+#include <string>
+
+static char const * const CMD_ARGUMENT_SERVER = "server";
+static char const * const CMD_ARGUMENT_CLIENT = "client";
+
+enum class TestMode
+{
+    ERROR = 0,
+    TEST,
+    SERVER,
+    CLIENT
+};
+
 int main(int argc, char **argv)
 {
     tbInitialize();
 
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    using Commander = libtbag::string::Commander;
+    using Args = libtbag::string::Arguments;
+    using Path = libtbag::filesystem::Path;
+
+    TestMode mode = TestMode::TEST;
+    std::string ip;
+    int port;
+
+    {   // COMMAND-LINE PARSING.
+        Commander cmd;
+        cmd.insert(CMD_ARGUMENT_SERVER, [&](Args const & args){
+            if (args.optString(0, &ip) && args.optInteger(1, &port)) {
+                mode = TestMode::SERVER;
+            } else {
+                mode = TestMode::ERROR;
+            }
+        });
+        cmd.insert(CMD_ARGUMENT_CLIENT, [&](Args const & args){
+            if (args.optString(0, &ip) && args.optInteger(1, &port)) {
+                mode = TestMode::CLIENT;
+            } else {
+                mode = TestMode::ERROR;
+            }
+        });
+        cmd.request(argc, argv);
+    }
+
+    if (mode == TestMode::TEST) {
+        testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+    } else if (mode == TestMode::SERVER) {
+        libtbag::network::socket::Server server;
+        return server.runIpv4(ip, port) ? 0 : 1;
+    } else if (mode == TestMode::CLIENT) {
+        libtbag::network::socket::Client client;
+        return client.connect(ip, port) ? 0 : 1;
+    } else {
+        std::cout << "Argument error.\n";
+        return 1;
+    }
 }
 
