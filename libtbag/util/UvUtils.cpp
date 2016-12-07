4316e999aa0a894d44c1b8900a110972b36126de
zer0 <osom8979@gmail.com>
2016-12-07 13:32:08 +0900

    Create UvHandle class.

--- libtbag/util/UvUtils.cpp
+++ libtbag/util/UvUtils.cpp
@@ -6,6 +6,8 @@
  */
 
 #include <libtbag/util/UvUtils.hpp>
+#include <cstdlib>
+#include <cstring>
 #include <uv.h>
 
 // -------------------
@@ -26,6 +28,7 @@ char const * getUvType(void * handle)
 #ifndef __CASE_TYPE_NAME_RETURN
 #define __CASE_TYPE_NAME_RETURN(name) case name: return #name
 #endif
+
     switch (static_cast<uv_handle_t*>(handle)->type) {
     __CASE_TYPE_NAME_RETURN(UV_NAMED_PIPE);
     __CASE_TYPE_NAME_RETURN(UV_TTY);
@@ -43,7 +46,66 @@ char const * getUvType(void * handle)
     __CASE_TYPE_NAME_RETURN(UV_SIGNAL);
     default: return "Unknown handle type.";
     }
+
 #undef __CASE_TYPE_NAME_RETURN
+}
+
+// ------------------------
+// UvHandle implementation.
+// ------------------------
+
+UvHandle::UvHandle(UvType type) : TYPE(type), _native(nullptr)
+{
+#ifndef __CASE_TYPE_ALLOC
+#define __CASE_TYPE_ALLOC(type, uv_type)          \
+    case UvType::type:                            \
+        _native = ::malloc(sizeof(uv_type));      \
+        ::memset(_native, 0x00, sizeof(uv_type)); \
+        break
+#endif
+
+    switch (TYPE) {
+    __CASE_TYPE_ALLOC(LOOP             , uv_loop_t);
+    __CASE_TYPE_ALLOC(HANDLE           , uv_handle_t);
+    __CASE_TYPE_ALLOC(STREAM           , uv_stream_t);
+    __CASE_TYPE_ALLOC(TCP              , uv_tcp_t);
+    __CASE_TYPE_ALLOC(UDP              , uv_udp_t);
+    __CASE_TYPE_ALLOC(PIPE             , uv_pipe_t);
+    __CASE_TYPE_ALLOC(TTY              , uv_tty_t);
+    __CASE_TYPE_ALLOC(POLL             , uv_poll_t);
+    __CASE_TYPE_ALLOC(TIMER            , uv_timer_t);
+    __CASE_TYPE_ALLOC(PREPARE          , uv_prepare_t);
+    __CASE_TYPE_ALLOC(CHECK            , uv_check_t);
+    __CASE_TYPE_ALLOC(IDLE             , uv_idle_t);
+    __CASE_TYPE_ALLOC(ASYNC            , uv_async_t);
+    __CASE_TYPE_ALLOC(PROCESS          , uv_process_t);
+    __CASE_TYPE_ALLOC(FS_EVENT         , uv_fs_event_t);
+    __CASE_TYPE_ALLOC(FS_POLL          , uv_fs_poll_t);
+    __CASE_TYPE_ALLOC(SIGNAL           , uv_signal_t);
+    __CASE_TYPE_ALLOC(REQ              , uv_req_t);
+    __CASE_TYPE_ALLOC(GETADDRINFO      , uv_getaddrinfo_t);
+    __CASE_TYPE_ALLOC(GETNAMEINFO      , uv_getnameinfo_t);
+    __CASE_TYPE_ALLOC(SHUTDOWN         , uv_shutdown_t);
+    __CASE_TYPE_ALLOC(WRITE            , uv_write_t);
+    __CASE_TYPE_ALLOC(CONNECT          , uv_connect_t);
+    __CASE_TYPE_ALLOC(UDP_SEND         , uv_udp_send_t);
+    __CASE_TYPE_ALLOC(FS               , uv_fs_t);
+    __CASE_TYPE_ALLOC(WORK             , uv_work_t);
+    __CASE_TYPE_ALLOC(CPU_INFO         , uv_cpu_info_t);
+    __CASE_TYPE_ALLOC(INTERFACE_ADDRESS, uv_interface_address_t);
+    __CASE_TYPE_ALLOC(DIRENT           , uv_dirent_t);
+    __CASE_TYPE_ALLOC(PASSWD           , uv_passwd_t);
+    }
+
+#undef __CASE_TYPE_ALLOC
+}
+
+UvHandle::~UvHandle()
+{
+    if (_native != nullptr) {
+        ::free(_native);
+        _native = nullptr;
+    }
 }
 
 } // namespace util
--- libtbag/util/UvUtils.hpp
+++ libtbag/util/UvUtils.hpp
@@ -15,6 +15,7 @@
 
 #include <libtbag/config.h>
 #include <libtbag/predef.hpp>
+#include <libtbag/Noncopyable.hpp>
 
 // -------------------
 NAMESPACE_LIBTBAG_OPEN
@@ -24,6 +25,87 @@ namespace util {
 
 TBAG_API void initUv();
 TBAG_API char const * getUvType(void * handle);
+
+/**
+ * Table of libuv handle types.
+ *
+ * @author zer0
+ * @date   2016-12-07
+ */
+enum class UvType : int
+{
+    // Handle types.
+    LOOP,
+    HANDLE,
+    STREAM,
+    TCP,
+    UDP,
+    PIPE,
+    TTY,
+    POLL,
+    TIMER,
+    PREPARE,
+    CHECK,
+    IDLE,
+    ASYNC,
+    PROCESS,
+    FS_EVENT,
+    FS_POLL,
+    SIGNAL,
+
+    // Request types.
+    REQ,
+    GETADDRINFO,
+    GETNAMEINFO,
+    SHUTDOWN,
+    WRITE,
+    CONNECT,
+    UDP_SEND,
+    FS,
+    WORK,
+
+    // None of the above.
+    CPU_INFO,
+    INTERFACE_ADDRESS,
+    DIRENT,
+    PASSWD,
+};
+
+/**
+ * libuv handle utility class.
+ *
+ * @author zer0
+ * @date   2016-12-07
+ */
+class TBAG_API UvHandle
+{
+private:
+    UvType const TYPE;
+    void * _native;
+
+public:
+    UvHandle(UvType type);
+    ~UvHandle();
+
+public:
+    inline operator bool() const TBAG_NOEXCEPT
+    { return _native != nullptr; }
+
+public:
+    inline UvType getType() const TBAG_NOEXCEPT
+    { return TYPE; }
+
+public:
+    inline void * getNative() TBAG_NOEXCEPT
+    { return _native; }
+    inline void const * getNative() const TBAG_NOEXCEPT
+    { return _native; }
+
+public:
+    template <typename T>
+    inline T * castNative() TBAG_NOEXCEPT
+    { return static_cast<T*>(_native); }
+};
 
 } // namespace util
 
