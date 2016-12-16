e0f65682e572eedf8cc2bff2383d4eff21966ce0
zer0 <osom8979@gmail.com>
2016-12-16 16:56:21 +0900

    Remove tLogF macros.

--- tester/log/LogTest.cpp
+++ tester/log/LogTest.cpp
@@ -18,25 +18,25 @@ TEST(LogTest, Console)
     log::createDefaultColorConsoleLogger();
     log::setDefaultLevel(log::LogLevel::LEVEL_NOTICE);
 
-    tDLogEF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "emergency", 0.1, 'T');
-    tDLogAF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "alert", 0.1, 'T');
-    tDLogCF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "critical", 0.1, 'T');
-    tDLogRF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "error", 0.1, 'T');
+    tDLogM("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "emergency", 0.1, 'T');
+    tDLogA("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "alert", 0.1, 'T');
+    tDLogC("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "critical", 0.1, 'T');
+    tDLogE("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "error", 0.1, 'T');
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    tDLogWF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "warning", 0.1, 'T');
-    tDLogNF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "notice", 0.1, 'T');
-    tDLogIF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "info", 0.1, 'T');
-    tDLogDF("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "debug", 0.1, 'T');
+    tDLogW("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "warning", 0.1, 'T');
+    tDLogN("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "notice", 0.1, 'T');
+    tDLogI("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "info", 0.1, 'T');
+    tDLogD("LogTest: {0}, {1}, {0}, {2}, {3}", 100, "debug", 0.1, 'T');
 
     log::setDefaultLevel(log::LogLevel::LEVEL_DEBUG);
     tDLogI("CHANGE LOG LEVEL: info.");
     tDLogD("CHANGE LOG LEVEL: debug.");
 
     log::setDefaultLevel(log::LogLevel::LEVEL_OFF);
-    tDLogE("OFF LOG: tDLogE");
+    tDLogM("OFF LOG: tDLogE");
     tDLogA("OFF LOG: tDLogA");
     tDLogC("OFF LOG: tDLogC");
-    tDLogR("OFF LOG: tDLogR");
+    tDLogE("OFF LOG: tDLogR");
     tDLogW("OFF LOG: tDLogW");
     tDLogN("OFF LOG: tDLogN");
     tDLogI("OFF LOG: tDLogI");
@@ -50,10 +50,10 @@ TEST(LogTest, File)
     log::createDefaultFileLogger("__log_test_file");
     log::setDefaultLevel(log::LogLevel::LEVEL_WARNING);
 
-    tDLogE("LogTest: emergency");
+    tDLogM("LogTest: emergency");
     tDLogA("LogTest: alert");
     tDLogC("LogTest: critical");
-    tDLogR("LogTest: error");
+    tDLogE("LogTest: error");
     tDLogW("LogTest: warning");
     tDLogN("LogTest: notice");
     tDLogI("LogTest: info");
