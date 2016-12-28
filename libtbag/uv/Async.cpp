a4ee99a602a12bb16057f9dd987b17dc47caf867
zer0 <osom8979@gmail.com>
2016-12-28 13:55:43 +0900

    Create libtbag::uv::Async class.

---
+++ libtbag/uv/Async.cpp
@@ -0,0 +1,76 @@
+/**
+ * @file   Async.cpp
+ * @brief  Async class implementation.
+ * @author zer0
+ * @date   2016-12-28
+ */
+
+#include <libtbag/uv/Async.hpp>
+#include <libtbag/log/Log.hpp>
+#include <libtbag/uv/Loop.hpp>
+
+#include <uv.h>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace uv {
+
+// --------------------
+// Global libuv events.
+// --------------------
+
+static void __global_uv_async_cb__(uv_async_t * handle)
+{
+    Async * async = static_cast<Async*>(handle->data);
+    if (async == nullptr) {
+        __tbag_error("__global_uv_async_cb__() handle data is nullptr.");
+        return;
+    }
+    async->onAsync();
+}
+
+// ---------------------
+// Async implementation.
+// ---------------------
+
+Async::Async(Loop & loop) : Handle(UvHandleType::ASYNC)
+{
+    int const CODE = ::uv_async_init(loop.cast<uv_loop_t>(), Parent::cast<uv_async_t>(), __global_uv_async_cb__);
+    if (CODE != 0) {
+        __tbag_error("Async::Async() error [{}] {}", CODE, getUvErrorName(CODE));
+        throw std::bad_alloc();
+    }
+}
+
+Async::~Async()
+{
+    // EMPTY.
+}
+
+bool Async::send()
+{
+    int const CODE = ::uv_async_send(Parent::cast<uv_async_t>());
+    if (CODE != 0) {
+        __tbag_error("Async::send() error [{}] {}", CODE, getUvErrorName(CODE));
+        return false;
+    }
+    return true;
+}
+
+// --------------
+// Event methods.
+// --------------
+
+void Async::onAsync()
+{
+    __tbag_debug("Async::onAsync() called.");
+}
+
+} // namespace uv
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
---
+++ libtbag/uv/Async.hpp
@@ -0,0 +1,90 @@
+/**
+ * @file   Async.hpp
+ * @brief  Async class prototype.
+ * @author zer0
+ * @date   2016-12-28
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_UV_ASYNC_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_UV_ASYNC_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+#include <libtbag/predef.hpp>
+#include <libtbag/Noncopyable.hpp>
+#include <libtbag/uv/Handle.hpp>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace uv {
+
+// Forward declaration.
+class Loop;
+
+/**
+ * Async class prototype.
+ *
+ * @author zer0
+ * @date   2016-12-28
+ *
+ * @remarks
+ *  Async handles allow the user to "wakeup" @n
+ *  the event loop and get a callback called from another thread.
+ *
+ * @warning
+ *  A uv_async_t handle is always active and cannot be deactivated, @n
+ *  except by closing it with uv_close().
+ */
+class TBAG_API Async : public Handle
+{
+public:
+    using Parent = Handle;
+
+public:
+    /**
+     * @warning
+     *  Unlike other handle initialization functions, @n
+     *  it immediately starts the handle.
+     */
+    Async(Loop & loop);
+    virtual ~Async();
+
+public:
+    /**
+     * Wakeup the event loop and call the async handle’s callback.
+     *
+     * @remarks
+     *  It’s safe to call this function from any thread. @n
+     *  The callback will be called on the loop thread.
+     *
+     * @warning
+     *  libuv will coalesce calls to uv_async_send(), @n
+     *  that is, not every call to it will yield an execution of the callback. @n
+     *  For example: @n
+     *  <pre>
+     *   if uv_async_send() is called 5 times in a row before the callback is called,
+     *   the callback will only be called once. If uv_async_send() is called again after
+     *   the callback was called, it will be called again.
+     *  </pre>
+     */
+    bool send();
+
+// Event methods.
+public:
+    virtual void onAsync();
+};
+
+} // namespace uv
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_UV_ASYNC_HPP__
+
