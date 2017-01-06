d8a6fe4aaec4f94faf1db3b5115b66e91ff7e17e
zer0 <osom8979@gmail.com>
2017-01-06 11:31:50 +0900

    Test libtbag::uv::Async class.

---
+++ tester/uv/AsyncTest.cpp
@@ -0,0 +1,49 @@
+/**
+ * @file   AsyncTest.cpp
+ * @brief  Async libuv class tester.
+ * @author zer0
+ * @date   2017-01-06
+ */
+
+#include <gtest/gtest.h>
+#include <libtbag/uv/Loop.hpp>
+#include <libtbag/uv/Async.hpp>
+
+using namespace libtbag;
+using namespace libtbag::uv;
+
+static bool g_is_async = false;
+static bool g_is_close = false;
+
+struct AsyncTest : public Async 
+{
+    AsyncTest(Loop & loop) : Async(loop)
+    { /* EMPTY. */ }
+
+    virtual void onAsync() override
+    {
+        g_is_async = true;
+        close();
+    }
+
+    virtual void onClose() override
+    {
+        g_is_close = true;
+    }
+};
+
+TEST(AsyncTest, Default)
+{
+    {
+        Loop loop;
+        // auto async = loop.newHandle<AsyncTest>(loop);
+        auto shared = loop.insertChildHandle(new AsyncTest(loop)).lock();
+        AsyncTest * handle = static_cast<AsyncTest*>(shared.get());
+        handle->send();
+        loop.run();
+    }
+
+    ASSERT_TRUE(g_is_async);
+    ASSERT_TRUE(g_is_close);
+}
+
