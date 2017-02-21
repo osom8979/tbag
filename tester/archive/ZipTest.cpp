2fe81a4282addbf7637c0e5a9094e48641cf05b4
zer0 <osom8979@gmail.com>
2017-02-21 16:01:02 +0900

    Test image zlib compression.

--- tester/archive/ZipTest.cpp
+++ tester/archive/ZipTest.cpp
@@ -7,10 +7,15 @@
 
 #include <gtest/gtest.h>
 #include <libtbag/archive/Zip.hpp>
+#include <libtbag/time/ElapsedTime.hpp>
 
 #include <cstdint>
+
 #include <string>
+#include <iostream>
+#include <chrono>
 #include <vector>
+#include <random>
 
 using namespace libtbag;
 using namespace libtbag::archive;
@@ -38,6 +43,45 @@ TEST(ZipTest, Default)
 
     result.assign(inflate.begin(), inflate.end());
     ASSERT_EQ(TEST_BODY, result);
+}
+
+TEST(ZipTest, ImageTest)
+{
+    int width    = 300;
+    int height   = 300;
+    int channels = 3;
+    int total    = width * height * channels;
+
+    std::random_device device;
+    std::mt19937 engine(device());
+    std::uniform_int_distribution<uint8_t> distribution(0x00, 0xFF);
+
+    std::vector<uint8_t> input;
+    input.resize(total);
+    for (auto & pixel : input) {
+        pixel = distribution(engine);
+    }
+
+    std::vector<uint8_t> deflate;
+    Zip::ResultCode result;
+
+    auto encode_duration = time::getDurationWithPredicated<std::chrono::milliseconds>([&](){
+        result = Zip::encode(deflate, &input[0], input.size());
+    });
+    ASSERT_EQ(Zip::ResultCode::SUCCESS, result);
+    std::cout << "Image(" << width << "x" << height << ") Encode size: " << deflate.size() << " byte, time: " << encode_duration.count() << " milliseconds.\n";
+
+    std::vector<uint8_t> inflate;
+    auto decode_duration = time::getDurationWithPredicated<std::chrono::milliseconds>([&](){
+        result = Zip::decode(inflate, &deflate[0], deflate.size());
+    });
+    ASSERT_EQ(Zip::ResultCode::SUCCESS, result);
+    std::cout << "Image(" << width << "x" << height << ") Decode size: " << inflate.size() << " byte, time: " << decode_duration.count() << " milliseconds.\n";
+
+    ASSERT_EQ(input.size(), inflate.size());
+    for (auto itr = input.begin(), itr2 = inflate.begin(); itr != input.end(); ++itr, ++itr2) {
+        ASSERT_EQ(*itr, *itr2);
+    }
 }
 
 TEST(ZipTest, unzip)
