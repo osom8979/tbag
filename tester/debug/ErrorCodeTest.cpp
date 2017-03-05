ae15b168314580a1bb8150496f22707bffadf56a
zer0 <osom8979@gmail.com>
2017-03-05 16:07:33 +0900

    Fix the uninitialize error.

--- tester/debug/ErrorCodeTest.cpp
+++ tester/debug/ErrorCodeTest.cpp
@@ -8,12 +8,14 @@
 #include <gtest/gtest.h>
 #include <libtbag/debug/ErrorCode.hpp>
 
+#include <limits>
+
 using namespace libtbag;
 using namespace libtbag::debug;
 
 TEST(ErrorCodeTest, Default)
 {
-    libtbag::Err code;
+    libtbag::Err code = static_cast<libtbag::Err>(std::numeric_limits<libtbag::ErrType>::max());
     ASSERT_NE(nullptr, getErrorMessage(code));
     ASSERT_STREQ(getErrorMessage(libtbag::Err::SUCCESS), getErrorMessage(0));
 }
