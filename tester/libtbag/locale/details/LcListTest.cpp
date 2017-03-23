d4f6838a05c16ea8fbdd08dc53f99be0f03d34ac
zer0 <osom8979@gmail.com>
2017-03-23 13:14:37 +0900

    Trivial commit.

--- tester/libtbag/locale/details/LcListTest.cpp
+++ tester/libtbag/locale/details/LcListTest.cpp
@@ -8,13 +8,15 @@
 #include <gtest/gtest.h>
 #include <libtbag/locale/details/LcList.hpp>
 
+#include <algorithm>
+
 using namespace libtbag;
 using namespace libtbag::locale;
 using namespace libtbag::locale::details;
 
 TEST(LcListTest, Default)
 {
-    auto const LOCALE_LIST = getAllLocale();
+    std::vector<std::string> const LOCALE_LIST = getAllLocale();
     ASSERT_EQ(LOCALE_LIST.size(), getAllLocaleCount());
     ASSERT_NE(LOCALE_LIST.end(), std::find(LOCALE_LIST.begin(), LOCALE_LIST.end(), std::string("ko_KR.UTF-8")));
 }
