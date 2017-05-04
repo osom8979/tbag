6fca92c1a70707b2d0184ccaa2037804ec0fe5e4
zer0 <osom8979@gmail.com>
2017-05-04 09:36:51 +0900

    Test Prepare & Check classes.

--- tester/libtbag/uvpp/TimerTest.cpp
+++ tester/libtbag/uvpp/TimerTest.cpp
@@ -8,12 +8,44 @@
 #include <gtest/gtest.h>
 #include <libtbag/uvpp/Loop.hpp>
 #include <libtbag/uvpp/Timer.hpp>
+#include <libtbag/uvpp/Check.hpp>
+#include <libtbag/uvpp/Prepare.hpp>
 
 #include <memory>
 #include <iostream>
 
 using namespace libtbag;
 using namespace libtbag::uvpp;
+
+struct CheckTest : public Check
+{
+    int counter;
+
+    CheckTest(Loop & loop) : Check(loop), counter(0)
+    { /* EMPTY. */ }
+
+    virtual void onCheck() override
+    {
+        ++counter;
+        stop();
+        close();
+    }
+};
+
+struct PrepareTest : public Prepare
+{
+    int counter;
+
+    PrepareTest(Loop & loop) : Prepare(loop), counter(0)
+    { /* EMPTY. */ }
+
+    virtual void onPrepare() override
+    {
+        ++counter;
+        stop();
+        close();
+    }
+};
 
 struct TimerTest : public Timer
 {
@@ -32,14 +64,19 @@ struct TimerTest : public Timer
 
 TEST(TimerTest, Default)
 {
-    std::shared_ptr<TimerTest> timer;
-    std::shared_ptr<Loop> loop;
+    Loop loop;
+    auto prepare = loop.newHandle<PrepareTest>(loop);
+    auto timer   = loop.newHandle<TimerTest>(loop);
+    auto check   = loop.newHandle<CheckTest>(loop);
 
-    loop.reset(new Loop());
-    timer = loop->newHandle<TimerTest>(*loop);
-
+    prepare->start();
     timer->start(0, 1/*millisec*/);
-    loop->run();
+    check->start();
+
+    loop.run();
+
+    ASSERT_EQ(1, prepare->counter);
     ASSERT_EQ(1, timer->counter);
+    ASSERT_EQ(1, check->counter);
 }
 
