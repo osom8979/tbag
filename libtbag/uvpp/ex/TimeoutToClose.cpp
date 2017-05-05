82394bc3fee29c5b8d7aeb578952951bc5200a6c
zer0 <osom8979@gmail.com>
2017-05-05 11:48:19 +0900

    Create TimeoutToClose class.

---
+++ libtbag/uvpp/ex/TimeoutToClose.cpp
@@ -0,0 +1,53 @@
+/**
+ * @file   TimeoutToClose.cpp
+ * @brief  TimeoutToClose class implementation.
+ * @author zer0
+ * @date   2017-05-05
+ */
+
+#include <libtbag/uvpp/ex/TimeoutToClose.hpp>
+#include <libtbag/log/Log.hpp>
+#include <libtbag/uvpp/Loop.hpp>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace uvpp {
+namespace ex   {
+
+TimeoutToClose::TimeoutToClose(Loop & loop, Handle * handle, uint64_t timeout, bool auto_close) : Timer(loop), _handle(handle)
+{
+    _cancel.store(false);
+    _auto_close.store(auto_close);
+}
+
+TimeoutToClose::~TimeoutToClose()
+{
+    // EMPTY.
+}
+
+void TimeoutToClose::onTimer()
+{
+    if (_cancel.load() == false && _handle != nullptr) {
+        _handle->close();
+    }
+
+    if (_auto_close.load()) {
+        stop();
+        close();
+    }
+}
+
+void TimeoutToClose::onClose()
+{
+    // EMPTY.
+}
+
+} // namespace ex
+} // namespace uvpp
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
---
+++ libtbag/uvpp/ex/TimeoutToClose.hpp
@@ -0,0 +1,74 @@
+/**
+ * @file   TimeoutToClose.hpp
+ * @brief  TimeoutToClose class prototype.
+ * @author zer0
+ * @date   2017-05-05
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_UVPP_EX_TIMEOUTTOCLOSE_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_UVPP_EX_TIMEOUTTOCLOSE_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+#include <libtbag/predef.hpp>
+#include <libtbag/uvpp/Timer.hpp>
+
+#include <atomic>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace uvpp {
+namespace ex   {
+
+/**
+ * TimeoutToClose class prototype.
+ *
+ * @author zer0
+ * @date   2017-05-05
+ */
+class TBAG_API TimeoutToClose : public Timer
+{
+public:
+    using AtomicBool = std::atomic_bool;
+
+private:
+    Handle * _handle;
+
+    AtomicBool _cancel;
+    AtomicBool _auto_close;
+
+public:
+    TimeoutToClose(Loop & loop, Handle * handle, uint64_t timeout, bool auto_close = true);
+    virtual ~TimeoutToClose();
+
+public:
+    inline bool isCancel() const TBAG_NOEXCEPT_EXPR(TBAG_NOEXCEPT_EXPR(_cancel.load()))
+    { return _cancel.load(); }
+    inline void cancel(bool flag) TBAG_NOEXCEPT_EXPR(TBAG_NOEXCEPT_EXPR(_cancel.store(true)))
+    { _cancel.store(flag); }
+
+    inline bool isAutoClose() const TBAG_NOEXCEPT_EXPR(TBAG_NOEXCEPT_EXPR(_auto_close.load()))
+    { return _auto_close.load(); }
+    inline void setAutoClose(bool flag) TBAG_NOEXCEPT_EXPR(TBAG_NOEXCEPT_EXPR(_auto_close.store(true)))
+    { _auto_close.store(flag); }
+
+public:
+    virtual void onTimer() override;
+    virtual void onClose() override;
+};
+
+} // namespace ex
+} // namespace uvpp
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_UVPP_EX_TIMEOUTTOCLOSE_HPP__
+
