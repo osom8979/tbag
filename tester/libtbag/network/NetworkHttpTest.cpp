4121cf25193d4a1892dd6d1542fb813d4d43f749
zer0 <osom8979@gmail.com>
2017-05-24 15:32:36 +0900

    Test HttpFilter class.

--- tester/libtbag/network/NetworkHttpTest.cpp
+++ tester/libtbag/network/NetworkHttpTest.cpp
@@ -19,9 +19,9 @@ using namespace libtbag;
 using namespace libtbag::network;
 using namespace libtbag::network::http;
 
-TEST(NetworkHttpTest, TcpHttpClient)
+TEST(NetworkHttpTest, HttpClient)
 {
-    log::SeverityGuard guard;
+    log::SeverityGuard guard(log::TBAG_DEFAULT_LOGGER_NAME, log::INFO_SEVERITY);
 
     HttpResponse response;
     auto result = http::requestWithSync("http://osom8979.github.io", 10000, response);
@@ -29,18 +29,20 @@ TEST(NetworkHttpTest, TcpHttpClient)
     ASSERT_EQ(200, response.status);
 }
 
-TEST(NetworkHttpTest, TcpHttpServer)
+TEST(NetworkHttpTest, HttpServer)
 {
-    log::SeverityGuard guard;
+    log::SeverityGuard guard(log::TBAG_DEFAULT_LOGGER_NAME, log::INFO_SEVERITY);
 
     char const * const TEST_BODY = "<html><head><title>TEST</title></head><body><p>Hello, World!</p></body></html>";
 
     uvpp::Loop loop;
     HttpServer server(loop);
 
-    int on_open    = 0;
-    int on_request = 0;
-    int on_close   = 0;
+    int on_open         = 0;
+    int on_request      = 0;
+    int on_request_doc  = 0;
+    int on_request_down = 0;
+    int on_close        = 0;
 
     server.init(details::ANY_IPV4, 0);
     int const SERVER_PORT = server.getPort();
@@ -49,21 +51,42 @@ TEST(NetworkHttpTest, TcpHttpServer)
     request_url += std::to_string(SERVER_PORT);
     request_url += "/";
 
+    std::string request_url_doc  = request_url + "Documents";
+    std::string request_url_down = request_url + "Downloads";
+
     std::cout << "Request URL: " << request_url << std::endl;
 
     server.setOnOpen([&](HttpServer::WeakClient node){
         std::cout << "Server.OnOpen()\n";
         ++on_open;
     });
+
     server.setOnRequest([&](Err code, HttpParser const & request, HttpBuilder & response, uint64_t & timeout){
         std::cout << "Server.OnRequest()\n";
         ++on_request;
-        request.getMethodName();
         response.setStatus(200);
         response.setReason("OK");
         response.setBody(TEST_BODY);
         timeout = 10000;
     });
+
+    server.setOnRequest("/Documents", [&](Err code, HttpParser const & request, HttpBuilder & response, uint64_t & timeout){
+        std::cout << "Server.OnRequest(/Documents)\n";
+        ++on_request_doc;
+        response.setStatus(200);
+        response.setReason("OK");
+        response.setBody(TEST_BODY);
+        timeout = 10000;
+    });
+    server.setOnRequest("/Downloads", [&](Err code, HttpParser const & request, HttpBuilder & response, uint64_t & timeout){
+        std::cout << "Server.OnRequest(/Downloads)\n";
+        ++on_request_down;
+        response.setStatus(200);
+        response.setReason("OK");
+        response.setBody(TEST_BODY);
+        timeout = 10000;
+    });
+
     server.setOnClose([&](HttpServer::WeakClient node){
         std::cout << "Server.OnClose()\n";
         ++on_close;
@@ -73,15 +96,33 @@ TEST(NetworkHttpTest, TcpHttpServer)
     Err server_result = Err::E_UNKNOWN;
     Err client_result = Err::E_UNKNOWN;
 
-    HttpResponse response;
+    HttpResponse response1;
+    HttpResponse response2;
+    HttpResponse response3;
 
     std::thread server_thread([&](){ server_result = loop.run(); });
-    std::thread client_thread([&](){ client_result = http::requestWithSync(request_url, 10000, response); });
+    std::thread client1_thread([&](){ client_result = http::requestWithSync(request_url     , 10000, response1); });
+    std::thread client2_thread([&](){ client_result = http::requestWithSync(request_url_doc , 10000, response2); });
+    std::thread client3_thread([&](){ client_result = http::requestWithSync(request_url_down, 10000, response3); });
 
-    client_thread.join();
+    client1_thread.join();
+    client2_thread.join();
+    client3_thread.join();
     server_thread.join();
 
-    ASSERT_STREQ(TEST_BODY, response.body.c_str());
-    ASSERT_EQ(200, response.status);
+    ASSERT_STREQ(TEST_BODY, response1.body.c_str());
+    ASSERT_EQ(200, response1.status);
+
+    ASSERT_STREQ(TEST_BODY, response2.body.c_str());
+    ASSERT_EQ(200, response2.status);
+
+    ASSERT_STREQ(TEST_BODY, response3.body.c_str());
+    ASSERT_EQ(200, response3.status);
+
+    ASSERT_EQ(3, on_open        );
+    ASSERT_EQ(1, on_request     );
+    ASSERT_EQ(1, on_request_doc );
+    ASSERT_EQ(1, on_request_down);
+    ASSERT_EQ(3, on_close       );
 }
 
