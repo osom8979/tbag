27a74ece6c6e23714f959fe95e431aa0789d3459
zer0 <osom8979@gmail.com>
2017-06-03 13:20:00 +0900

    Create ServiceApp class.

---
+++ libtbag/app/ex/ServiceApp.cpp
@@ -0,0 +1,361 @@
+/**
+ * @file   ServiceApp.cpp
+ * @brief  ServiceApp class implementation.
+ * @author zer0
+ * @date   2017-06-02
+ */
+
+#include <libtbag/app/ex/ServiceApp.hpp>
+#include <libtbag/container/Global.hpp>
+#include <libtbag/filesystem/Path.hpp>
+#include <libtbag/string/StringUtils.hpp>
+#include <libtbag/string/Environments.hpp>
+#include <libtbag/signal/SignalHandler.hpp>
+#include <libtbag/util/Version.hpp>
+#include <libtbag/log/Log.hpp>
+
+#include <cassert>
+#include <iostream>
+
+#define SERVICE_APP_COMMAND_APP       "app"
+#define SERVICE_APP_COMMAND_INSTALL   "install"
+#define SERVICE_APP_COMMAND_UNINSTALL "uninstall"
+#define SERVICE_APP_COMMAND_START     "start"
+#define SERVICE_APP_COMMAND_STOP      "stop"
+#define SERVICE_APP_COMMAND_REQUEST   "request"
+
+#define SERVICE_APP_OPTIONS_PREFIX    "--"
+#define SERVICE_APP_OPTIONS_DELIMITER "="
+
+#define SERVICE_APP_COMMAND_PREFIX    ""
+#define SERVICE_APP_COMMAND_DELIMITER "/"
+
+#define SERVICE_APP_OPTIONS_GLOBAL    "global"
+#define SERVICE_APP_OPTIONS_HOME      "home"
+#define SERVICE_APP_OPTIONS_LOCAL     "local"
+#define SERVICE_APP_OPTIONS_CONFIG    "config"
+#define SERVICE_APP_OPTIONS_VERBOSE   "verbose"
+#define SERVICE_APP_OPTIONS_VERSION   "version"
+
+#define SERVICE_APP_ENVIRONMENT_TITLE "TITLE"
+#define SERVICE_APP_COMMAND_HELP_HEAD "Command list:"
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace app {
+namespace ex  {
+
+TBAG_CONSTEXPR static char const * const SERVICE_APP_MAIN_SYNOPSIS = ""
+        "${" SERVICE_APP_ENVIRONMENT_TITLE "} application.\n\n"
+        " Usage: ${" SERVICE_APP_ENVIRONMENT_TITLE "} {options} {command}\n"
+        ""/* -- END -- */;
+
+TBAG_CONSTEXPR static char const * const SERVICE_APP_MAIN_REMARKS = "\n"
+        "Configuration file:\n"
+        "  When reading, the values are read from the system,\n"
+        "  global and repository local configuration files by default,\n"
+        "  and options "
+        SERVICE_APP_OPTIONS_PREFIX SERVICE_APP_OPTIONS_GLOBAL ", "
+        SERVICE_APP_OPTIONS_PREFIX SERVICE_APP_OPTIONS_HOME   ", "
+        SERVICE_APP_OPTIONS_PREFIX SERVICE_APP_OPTIONS_LOCAL  " and "
+        SERVICE_APP_OPTIONS_PREFIX SERVICE_APP_OPTIONS_CONFIG SERVICE_APP_OPTIONS_DELIMITER "{filename}\n"
+        "  can be used to tell the command to read from only that location\n"
+        ""/* -- END -- */;
+
+ServiceApp::ServiceApp(int argc, char ** argv, char ** envs, bool with_service)
+        : app::Service(argc, argv, envs),
+          _options(SERVICE_APP_OPTIONS_PREFIX, SERVICE_APP_OPTIONS_DELIMITER),
+          _commander(SERVICE_APP_COMMAND_PREFIX, SERVICE_APP_COMMAND_DELIMITER),
+          _enable_help(false), _enable_verbose(false), _enable_unknown(false), _enable_version(false)
+{
+    using namespace libtbag::container;
+    using namespace libtbag::string;
+
+    auto config = Global::getInstance()->insertNewObject<DefaultXmlModel>(GLOBAL_MODEL_OBJECT_KEY);
+    assert(static_cast<bool>(config));
+    _config_path = config->getFilePath(DefaultXmlModel::Scope::EXE).getString();
+}
+
+ServiceApp::~ServiceApp()
+{
+    // EMPTY.
+}
+
+void ServiceApp::installDefault()
+{
+    installDefaultSynopsis();
+    installDefaultRemarks();
+
+    installDefaultCommand();
+    installDefaultOptions();
+
+    installDefaultLogNode();
+    installDefaultServerNode();
+}
+
+void ServiceApp::installDefaultSynopsis()
+{
+    libtbag::string::Environments env;
+    env.push(SERVICE_APP_ENVIRONMENT_TITLE, filesystem::Path::getExePath().getName());
+    installDefaultSynopsis(env.convert(std::string(SERVICE_APP_MAIN_SYNOPSIS)));
+}
+
+void ServiceApp::installDefaultSynopsis(std::string const & synopsis)
+{
+    _options.setSynopsis(synopsis);
+}
+
+void ServiceApp::installDefaultRemarks()
+{
+    installDefaultRemarks(std::string(SERVICE_APP_MAIN_REMARKS));
+}
+
+void ServiceApp::installDefaultRemarks(std::string const & remarks)
+{
+    _commander.setRemarks(remarks);
+}
+
+void ServiceApp::installDefaultCommand()
+{
+    using namespace libtbag::string;
+    _commander.insert(SERVICE_APP_COMMAND_APP, [&](Arguments const & args){
+        _exit_code = onRunning();
+    }, "Normal application mode. [DEFAULT]");
+    _commander.insert(SERVICE_APP_COMMAND_INSTALL, [&](Arguments const & args){
+        _exit_code = (install() == Err::E_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE);
+    }, "Install service.");
+    _commander.insert(SERVICE_APP_COMMAND_UNINSTALL, [&](Arguments const & args){
+        _exit_code = (uninstall() == Err::E_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE);
+    }, "Uninstall service.");
+    _commander.insert(SERVICE_APP_COMMAND_START, [&](Arguments const & args){
+        _exit_code = (start() == Err::E_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE);
+    }, "Start service.");
+    _commander.insert(SERVICE_APP_COMMAND_STOP, [&](Arguments const & args){
+        _exit_code = (stop() == Err::E_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE);
+    }, "Stop service.");
+}
+
+void ServiceApp::installDefaultOptions()
+{
+    using namespace libtbag::string;
+    auto config = getModel().lock();
+    assert(static_cast<bool>(config));
+
+    std::string const GLOBAL_PATH = config->getFilePath(DefaultXmlModel::Scope::GLOBAL).getString();
+    std::string const HOME_PATH   = config->getFilePath(DefaultXmlModel::Scope::HOME  ).getString();
+    std::string const LOCAL_PATH  = config->getFilePath(DefaultXmlModel::Scope::EXE   ).getString();
+
+    _options.insert(SERVICE_APP_OPTIONS_GLOBAL, [&, GLOBAL_PATH](Arguments const & args){
+        _config_path = GLOBAL_PATH;
+    }, std::string("Use the global resource. The path is ") + GLOBAL_PATH);
+    _options.insert(SERVICE_APP_OPTIONS_HOME, [&, HOME_PATH](Arguments const & args){
+        _config_path = HOME_PATH;
+    }, std::string("Use the home resource. The path is ") + HOME_PATH);
+    _options.insert(SERVICE_APP_OPTIONS_LOCAL, [&, LOCAL_PATH](Arguments const & args){
+        _config_path = LOCAL_PATH;
+    }, std::string("Use the local resource. The path is ") + LOCAL_PATH + " [DEFAULT]");
+    _options.insert(SERVICE_APP_OPTIONS_CONFIG, [&](Arguments const & args){
+        if (args.empty() == false) {
+            _config_path = args.get(0);
+        } else {
+            _enable_unknown = true;
+        }
+    }, "Use the given config file.", "{filename}");
+
+    _options.insertHelpCommand([&](Arguments const & args){
+        _enable_help = true;
+    });
+    _options.insert(SERVICE_APP_OPTIONS_VERBOSE, [&](Arguments const & args){
+        _enable_verbose = true;
+    }, "Be more verbose/talkative during the operation.");
+}
+
+void ServiceApp::installDefaultVersion(int major, int minor, int patch)
+{
+    using namespace libtbag::string;
+    _version.set(static_cast<uint32_t>(major), static_cast<uint32_t>(minor), static_cast<uint32_t>(patch));
+    _options.insert(SERVICE_APP_OPTIONS_VERSION, [&](Arguments const & args){
+        _enable_version = true;
+    }, "print the version number and exit.");
+}
+
+void ServiceApp::installDefaultLogNode()
+{
+    installDefaultLogNode(std::string(log::TBAG_DEFAULT_LOGGER_NAME));
+}
+
+void ServiceApp::installDefaultLogNode(std::string const & logger_name)
+{
+    using namespace libtbag::container;
+    auto config = getModel().lock();
+    assert(static_cast<bool>(config));
+    config->newAdd<DefaultLogXmlNode>(logger_name);
+}
+
+void ServiceApp::installDefaultServerNode()
+{
+    using namespace libtbag::container;
+    auto config = getModel().lock();
+    assert(static_cast<bool>(config));
+    config->newAdd<ServerXmlNode>();
+}
+
+void ServiceApp::installDefaultServerNode(std::string const & var, std::string const & ip, int port)
+{
+    using namespace libtbag::container;
+    auto config = getModel().lock();
+    assert(static_cast<bool>(config));
+    config->newAdd<ServerXmlNode>(var, ip, port);
+}
+
+bool ServiceApp::loadOrDefaultSaveConfig()
+{
+    return loadOrDefaultSaveConfig(_config_path);
+}
+
+bool ServiceApp::loadOrDefaultSaveConfig(std::string const & path)
+{
+    auto config = getModel().lock();
+    assert(static_cast<bool>(config));
+
+    auto const PATH = filesystem::Path(path);
+    if (PATH.exists() == false) {
+        std::cerr << "Not found config file: " << path << std::endl;
+        return false;
+    }
+    if (PATH.isReadable() == false) {
+        std::cerr << "Could not read a config file: " << path << std::endl;
+        return false;
+    }
+
+    if (config->loadOrDefaultSave(PATH) == false) {
+        std::cerr << "Load or save failed.\n";
+        return false;
+    }
+
+    if (_enable_verbose) {
+        std::cout << "Load or save config file: " << path << std::endl;
+    }
+    return true;
+}
+
+bool ServiceApp::createLoggers()
+{
+    auto log = getLog().lock();
+    if (static_cast<bool>(log) == false) {
+        return false;
+    }
+
+    if (log->createLoggers() >= 1) {
+        auto const NAMES = log->getNames();
+        std::size_t const NAMES_SIZE = NAMES.size();
+        assert(NAMES_SIZE >= 1);
+
+        std::stringstream ss;
+        ss << NAMES[0];
+        for (std::size_t i = 1; i < NAMES_SIZE; ++i) {
+            ss << "," << NAMES[i];
+        }
+
+        if (_enable_verbose) {
+            std::cout << "Create loggers: " << ss.str() << std::endl;
+        }
+    }
+
+    return true;
+}
+
+void ServiceApp::registerDefaultSignalHandler()
+{
+    libtbag::signal::registerDefaultStdTerminateHandler();
+    libtbag::signal::registerDefaultHandler();
+}
+
+void ServiceApp::registerDefaultSignalHandler(std::string const & logger)
+{
+    libtbag::signal::registerDefaultStdTerminateHandler(logger);
+    libtbag::signal::registerDefaultHandler(logger);
+}
+
+int ServiceApp::run()
+{
+    using namespace libtbag::string;
+
+    StringVector cmds;
+    _options.setDefaultCallback([&](Arguments const & args){
+        if (args.empty() == false) {
+            cmds.push_back(libtbag::string::lower(args.get(0)));
+        }
+    });
+
+    bool const IGNORE_FIRST = true;
+    _options.request(getArgc(), getArgv(), IGNORE_FIRST);
+
+    if (onCreate() == false) {
+        std::cerr << "Create failed.\n";
+        return EXIT_FAILURE;
+    }
+
+    if (loadOrDefaultSaveConfig() && onLoadConfig()) {
+        if (_enable_help || _enable_unknown) {
+            std::cout << _commander.help() << std::endl;
+            _exit_code = EXIT_FAILURE;
+        } else if (_enable_version) {
+            std::cout << _version.toString() << std::endl;
+            _exit_code = EXIT_SUCCESS;
+        } else {
+            char const SPACE = ' ';
+            std::stringstream ss;
+            for (auto & cmd : cmds) {
+                ss << cmd << SPACE;
+            }
+
+            _commander.setCallOnce();
+            std::size_t const CALL_COUNT = _commander.request(ss.str());
+            if (CALL_COUNT == 0U) {
+                onDefaultCommand(cmds);
+            }
+        }
+    } else {
+        _exit_code = EXIT_FAILURE;
+    }
+
+    onDestroy();
+    return _exit_code;
+}
+
+// ---------------
+// Static methods.
+// ---------------
+
+ServiceApp::WeakModel ServiceApp::getModel()
+{
+    using namespace libtbag::container;
+    return Global::getInstance()->find<DefaultXmlModel>(GLOBAL_MODEL_OBJECT_KEY);
+}
+
+ServiceApp::WeakLog ServiceApp::getLog()
+{
+    if (auto config = getModel().lock()) {
+        return config->getWeak<DefaultLogXmlNode>();
+    }
+    return WeakLog();
+}
+
+ServiceApp::WeakServer ServiceApp::getServer()
+{
+    if (auto config = getModel().lock()) {
+        return config->getWeak<ServerXmlNode>();
+    }
+    return WeakServer();
+}
+
+} // namespace ex
+} // namespace app
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
---
+++ libtbag/app/ex/ServiceApp.hpp
@@ -0,0 +1,144 @@
+/**
+ * @file   ServiceApp.hpp
+ * @brief  ServiceApp class prototype.
+ * @author zer0
+ * @date   2017-06-02
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_APP_EX_SERVICEAPP_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_APP_EX_SERVICEAPP_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+#include <libtbag/predef.hpp>
+#include <libtbag/Err.hpp>
+
+#include <libtbag/app/Service.hpp>
+#include <libtbag/string/HelpCommander.hpp>
+#include <libtbag/dom/DefaultXmlModel.hpp>
+#include <libtbag/dom/node/ServerXmlNode.hpp>
+#include <libtbag/log/node/DefaultLogXmlNode.hpp>
+
+#include <memory>
+#include <vector>
+#include <string>
+#include <libtbag/util/Version.hpp>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace app {
+namespace ex  {
+
+/**
+ * ServiceApp class prototype.
+ *
+ * @author zer0
+ * @date   2017-06-02
+ */
+class TBAG_API ServiceApp : private Service
+{
+public:
+    using Version = util::Version;
+
+    using HelpCommander     = string::HelpCommander;
+    using DefaultXmlModel   = dom::DefaultXmlModel;
+    using DefaultLogXmlNode = log::node::DefaultLogXmlNode;
+    using ServerXmlNode     = dom::node::ServerXmlNode;
+
+    using WeakModel  = std::weak_ptr<DefaultXmlModel>;
+    using WeakLog    = std::weak_ptr<DefaultLogXmlNode>;
+    using WeakServer = std::weak_ptr<ServerXmlNode>;
+
+    using StringVector = std::vector<std::string>;
+
+public:
+    enum class RunningMode
+    {
+        RUN_UNKNOWN,
+        RUN_APPLICATION,
+        RUN_INSTALL,
+        RUN_UNINSTALL,
+        RUN_START,
+        RUN_STOP,
+    };
+
+public:
+    TBAG_CONSTEXPR static char const * const GLOBAL_MODEL_OBJECT_KEY
+            = "libtbag.app.ex.ServiceApp.DefaultXmlModel";
+
+private:
+    HelpCommander _options;
+    HelpCommander _commander;
+
+    std::string _config_path;
+    Version _version;
+
+private:
+    bool _enable_help;
+    bool _enable_verbose;
+    bool _enable_unknown;
+    bool _enable_version;
+
+private:
+    int _exit_code;
+
+public:
+    ServiceApp(int argc, char ** argv, char ** envs, bool with_service = false);
+    virtual ~ServiceApp();
+
+protected:
+    void installDefault();
+
+    void installDefaultSynopsis();
+    void installDefaultSynopsis(std::string const & synopsis);
+
+    void installDefaultRemarks();
+    void installDefaultRemarks(std::string const & remarks);
+
+    void installDefaultCommand();
+    void installDefaultOptions();
+    void installDefaultVersion(int major, int minor = 0, int patch = 0);
+
+    void installDefaultLogNode();
+    void installDefaultLogNode(std::string const & logger_name);
+    void installDefaultServerNode();
+    void installDefaultServerNode(std::string const & var, std::string const & ip, int port);
+
+private:
+    bool loadOrDefaultSaveConfig();
+    bool loadOrDefaultSaveConfig(std::string const & path);
+
+protected:
+    bool createLoggers();
+    void registerDefaultSignalHandler();
+    void registerDefaultSignalHandler(std::string const & logger);
+
+public:
+    int run();
+
+// Static methods.
+public:
+    static WeakModel  getModel();
+    static WeakLog    getLog();
+    static WeakServer getServer();
+
+public:
+    virtual int onLoadConfig() { return 0; }
+    virtual int onDefaultCommand(StringVector const & args) { return 0; }
+};
+
+} // namespace ex
+} // namespace app
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_APP_EX_SERVICEAPP_HPP__
+
