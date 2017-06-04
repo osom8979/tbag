00fd49fad51235e6fc54845fd00f31efd8b49cb0
zer0 <osom8979@gmail.com>
2017-06-04 13:30:42 +0900

    Add a MDB error code.

--- libtbag/Err.cpp
+++ libtbag/Err.cpp
@@ -9,6 +9,7 @@
 #include <libtbag/Err.hpp>
 #include <libtbag/log/Log.hpp>
 #include <uv.h>
+#include <lmdb.h>
 
 // -------------------
 NAMESPACE_LIBTBAG_OPEN
@@ -18,7 +19,7 @@ char const * getErrName(Err code) TBAG_NOEXCEPT
 {
     switch (code) {
 #define _TBAG_XX(name, message) case Err::E##name: return "E"#name;
-    TBAG_ERROR_INFO_MAP(_TBAG_XX, _TBAG_XX)
+    TBAG_ERROR_INFO_MAP(_TBAG_XX, _TBAG_XX, _TBAG_XX)
 #undef _TBAG_XX
     default: return "E_UNKNOWN";
     }
@@ -28,7 +29,7 @@ char const * getErrDetail(Err code) TBAG_NOEXCEPT
 {
     switch (code) {
 #define _TBAG_XX(name, message) case Err::E##name: return message;
-    TBAG_ERROR_INFO_MAP(_TBAG_XX, _TBAG_XX)
+    TBAG_ERROR_INFO_MAP(_TBAG_XX, _TBAG_XX, _TBAG_XX)
 #undef _TBAG_XX
     default: return "Unknown error.";
     }
@@ -41,7 +42,7 @@ Err convertUvErrorToErr(int uv_error_code) TBAG_NOEXCEPT
     case 0: return Err::E_SUCCESS;
 #define _TBAG_ERASE_XX(name, msg)
 #define _TBAG_XX(name, msg) case UV##name: return Err::E##name;
-    TBAG_ERROR_INFO_MAP(_TBAG_ERASE_XX, _TBAG_XX)
+    TBAG_ERROR_INFO_MAP(_TBAG_ERASE_XX, _TBAG_XX, _TBAG_ERASE_XX)
 #undef _TBAG_ERASE_XX
 #undef _TBAG_XX
     default: return Err::E_UNKNOWN;
@@ -52,8 +53,32 @@ Err convertUvErrorToErr(int uv_error_code) TBAG_NOEXCEPT
 Err convertUvErrorToErrWithLogging(char const * prefix, int uv_error_code)
 {
     if (uv_error_code != 0) {
-        tDLogE("{} error [{}] {}", prefix, uv_error_code, getUvErrorName(uv_error_code));
+        tDLogE("uv {} error [{}] {}", prefix, uv_error_code, getUvErrorName(uv_error_code));
         return convertUvErrorToErr(uv_error_code);
+    }
+    return Err::E_SUCCESS;
+}
+
+Err convertMdbErrorToErr(int mdb_error_code) TBAG_NOEXCEPT
+{
+    // @formatter:off
+    switch (mdb_error_code) {
+    case 0: return Err::E_SUCCESS;
+#define _TBAG_ERASE_XX(name, msg)
+#define _TBAG_XX(name, msg) case MDB##name: return Err::E##name;
+    TBAG_ERROR_INFO_MAP(_TBAG_ERASE_XX, _TBAG_ERASE_XX, _TBAG_XX)
+#undef _TBAG_ERASE_XX
+#undef _TBAG_XX
+    default: return Err::E_UNKNOWN;
+    }
+    // @formatter:on
+}
+
+Err convertMdbErrorToErrWithLogging(char const * prefix, int mdb_error_code)
+{
+    if (mdb_error_code != MDB_SUCCESS) {
+        tDLogE("mdb {} error [{}] {}", prefix, mdb_error_code, getUvErrorName(mdb_error_code));
+        return convertMdbErrorToErr(mdb_error_code);
     }
     return Err::E_SUCCESS;
 }
