f53b0ff4d03e245e7f90dde1a642d4df7ee5d2d7
zer0 <osom8979@gmail.com>
2017-06-08 17:00:36 +0900

    Create TraceDyMem package.

---
+++ libtbag/debug/TraceDyMem.hpp
@@ -0,0 +1,63 @@
+/**
+ * @file   TraceDyMem.hpp
+ * @brief  TraceDyMem class prototype.
+ * @author zer0
+ * @date   2017-06-08
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_DEBUG_TRACEDYMEM_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_DEBUG_TRACEDYMEM_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+#include <libtbag/predef.hpp>
+#include <cstddef>
+
+#ifndef TBAG_MEMORY_TRACE_LEVEL
+#define TBAG_MEMORY_TRACE_LEVEL 0
+#endif
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace debug {
+
+TBAG_CONSTEXPR int const TBAG_MEMORY_LEVEL0 = 0;
+TBAG_CONSTEXPR int const TBAG_MEMORY_LEVEL1 = 1;
+TBAG_CONSTEXPR int const TBAG_MEMORY_LEVEL2 = 2;
+
+TBAG_CONSTEXPR int const TBAG_MEMORY_NO_TRACE     = TBAG_MEMORY_LEVEL0;
+TBAG_CONSTEXPR int const TBAG_MEMORY_SIMPLE_TRACE = TBAG_MEMORY_LEVEL1;
+TBAG_CONSTEXPR int const TBAG_MEMORY_STACK_TRACE  = TBAG_MEMORY_LEVEL2;
+
+/** Memory trace level. */
+TBAG_CONSTEXPR int getTraceLevel()
+{
+    return TBAG_MEMORY_TRACE_LEVEL;
+}
+
+/** Allocate and zero-initialize array. */
+void * calloc(std::size_t num, std::size_t size);
+
+/** Deallocate memory block. */
+void free(void * ptr);
+
+/** Allocate memory block. */
+void * malloc(std::size_t size);
+
+/** Reallocate memory block. */
+void * realloc(void * ptr, std::size_t size);
+
+} // namespace debug
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_DEBUG_TRACEDYMEM_HPP__
+
