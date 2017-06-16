30d27276edc60bb5bdcb7219768e67a67b5d9ab4
zer0 <osom8979@gmail.com>
2017-06-17 08:14:01 +0900

    Restore some methods in UdpNode class.

--- libtbag/network/udp/UdpNode.cpp
+++ libtbag/network/udp/UdpNode.cpp
@@ -6,8 +6,9 @@
  */
 
 #include <libtbag/network/udp/UdpNode.hpp>
+#include <libtbag/debug/Assert.hpp>
 #include <libtbag/log/Log.hpp>
-#include "UdpNode.hpp"
+#include <libtbag/Noncopyable.hpp>
 
 // -------------------
 NAMESPACE_LIBTBAG_OPEN
@@ -16,14 +17,71 @@ NAMESPACE_LIBTBAG_OPEN
 namespace network {
 namespace udp     {
 
-UdpNode::UdpNode()
+/**
+ * Udp internal helper class.
+ *
+ * @author zer0
+ * @date   2017-06-17
+ *
+ * @warning
+ *  Don't mutex guard in this class. (However, callbacks are an exception)
+ */
+struct UdpNode::Internal : private Noncopyable
 {
-    // EMPTY.
+    using binf = UdpNode::binf;
+
+    UdpNode * _parent;
+
+    Internal(UdpNode * parent) : _parent(parent)
+    {
+        // EMPTY.
+    }
+
+    ~Internal()
+    {
+        // EMPTY.
+    }
+};
+
+// -----------------------
+// UdpNode implementation.
+// -----------------------
+
+UdpNode::UdpNode(Loop & loop, NodeType type, SharedSafetyAsync async, ServerInterface * parent)
+        : NODE_TYPE(type), _owner_async(false), _internal(new Internal(this))
+{
+    _client = loop.newHandle<UdpNodeBackend>(loop, this);
+    assert(static_cast<bool>(_client));
+
+    if (static_cast<bool>(async) && loop.id() == async->getLoop()->id()) {
+        _owner_async = false;
+        _async = async;
+    } else {
+        _owner_async = true;
+        _async.reset();
+    }
+
+    _sender.status = SendStatus::SS_NOT_READY;
 }
 
 UdpNode::~UdpNode()
 {
     // EMPTY.
+}
+
+char const * UdpNode::getSendStatusName(SendStatus status) TBAG_NOEXCEPT
+{
+    switch (status) {
+    case SendStatus::SS_NOT_READY:     return "NOT_READY";
+    case SendStatus::SS_READY:         return "READY";
+    case SendStatus::SS_ASYNC:         return "ASYNC";
+    case SendStatus::SS_ASYNC_CANCEL:  return "ASYNC_CANCEL";
+    case SendStatus::SS_SEND:          return "SEND";
+    case SendStatus::SS_END:           return "END";
+    }
+
+    TBAG_INACCESSIBLE_BLOCK_ASSERT();
+    return "UNKNOWN";
 }
 
 } // namespace udp
--- libtbag/network/udp/UdpNode.hpp
+++ libtbag/network/udp/UdpNode.hpp
@@ -22,6 +22,7 @@
 #include <libtbag/network/details/FunctionalNet.hpp>
 #include <libtbag/network/udp/UdpNodeBackend.hpp>
 #include <libtbag/uvpp/ex/SafetyAsync.hpp>
+#include <libtbag/uvpp/func/FunctionalTimer.hpp>
 #include <libtbag/uvpp/Request.hpp>
 
 #include <string>
@@ -45,8 +46,136 @@ namespace udp     {
 class TBAG_API UdpNode : public details::ClientInterface
 {
 public:
-    UdpNode();
+    using ReadPacket = details::ReadPacket;
+    using ServerInterface = details::ServerInterface;
+
+    using Loop = uvpp::Loop;
+    using Udp  = uvpp::Udp;
+    using binf = uvpp::binf;
+
+    using UdpSendRequest = uvpp::UdpSendRequest;
+
+public:
+    using SharedClientBackend = std::shared_ptr<UdpNodeBackend>;
+    using   WeakClientBackend =   std::weak_ptr<UdpNodeBackend>;
+
+public:
+    using       SafetyAsync = uvpp::ex::SafetyAsync;
+    using SharedSafetyAsync = std::shared_ptr<SafetyAsync>;
+    using   WeakSafetyAsync = std::shared_ptr<SafetyAsync>;
+
+public:
+    using       FuncTimer = uvpp::func::FuncTimer;
+    using SharedFuncTimer = std::shared_ptr<FuncTimer>;
+    using   WeakFuncTimer = std::shared_ptr<FuncTimer>;
+
+public:
+    using Id      = id::Id;
+    using Buffer  = std::vector<char>;
+    using Buffers = std::vector<Buffer>;
+
+    using Mutex = std::mutex;
+    using Guard = std::lock_guard<Mutex>;
+
+public:
+    enum class NodeType
+    {
+        NT_RECV,
+        NT_SEND,
+    };
+
+    enum class SendStatus
+    {
+        SS_NOT_READY,
+        SS_READY,           ///< Next: call send.
+        SS_ASYNC,           ///< Next: onTimeout or onAsync.
+        SS_ASYNC_CANCEL,    ///< Next: onAsync.
+        SS_SEND,            ///< Next: onTimeout or onWrite.
+        SS_END,
+    };
+
+public:
+    struct Internal;
+    friend struct Internal;
+
+public:
+    using UniqueInternal = std::unique_ptr<Internal>;
+
+private:
+    NodeType const NODE_TYPE;
+    bool _owner_async;
+
+private:
+    UniqueInternal       _internal;
+    SharedClientBackend  _client;
+    SharedSafetyAsync    _async;
+    SharedFuncTimer      _close_timer;
+
+private:
+    union {
+        struct sockaddr_in  ipv4;
+        struct sockaddr_in6 ipv6;
+    } _addr;
+
+private:
+    mutable Mutex _mutex;
+
+    struct {
+        SendStatus     status;
+        UdpSendRequest send_req;
+        Buffers        buffers;
+    } _sender;
+
+public:
+    UdpNode(Loop & loop, NodeType type, SharedSafetyAsync async, ServerInterface * parent);
     virtual ~UdpNode();
+
+public:
+    WeakClientBackend getClient() { Guard g(_mutex); return WeakClientBackend(_client); }
+    WeakSafetyAsync   getAsync () { Guard g(_mutex); return WeakSafetyAsync(_async); }
+
+private:
+    static char const * getSendStatusName(SendStatus status) TBAG_NOEXCEPT;
+
+public:
+    inline SendStatus getSendStatus() const TBAG_NOEXCEPT
+    { return _sender.status; }
+    inline char const * getSendStatusName() const TBAG_NOEXCEPT
+    { return getSendStatusName(_sender.status); }
+
+//private:
+//    void onAsyncSend();
+//
+//public:
+//    virtual Id          id   () const override;
+//    virtual std::string dest () const override;
+//    virtual int         port () const override;
+//    virtual void *      udata() override;
+//
+//public:
+//    virtual Err  init  (char const * destination, int port = 0, uint64_t millisec = 0) override;
+//    virtual Err  start () override;
+//    virtual Err  stop  () override;
+//    virtual void close () override;
+//    virtual void cancel() override;
+//    virtual Err  write (binf const * buffer, std::size_t size, uint64_t millisec = 0) override;
+//    virtual Err  write (char const * buffer, std::size_t size, uint64_t millisec = 0) override;
+//
+//// Event backend.
+//public:
+//    virtual void backConnect(Err code) override;
+//    virtual void backShutdown(Err code) override;
+//    virtual void backWrite(Err code) override;
+//    virtual void backRead(Err code, ReadPacket const & packet) override;
+//    virtual void backClose() override;
+
+public:
+    template <typename Predicated>
+    void updateUserData(Predicated predicated)
+    {
+        Guard guard(_mutex);
+        predicated(_client->getUserData());
+    }
 };
 
 using FunctionalUdpNode = details::FunctionalClient<UdpNode>;
