fd9d432782406a375e491122347c0d1f5fcb6a36
zer0 <osom8979@gmail.com>
2017-07-31 09:58:25 +0900

    Restore LuaMachine class from af218439^

---
+++ libtbag/script/LuaMachine.cpp
@@ -0,0 +1,127 @@
+/**
+ * @file   LuaMachine.cpp
+ * @brief  LuaMachine class implementation.
+ * @author zer0
+ * @date   2016-07-06
+ * @date   2017-07-31 (Restore this file)
+ */
+
+#include <libtbag/script/LuaMachine.hpp>
+
+#include <cassert>
+#include <string>
+#include <algorithm>
+
+#include <lua.hpp>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace script {
+
+template <typename T>
+inline lua_State * lua_state_cast(T * object)
+{
+    return static_cast<lua_State*>(object);
+}
+
+LuaMachine::LuaMachine() : _machine(nullptr)
+{
+    // EMPTY.
+}
+
+LuaMachine::~LuaMachine()
+{
+    this->release();
+}
+
+bool LuaMachine::initialize()
+{
+    _machine = static_cast<FakeLuaState*>(luaL_newstate());
+    if (_machine != nullptr) {
+        luaL_openlibs(lua_state_cast(_machine));
+    }
+
+    return this->isReady();
+}
+
+void LuaMachine::release()
+{
+    if (this->isReady()) {
+        lua_close(lua_state_cast(_machine));
+        _machine = nullptr;
+    }
+}
+
+bool LuaMachine::load(std::string const & path)
+{
+    if (this->isReady() == false) {
+        return false;
+    }
+
+    if (luaL_loadfile(lua_state_cast(_machine), path.c_str()) != 0) {
+        // maybe LUA_ERRFILE
+        return false;
+    }
+
+    lua_pcall(lua_state_cast(_machine), 0, 0, 0);
+    return true;
+}
+
+int LuaMachine::getStackSize() const
+{
+    return lua_gettop(lua_state_cast(_machine));
+}
+
+void LuaMachine::pop(int size)
+{
+    lua_pop(lua_state_cast(_machine), size);
+}
+
+void LuaMachine::push()
+{
+    lua_pushnil(lua_state_cast(_machine));
+}
+
+void LuaMachine::push(bool value)
+{
+    lua_pushboolean(lua_state_cast(_machine), value);
+}
+
+void LuaMachine::push(int value)
+{
+    lua_pushinteger(lua_state_cast(_machine), value);
+}
+
+void LuaMachine::push(double value)
+{
+    lua_pushnumber(lua_state_cast(_machine), value);
+}
+
+void LuaMachine::push(std::string const & value)
+{
+    lua_pushstring(lua_state_cast(_machine), value.c_str());
+}
+
+std::string LuaMachine::getLuaString(std::string const & name)
+{
+    lua_pushstring(lua_state_cast(_machine), name.c_str());
+    lua_rawget(lua_state_cast(_machine), LUA_REGISTRYINDEX);
+
+    if (lua_isstring(lua_state_cast(_machine), -1)) {
+        assert(false);
+        return std::string();
+    }
+
+    std::string result = lua_tostring(lua_state_cast(_machine), -1);
+    lua_pop(lua_state_cast(_machine), 1);
+    return result;
+}
+
+} // namespace script
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
---
+++ libtbag/script/LuaMachine.hpp
@@ -0,0 +1,101 @@
+/**
+ * @file   LuaMachine.hpp
+ * @brief  LuaMachine class prototype.
+ * @author zer0
+ * @date   2016-04-26
+ * @date   2017-07-31 (Restore this file)
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_SCRIPT_LUAMACHINE_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_SCRIPT_LUAMACHINE_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+#include <libtbag/predef.hpp>
+#include <libtbag/Noncopyable.hpp>
+
+#include <string>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace script {
+
+/**
+ * LuaMachine class prototype.
+ *
+ * @author zer0
+ * @date   2016-04-26
+ * @date   2017-07-31 (Restore this class)
+ */
+class TBAG_API LuaMachine : private Noncopyable
+{
+public:
+    using FakeLuaState = void;
+
+public:
+    enum class LuaType : int
+    {
+        NIL = 0,
+        BOOLEAN,
+        FUNCTION,
+        NUMBER,
+        STRING,
+        TABLE,
+        THREAD,
+        USERDATA,
+    };
+
+private:
+    FakeLuaState * _machine;
+
+public:
+    LuaMachine();
+    ~LuaMachine();
+
+public:
+    inline bool isReady() const
+    {
+        return (_machine != nullptr);
+    }
+
+public:
+    bool initialize();
+    void release();
+
+// File operators.
+public:
+    bool load(std::string const & path);
+
+// Stack operators.
+public:
+    int getStackSize() const;
+
+public:
+    void pop(int size);
+
+public:
+    void push();
+    void push(bool value);
+    void push(int value);
+    void push(double value);
+    void push(std::string const & value);
+
+// Variables.
+public:
+    std::string getLuaString(std::string const & name);
+};
+
+} // namespace script
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_SCRIPT_LUAMACHINE_HPP__
+
