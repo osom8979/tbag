b59f48fc9b73763897ed5a2d4eb47b6f5828c158
zer0 <osom8979@gmail.com>
2017-08-04 08:57:48 +0900

    Remove ProcessManagerTest.Default tester.

--- tester/libtbag/process/ProcessManagerTest.cpp
+++ tester/libtbag/process/ProcessManagerTest.cpp
@@ -17,42 +17,6 @@ using namespace libtbag::uvpp;
 
 TEST(ProcessManagerTest, Default)
 {
-    using namespace libtbag::filesystem;
-    auto const TBPROC_EXE = Path().getExeDir() / getExecutableName("tbproc");
-    auto const TBPROC_TXT = Path().getExeDir() / "tbproc.txt";
-
-    char const * const WRITE_BODY = "TEMP_BODY";
-
-    ASSERT_TRUE(TBPROC_EXE.exists());
-    if (TBPROC_TXT.exists()) {
-        ASSERT_TRUE(TBPROC_TXT.remove());
-    }
-    ASSERT_FALSE(TBPROC_TXT.exists());
-
-    int64_t result_exit_status = 0;
-    int result_term_signal = 0;
-
-    Loop loop;
-    ProcessManager mgr;
-    Process::Options options;
-    options.setCurrentWorking();
-    options.setFile(TBPROC_EXE);
-    options.appendArgument("file");
-    options.appendArgument(WRITE_BODY);
-    int pid = mgr.exec(loop, options);
-    ASSERT_LT(0, pid);
-
-    auto shared = mgr.get(pid).lock();
-    ASSERT_TRUE(static_cast<bool>(shared));
-
-    ASSERT_EQ(Err::E_SUCCESS, loop.run());
-    ASSERT_TRUE(shared->isExit());
-    ASSERT_EQ(0, shared->getExitStatus());
-    ASSERT_EQ(0, shared->getTermSignal());
-    ASSERT_TRUE(TBPROC_TXT.exists());
-
-    std::string buffer;
-    ASSERT_EQ(Err::E_SUCCESS, readFile(TBPROC_TXT, buffer));
-    ASSERT_EQ(std::string(WRITE_BODY), buffer);
+    // TODO: Preparing to test...
 }
 
