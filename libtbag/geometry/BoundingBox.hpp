3f65f1fc591c756d610e093c4c0c89ed9656c678
zer0 <osom8979@gmail.com>
2017-09-20 11:04:47 +0900

    Create getBoundingBox() method.

---
+++ libtbag/geometry/BoundingBox.hpp
@@ -0,0 +1,58 @@
+/**
+ * @file   BoundingBox.hpp
+ * @brief  BoundingBox utility prototype.
+ * @author zer0
+ * @date   2017-09-20
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_GEOMETRY_BOUNDINGBOX_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_GEOMETRY_BOUNDINGBOX_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+#include <libtbag/predef.hpp>
+#include <libtbag/geometry/Point.hpp>
+#include <libtbag/geometry/Rect.hpp>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace geometry {
+
+template <typename T>
+BaseRect<typename remove_cr<T>::type>
+getBoundingBox(BasePoint<T> const * points, std::size_t size) TBAG_NOEXCEPT
+{
+    typedef typename remove_cr<T>::type __point_tparam;
+    if (points == nullptr || size <= 0) {
+        return makeRect<__point_tparam>(0, 0, 0, 0);
+    }
+
+    BasePoint<__point_tparam> lt = *points;
+    BasePoint<__point_tparam> rd = *points;
+
+    for (std::size_t i = 1; i < size; ++i) {
+        // @formatter:off
+        if ((points + i)->x < lt.x) { lt.x = (points + i)->x; }
+        if ((points + i)->y < lt.y) { lt.y = (points + i)->y; }
+        if ((points + i)->x > rd.x) { rd.x = (points + i)->x; }
+        if ((points + i)->y > rd.y) { rd.y = (points + i)->y; }
+        // @formatter:on
+    }
+
+    return makeRect(lt, rd);
+}
+
+} // namespace geometry
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_GEOMETRY_BOUNDINGBOX_HPP__
+
---
+++ tester/libtbag/geometry/BoundingBoxTest.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file   BoundingBoxTest.cpp
+ * @brief  BoundingBox tester.
+ * @author zer0
+ * @date   2017-09-20
+ */
+
+#include <gtest/gtest.h>
+#include <libtbag/geometry/BoundingBox.hpp>
+
+using namespace libtbag;
+using namespace libtbag::geometry;
+
+TEST(BoundingBoxTest, Default)
+{
+    /*
+     *  0 |  10  20  30  40  50
+     * ---+--------------------
+     *    |
+     * 10 |   *-----------+
+     *    |   |       *   |
+     * 20 |   |           *
+     *    |   |           |
+     * 30 |   |   *       |
+     *    |   |           |
+     * 40 |   +-*---------+
+     *    |
+     * 50 |
+     */
+    std::vector<Point> points;
+    points.push_back(makePoint(10, 10));
+    points.push_back(makePoint(30, 15));
+    points.push_back(makePoint(40, 20));
+    points.push_back(makePoint(20, 30));
+    points.push_back(makePoint(15, 40));
+    auto rect = getBoundingBox(points.data(), points.size());
+
+    ASSERT_EQ(10, rect.x);
+    ASSERT_EQ(10, rect.y);
+    ASSERT_EQ(30, rect.width);
+    ASSERT_EQ(30, rect.height);
+}
+
