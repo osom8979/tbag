872f0a307c69caf73516a94de75445bb5b41d6d2
zer0 <osom8979@gmail.com>
2017-09-27 17:07:05 +0900

    Create SafetyClientProperty structure.

--- libtbag/network/stream/client/WriteStateProperty.hpp
+++ libtbag/network/stream/client/WriteStateProperty.hpp
@@ -16,11 +16,19 @@
 #include <libtbag/config.h>
 #include <libtbag/predef.hpp>
 #include <libtbag/Noncopyable.hpp>
+#include <libtbag/Err.hpp>
+
+#include <libtbag/id/Id.hpp>
 #include <libtbag/debug/Assert.hpp>
+#include <libtbag/container/ReuseQueue.hpp>
 
 #include <libtbag/network/details/NetCommon.hpp>
+#include <libtbag/uvpp/ex/SafetyAsync.hpp>
+#include <libtbag/uvpp/Stream.hpp>
+#include <libtbag/uvpp/Timer.hpp>
 #include <libtbag/uvpp/Request.hpp>
 
+#include <memory>
 #include <vector>
 
 // -------------------
@@ -119,24 +127,152 @@ struct WriteInfo : private Noncopyable
     { return isReady() || (isAsync() && buffer.empty() == false); }
 };
 
+struct BasicClientTypes
+{
+    using ClientInterface = details::ClientInterface;
+    using StreamType      = details::StreamType;
+    using ReadPacket      = details::ReadPacket;
+
+    using ConnectRequest  = uvpp::ConnectRequest;
+    using WriteRequest    = uvpp::WriteRequest;
+    using ShutdownRequest = uvpp::ShutdownRequest;
+
+    using Loop   = uvpp::Loop;
+    using Timer  = uvpp::Timer;
+    using Stream = uvpp::Stream;
+    using binf   = uvpp::binf;
+    using Id     = id::Id;
+
+    using Buffer = std::vector<char>;
+    using Mutex  = std::mutex;
+    using Guard  = std::lock_guard<Mutex>;
+
+    using SharedBuffer = std::shared_ptr<Buffer>;
+    using ReuseQueue   = container::ReuseQueue<SharedBuffer>;
+
+    using SafetyAsync  = uvpp::ex::SafetyAsync;
+    using JobInterface = SafetyAsync::JobInterface;
+
+    using SharedStream = std::shared_ptr<Stream>;
+    using WeakStream   = std::weak_ptr<Stream>;
+
+    struct     OnUserTimerCallback { virtual void     onUserTimer() = 0; };
+    struct OnShutdownTimerCallback { virtual void onShutdownTimer() = 0; };
+    struct    OnAsyncWriteCallback { virtual void    onAsyncWrite() = 0; };
+
+    struct UserTimer final : public Timer
+    {
+        OnUserTimerCallback * parent;
+        UserTimer(Loop & loop, OnUserTimerCallback * p) : uvpp::Timer(loop), parent(p) { /* EMPTY. */ }
+        virtual ~UserTimer() { /* EMPTY. */ }
+        virtual void onTimer() override { parent->onUserTimer(); }
+    };
+
+    struct ShutdownTimer final : public Timer
+    {
+        OnShutdownTimerCallback * parent;
+        ShutdownTimer(Loop & loop, OnShutdownTimerCallback * p) : uvpp::Timer(loop), parent(p) { /* EMPTY. */ }
+        virtual ~ShutdownTimer() { /* EMPTY. */ }
+        virtual void onTimer() override { parent->onShutdownTimer(); }
+    };
+
+    struct AsyncWrite final : public JobInterface
+    {
+        OnAsyncWriteCallback * parent;
+        AsyncWrite(OnAsyncWriteCallback * p) : parent(p) { /* EMPTY. */ }
+        virtual ~AsyncWrite() { /* EMPTY. */ }
+        virtual void run() override { parent->onAsyncWrite(); }
+    };
+
+    using SharedSafetyAsync   = std::shared_ptr<SafetyAsync>;
+    using WeakSafetyAsync     = std::weak_ptr<SafetyAsync>;
+
+    using SharedUserTimer     = std::shared_ptr<UserTimer>;
+    using WeakUserTimer       = std::weak_ptr<UserTimer>;
+
+    using SharedShutdownTimer = std::shared_ptr<ShutdownTimer>;
+    using WeakShutdownTimer   = std::weak_ptr<ShutdownTimer>;
+
+    struct UpdateReady { /* EMPTY. */ };
+};
+
 /**
- * WriteStateProperty class prototype.
+ * BasicClientProperty structure.
  *
  * @author zer0
  * @date   2017-09-27
  */
-class TBAG_API WriteStateProperty : private Noncopyable
+struct BasicClientProperty : public BasicClientTypes
 {
-private:
-    // Insert member variables.
+    StreamType stream_type;
 
-public:
-    WriteStateProperty()
+    ClientInterface * interface;
+
+    int max_fail_count; ///< Maximum continuous failure count.
+    int cur_fail_count; ///< Current continuous failure count.
+
+    uint64_t timeout;   ///< Write timeout. (milliseconds)
+    Buffer   buffer;    ///< Write buffer.
+
+    ReuseQueue  queue;
+    std::size_t max_queue_size;
+
+    ConnectRequest    connect_req;
+    WriteRequest        write_req;
+    ShutdownRequest  shutdown_req;
+
+    SharedStream         client_stream;
+    SharedSafetyAsync    safety_async;
+    SharedUserTimer      user_timer;
+    SharedShutdownTimer  shutdown_timer;
+
+    BasicClientProperty() : max_fail_count(details::MAXIMUM_CONTINUOUS_FAILURE_COUNT_OF_WRITE),
+                            cur_fail_count(0), timeout(0), max_queue_size(details::MAXIMUM_WRITE_QUEUE_SIZE)
+    { /* EMPTY. */ }
+    virtual ~BasicClientProperty()
+    { /* EMPTY. */ }
+};
+
+/**
+ * SafetyClientProperty structure.
+ *
+ * @author zer0
+ * @date   2017-09-27
+ */
+struct SafetyClientProperty : public BasicClientProperty
+{
+    using ClientGuard = std::lock_guard<SafetyClientProperty>;
+
+    mutable Mutex mutex;
+
+    SafetyClientProperty()
+    { /* EMPTY. */ }
+    virtual ~SafetyClientProperty()
+    { /* EMPTY. */ }
+
+    inline void     lock() const TBAG_NOEXCEPT_SP_OP(    mutex.lock()) {            mutex.lock(); }
+    inline void   unlock() const TBAG_NOEXCEPT_SP_OP(  mutex.unlock()) {          mutex.unlock(); }
+    inline bool try_lock() const TBAG_NOEXCEPT_SP_OP(mutex.try_lock()) { return mutex.try_lock(); }
+
+    template <typename Predicated>
+    void set(Predicated predicated)
     {
+        Guard const LOCK(mutex);
+        predicated(static_cast<BasicClientProperty&>(*this));
     }
 
-    virtual ~WriteStateProperty()
+    template <typename ReturnType, typename Predicated>
+    ReturnType get(Predicated predicated)
     {
+        Guard const LOCK(mutex);
+        return predicated(static_cast<BasicClientProperty&>(*this));
+    }
+
+    template <typename ReturnType, typename Predicated>
+    ReturnType get(Predicated predicated) const
+    {
+        Guard const LOCK(mutex);
+        return predicated(static_cast<BasicClientProperty const &>(*this));
     }
 };
 
