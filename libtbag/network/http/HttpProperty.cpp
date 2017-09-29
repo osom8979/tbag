8b2d567eb9f320c08163b964c3e566f057a156ea
zer0 <osom8979@gmail.com>
2017-09-29 15:59:50 +0900

    Create HttpMethod package.

--- libtbag/network/http/HttpProperty.cpp
+++ libtbag/network/http/HttpProperty.cpp
@@ -17,44 +17,6 @@ NAMESPACE_LIBTBAG_OPEN
 namespace network {
 namespace http    {
 
-#define _TBAG_XX(num, name, str) static_assert( \
-        static_cast<int>(::http_method::HTTP_##name) == \
-        static_cast<int>(HttpMethod::M_##name), \
-        "Mismatch HTTP Method number.");
-TBAG_HTTP_METHOD_MAP(_TBAG_XX)
-#undef _TBAG_XX
-
-char const * getHttpMethodName(HttpMethod method) TBAG_NOEXCEPT
-{
-    switch (method) {
-#define _TBAG_XX(num, name, str) case HttpMethod::M_##name: return #str;
-    TBAG_HTTP_METHOD_MAP(_TBAG_XX)
-#undef _TBAG_XX
-    default: return "M_UNKNOWN";
-    }
-}
-
-int getHttpMethodNumber(HttpMethod method) TBAG_NOEXCEPT
-{
-    switch (method) {
-#define _TBAG_XX(num, name, str) case HttpMethod::M_##name: return num;
-    TBAG_HTTP_METHOD_MAP(_TBAG_XX)
-#undef _TBAG_XX
-    default: return TBAG_UNKNOWN_HTTP_METHOD;
-    }
-}
-
-HttpMethod getHttpMethod(std::string const & name)
-{
-    std::string const UPPER_NAME = string::upper(name);
-#define _TBAG_XX(num, name, str) \
-    if (UPPER_NAME == getHttpMethodName(HttpMethod::M_##name)) \
-    { return HttpMethod::M_##name; }
-    TBAG_HTTP_METHOD_MAP(_TBAG_XX)
-#undef _TBAG_XX
-    return HttpMethod::M_UNKNOWN;
-}
-
 HttpStatus getHttpStatus(int status) TBAG_NOEXCEPT
 {
     switch (status) {
--- libtbag/network/http/HttpProperty.hpp
+++ libtbag/network/http/HttpProperty.hpp
@@ -21,6 +21,8 @@
 #include <libtbag/network/Uri.hpp>
 #include <libtbag/string/StringUtils.hpp>
 
+#include <libtbag/network/http/common/HttpMethod.hpp>
+
 #include <utility>
 #include <algorithm>
 #include <string>
@@ -33,6 +35,8 @@ NAMESPACE_LIBTBAG_OPEN
 
 namespace network {
 namespace http    {
+
+using namespace libtbag::network::http::common;
 
 // --------------
 // @formatter:off
@@ -90,92 +94,6 @@ TBAG_CONSTEXPR char const * const DEFAULT_VALUE_OF_CONTENT_TYPE      = "text/htm
 TBAG_CONSTEXPR char const * const DEFAULT_VALUE_OF_USER_AGENT        = VALUE_TBAG_SERVER_INFO;
 TBAG_CONSTEXPR char const * const DEFAULT_VALUE_OF_ACCEPT            = "*/*";
 TBAG_CONSTEXPR char const * const DEFAULT_VALUE_OF_TRANSFER_ENCODING = "identity";
-
-// ------------
-// HTTP METHOD.
-// ------------
-
-TBAG_CONSTEXPR int const TBAG_UNKNOWN_HTTP_METHOD = -1;
-
-#ifndef TBAG_HTTP_METHOD_MAP
-#define TBAG_HTTP_METHOD_MAP(_TBAG_XX)      \
-    _TBAG_XX( 0, DELETE,      DELETE)       \
-    _TBAG_XX( 1, GET,         GET)          \
-    _TBAG_XX( 2, HEAD,        HEAD)         \
-    _TBAG_XX( 3, POST,        POST)         \
-    _TBAG_XX( 4, PUT,         PUT)          \
-    /* pathological */                      \
-    _TBAG_XX( 5, CONNECT,     CONNECT)      \
-    _TBAG_XX( 6, OPTIONS,     OPTIONS)      \
-    _TBAG_XX( 7, TRACE,       TRACE)        \
-    /* WebDAV */                            \
-    _TBAG_XX( 8, COPY,        COPY)         \
-    _TBAG_XX( 9, LOCK,        LOCK)         \
-    _TBAG_XX(10, MKCOL,       MKCOL)        \
-    _TBAG_XX(11, MOVE,        MOVE)         \
-    _TBAG_XX(12, PROPFIND,    PROPFIND)     \
-    _TBAG_XX(13, PROPPATCH,   PROPPATCH)    \
-    _TBAG_XX(14, SEARCH,      SEARCH)       \
-    _TBAG_XX(15, UNLOCK,      UNLOCK)       \
-    _TBAG_XX(16, BIND,        BIND)         \
-    _TBAG_XX(17, REBIND,      REBIND)       \
-    _TBAG_XX(18, UNBIND,      UNBIND)       \
-    _TBAG_XX(19, ACL,         ACL)          \
-    /* subversion */                        \
-    _TBAG_XX(20, REPORT,      REPORT)       \
-    _TBAG_XX(21, MKACTIVITY,  MKACTIVITY)   \
-    _TBAG_XX(22, CHECKOUT,    CHECKOUT)     \
-    _TBAG_XX(23, MERGE,       MERGE)        \
-    /* upnp */                              \
-    _TBAG_XX(24, MSEARCH,     M-SEARCH)     \
-    _TBAG_XX(25, NOTIFY,      NOTIFY)       \
-    _TBAG_XX(26, SUBSCRIBE,   SUBSCRIBE)    \
-    _TBAG_XX(27, UNSUBSCRIBE, UNSUBSCRIBE)  \
-    /* RFC-5789 */                          \
-    _TBAG_XX(28, PATCH,       PATCH)        \
-    _TBAG_XX(29, PURGE,       PURGE)        \
-    /* CalDAV */                            \
-    _TBAG_XX(30, MKCALENDAR,  MKCALENDAR)   \
-    /* RFC-2068, section 19.6.1.2 */        \
-    _TBAG_XX(31, LINK,        LINK)         \
-    _TBAG_XX(32, UNLINK,      UNLINK)       \
-    /* EMD */
-#endif
-
-/**
- * List of http method.
- *
- * @author zer0
- * @date 2017-05-25
- */
-enum class HttpMethod : int
-{
-    M_UNKNOWN = -1,
-#define _TBAG_XX(num, name, str) M_##name = num,
-    TBAG_HTTP_METHOD_MAP(_TBAG_XX)
-#undef _TBAG_XX
-};
-
-#define _TBAG_XX(num, name, str)                                      \
-    struct HttpMethod##name                                           \
-    {                                                                 \
-        TBAG_CONSTEXPR static char const * getMethod() TBAG_NOEXCEPT  \
-        {                                                             \
-            return #name;                                             \
-        }                                                             \
-    };                                                                \
-    /* -- END -- */
-    TBAG_HTTP_METHOD_MAP(_TBAG_XX)
-#undef _TBAG_XX
-
-static_assert(static_cast<int>(HttpMethod::M_UNKNOWN) == TBAG_UNKNOWN_HTTP_METHOD,
-              "Unknown HTTP Method number is not equals.");
-
-// @formatter:off
-TBAG_API char const * getHttpMethodName  (HttpMethod method) TBAG_NOEXCEPT;
-TBAG_API int          getHttpMethodNumber(HttpMethod method) TBAG_NOEXCEPT;
-TBAG_API HttpMethod   getHttpMethod(std::string const & name);
-// @formatter:on
 
 // HTTP Status Code Definitions.
 // Reference: https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
