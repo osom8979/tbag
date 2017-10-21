a6b693692500c86294bc5bc0a355cc3e46b79427
zer0 <osom8979@gmail.com>
2017-10-21 10:53:16 +0900

    Trivial commit.

--- tbag.loc.sh
+++ tbag.loc.sh
@@ -2,5 +2,12 @@
 
 echo 'Line of count in tbag project.'
 
-find . -name "*.cpp" -o -name "*.hpp" -o -name "*.c" -o -name "*.h" -o -name "*.py" -o -name "*.cmake" | grep -v dep/ | grep -v build/ | grep -v 3rd/ | egrep -v "cmake-build-.*/" | grep -v external/ | xargs wc -l | tail -1
+find . -name "*.cpp" -o -name "*.hpp" -o -name "*.c" -o -name "*.h" -o -name "*.py" -o -name "*.cmake" | \
+    grep -v dep/        | \
+    grep -v build/      | \
+    grep -v 3rd/        | \
+    grep -v external/   | \
+    egrep -v "cmake-build-.*/" | \
+    xargs wc -l | \
+    tail -1
 
