2821dd5cbb035d329df21f8717cd98b5b0ab8fc6
zer0 <osom8979@gmail.com>
2017-10-21 10:53:52 +0900

    Create prototype of RotatePath class.

--- libtbag/filesystem/RotatePath.cpp
+++ libtbag/filesystem/RotatePath.cpp
@@ -6,7 +6,9 @@
  */
 
 #include <libtbag/filesystem/RotatePath.hpp>
+#include <libtbag/filesystem/details/FsCommon.hpp>
 #include <libtbag/log/Log.hpp>
+#include <utility>
 
 // -------------------
 NAMESPACE_LIBTBAG_OPEN
@@ -14,14 +16,160 @@ NAMESPACE_LIBTBAG_OPEN
 
 namespace filesystem {
 
+// ---------------------------------
+// RotatePathUpdater implementation.
+// ---------------------------------
+
+RotatePathUpdater::RotatePathUpdater(std::string const & rule) : update_rule(rule)
+{
+    // EMPTY.
+}
+
+RotatePathUpdater::~RotatePathUpdater()
+{
+    // EMPTY.
+}
+
+Path RotatePathUpdater::obtain() const
+{
+    return Path();
+}
+
+// -------------------------------------
+// RotatePathSizeChecker implementation.
+// -------------------------------------
+
+RotatePathSizeChecker::RotatePathSizeChecker(std::string const & rule, std::size_t size)
+        : RotatePathUpdater(rule), max_size(size)
+{
+    // EMPTY.
+}
+
+RotatePathSizeChecker::~RotatePathSizeChecker()
+{
+    // EMPTY.
+}
+
+bool RotatePathSizeChecker::test(Path const & prev, Path & next, char const * buffer, std::size_t size) const
+{
+    RotatePath::FileState state = {0};
+    if (details::getState(prev, &state) == false) {
+        return false;
+    }
+
+    if (state.size + size < max_size) {
+        return false;
+    }
+
+    // TODO: Update next path.
+    return true;
+}
+
+// --------------------------
+// RotatePath implementation.
+// --------------------------
+
 RotatePath::RotatePath()
 {
     // EMPTY.
 }
 
+RotatePath::RotatePath(Path const & path) : _path(path)
+{
+    // EMPTY.
+}
+
+RotatePath::RotatePath(Path const & path, SharedChecker const & checker) : _path(path), _checker(checker)
+{
+    // EMPTY.
+}
+
+RotatePath::RotatePath(RotatePath const & obj)
+{
+    (*this) = obj;
+}
+
+RotatePath::RotatePath(RotatePath && obj)
+{
+    (*this) = std::move(obj);
+}
+
 RotatePath::~RotatePath()
 {
     // EMPTY.
+}
+
+RotatePath & RotatePath::operator =(RotatePath const & obj)
+{
+    if (this != &obj) {
+        _checker = obj._checker;
+        _path = obj._path;
+    }
+    return *this;
+}
+
+RotatePath & RotatePath::operator =(RotatePath && obj)
+{
+    if (this != &obj) {
+        _checker.swap(obj._checker);
+        _path.swap(obj._path);
+    }
+    return *this;
+}
+
+bool RotatePath::testIfRead(Path const & prev, Path & next) const
+{
+    if (static_cast<bool>(_checker)) {
+        return _checker->test(prev, next, nullptr, 0);
+    }
+    return false;
+}
+
+bool RotatePath::testIfRead(Path & next) const
+{
+    return testIfRead(_path, next);
+}
+
+bool RotatePath::testIfWrite(Path const & prev, Path & next, char const * buffer, std::size_t size) const
+{
+    if (static_cast<bool>(_checker)) {
+        return _checker->test(prev, next, buffer, size);
+    }
+    return false;
+}
+
+bool RotatePath::testIfWrite(Path & next, char const * buffer, std::size_t size) const
+{
+    return testIfWrite(_path, next, buffer, size);
+}
+
+bool RotatePath::next(char const * buffer, std::size_t size)
+{
+    Path next;
+    if (testIfWrite(next, buffer, size)) {
+        _path = next;
+        return true;
+    }
+    return false;
+}
+
+bool RotatePath::next()
+{
+    Path next;
+    if (testIfRead(next)) {
+        _path = next;
+        return true;
+    }
+    return false;
+}
+
+RotatePath::FileState RotatePath::getState() const
+{
+    FileState state = {0};
+    if (details::getState(_path, &state) == false) {
+        tDLogE("RotatePath::getState() result error.");
+    }
+    return state;
 }
 
 } // namespace filesystem
--- libtbag/filesystem/RotatePath.hpp
+++ libtbag/filesystem/RotatePath.hpp
@@ -15,14 +15,71 @@
 
 #include <libtbag/config.h>
 #include <libtbag/predef.hpp>
-#include <libtbag/Noncopyable.hpp>
 #include <libtbag/filesystem/Path.hpp>
+
+#include <string>
+#include <memory>
 
 // -------------------
 NAMESPACE_LIBTBAG_OPEN
 // -------------------
 
 namespace filesystem {
+
+/**
+ * Condition checker interface of RotatePath.
+ *
+ * @author zer0
+ * @date   2017-10-21
+ */
+struct RotatePathCheckerInterface
+{
+    /**
+     * Rotate check method.
+     *
+     * @param[in] prev
+     *      Previous path information.
+     * @param[out] next
+     *      Update path information.
+     * @param[in] buffer
+     *      If you want a wirte in file, this parameter is the source buffer.
+     * @param[in] size
+     *      If you want a wirte in file, this parameter is the size of source buffer.
+     */
+    virtual bool test(Path const & prev, Path & next, char const * buffer, std::size_t size) const = 0;
+};
+
+/**
+ * Path updater.
+ *
+ * @author zer0
+ * @date   2017-10-21
+ */
+struct TBAG_API RotatePathUpdater : public RotatePathCheckerInterface
+{
+    std::string update_rule;
+
+    RotatePathUpdater(std::string const & rule);
+    virtual ~RotatePathUpdater();
+
+    Path obtain() const;
+};
+
+/**
+ * Maximum file size checkr.
+ *
+ * @author zer0
+ * @date   2017-10-21
+ */
+struct TBAG_API RotatePathSizeChecker : public RotatePathUpdater
+{
+    std::size_t max_size;
+
+    RotatePathSizeChecker(std::string const & rule, std::size_t size = 0);
+    virtual ~RotatePathSizeChecker();
+
+    virtual bool test(Path const & prev, Path & next, char const * buffer, std::size_t size) const override;
+};
 
 /**
  * RotatePath class prototype.
@@ -34,15 +91,51 @@ namespace filesystem {
  *  - absolute time.
  *  - file size.
  */
-class TBAG_API RotatePath : private Noncopyable
+class TBAG_API RotatePath
 {
 public:
     using TimeSpec  = details::TimeSpec;
     using FileState = details::FileState;
 
+    using SharedChecker = std::shared_ptr<RotatePathCheckerInterface>;
+
+private:
+    SharedChecker _checker;
+    Path _path;
+
 public:
     RotatePath();
+    RotatePath(Path const & path);
+    RotatePath(Path const & path, SharedChecker const & checker);
+    RotatePath(RotatePath const & obj);
+    RotatePath(RotatePath && obj);
     virtual ~RotatePath();
+
+public:
+    RotatePath & operator =(RotatePath const & obj);
+    RotatePath & operator =(RotatePath && obj);
+
+public:
+    void setChecker(RotatePathCheckerInterface * checker) { setChecker(checker); }
+    void setChecker(SharedChecker const & checker) { _checker = checker; }
+
+public:
+    void setPath(Path const & path) { _path = path; }
+    Path getPath() const { return _path; }
+
+public:
+    bool testIfRead(Path const & prev, Path & next) const;
+    bool testIfRead(Path & next) const;
+
+    bool testIfWrite(Path const & prev, Path & next, char const * buffer, std::size_t size) const;
+    bool testIfWrite(Path & next, char const * buffer, std::size_t size) const;
+
+public:
+    bool next(char const * buffer, std::size_t size);
+    bool next();
+
+public:
+    FileState getState() const;
 };
 
 } // namespace filesystem
