4666ea31442bdd4426be98f61383237a6ad4e000
zer0 <osom8979@gmail.com>
2017-11-03 14:03:51 +0900

    Create Tblas package.

---
+++ libtbag/math/blas/Tblas.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file   Tblas.cpp
+ * @brief  Tblas class implementation.
+ * @author zer0
+ * @date   2017-11-03
+ */
+
+#include <libtbag/math/blas/Tblas.hpp>
+#include <libtbag/log/Log.hpp>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace math {
+namespace blas {
+
+} // namespace blas
+} // namespace math
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
