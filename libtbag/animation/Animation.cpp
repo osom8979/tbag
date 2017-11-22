41c7457f529f660157b2384fe47f6f9669aaab17
zer0 <osom8979@gmail.com>
2017-11-22 21:48:10 +0900

    Create Animation class.

---
+++ libtbag/animation/Animation.cpp
@@ -0,0 +1,63 @@
+/**
+ * @file   Animation.cpp
+ * @brief  Animation class implementation.
+ * @author zer0
+ * @date   2017-11-22
+ */
+
+#include <libtbag/animation/Animation.hpp>
+#include <libtbag/log/Log.hpp>
+#include <utility>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace animation {
+
+Animation::Animation()
+{
+    // EMPTY.
+}
+
+Animation::Animation(Animation const & obj)
+{
+    (*this) = obj;
+}
+
+Animation::Animation(Animation && obj)
+{
+    (*this) = std::move(obj);
+}
+
+Animation::~Animation()
+{
+    // EMPTY.
+}
+
+Animation & Animation::operator =(Animation const & obj)
+{
+    if (this != &obj) {
+    }
+    return *this;
+}
+
+Animation & Animation::operator =(Animation && obj)
+{
+    swap(obj);
+    return *this;
+}
+
+void Animation::swap(Animation & obj)
+{
+    if (this != &obj) {
+        _interpolator.swap(obj._interpolator);
+    }
+}
+
+} // namespace animation
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
---
+++ libtbag/animation/Animation.hpp
@@ -0,0 +1,69 @@
+/**
+ * @file   Animation.hpp
+ * @brief  Animation class prototype.
+ * @author zer0
+ * @date   2017-11-22
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_ANIMATION_ANIMATION_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_ANIMATION_ANIMATION_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+#include <libtbag/predef.hpp>
+#include <libtbag/animation/TimeInterpolator.hpp>
+
+#include <memory>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace animation {
+
+/**
+ * Animation class prototype.
+ *
+ * @author zer0
+ * @date   2017-11-22
+ */
+class TBAG_API Animation
+{
+public:
+    using SharedTimeInterpolator = std::shared_ptr<TimeInterpolator>;
+
+private:
+    SharedTimeInterpolator _interpolator;
+
+public:
+    Animation();
+    Animation(Animation const & obj);
+    Animation(Animation && obj);
+    virtual ~Animation();
+
+public:
+    Animation & operator =(Animation const & obj);
+    Animation & operator =(Animation && obj);
+
+public:
+    void swap(Animation & obj);
+
+public:
+    friend void swap(Animation & lh, Animation & rh)
+    {
+        lh.swap(rh);
+    }
+};
+
+} // namespace animation
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_ANIMATION_ANIMATION_HPP__
+
