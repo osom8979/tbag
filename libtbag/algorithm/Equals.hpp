3246b392203baabddebec02e46f7c6989e6987e4
zer0 <osom8979@gmail.com>
2017-12-14 16:46:21 +0900

    Create GreaterOrEquals & LessOrEquals classes.

--- libtbag/algorithm/Equals.hpp
+++ libtbag/algorithm/Equals.hpp
@@ -36,11 +36,35 @@ namespace algorithm {
  * Check the equals for the floating point types.
  */
 template <typename T>
-bool isFloatingPointEquals(T a, T b, T epsilon = TBAG_DEFAULT_FLOATING_POINT_EQUALS_EPSILON)
+bool isFloatingPointEquals(T a, T b, T epsilon = TBAG_DEFAULT_FLOATING_POINT_EQUALS_EPSILON) TBAG_NOEXCEPT
 {
     static_assert(std::is_floating_point<T>::value, "Only floating point types are supported.");
-    return (std::fabs(a - b) < epsilon);
+    return (std::fabs(a - b) <= epsilon);
 }
+
+/**
+ * Check if the first number is greater than or equal to the second number.
+ */
+template <typename T>
+bool isFloatingPointGreaterOrEqual(T a, T b, T epsilon = TBAG_DEFAULT_FLOATING_POINT_EQUALS_EPSILON) TBAG_NOEXCEPT
+{
+    static_assert(std::is_floating_point<T>::value, "Only floating point types are supported.");
+    return (a > b) || isFloatingPointEquals(a, b, epsilon);
+}
+
+/**
+ * Check if the first number is less than or equal to the second number.
+ */
+template <typename T>
+bool isFloatingPointLessOrEqual(T a, T b, T epsilon = TBAG_DEFAULT_FLOATING_POINT_EQUALS_EPSILON) TBAG_NOEXCEPT
+{
+    static_assert(std::is_floating_point<T>::value, "Only floating point types are supported.");
+    return (a < b) || isFloatingPointEquals(a, b, epsilon);
+}
+
+// -------
+// Equals.
+// -------
 
 template <typename T, typename E, bool __is_floating_point__> struct __EqualsBackend;
 
@@ -49,7 +73,7 @@ struct __EqualsBackend<T, E, true> : public std::true_type
 {
     static_assert(std::is_floating_point<T>::value, "Only floating point types are supported.");
 
-    inline static bool __equals(T lh, T rh, E epsilon) TBAG_NOEXCEPT
+    inline static bool test(T lh, T rh, E epsilon) TBAG_NOEXCEPT
     { return isFloatingPointEquals(lh, rh, epsilon); }
 };
 
@@ -58,7 +82,7 @@ struct __EqualsBackend<T, E, false> : public std::false_type
 {
     static_assert(std::is_integral<T>::value, "Only integer types are supported.");
 
-    inline static bool __equals(T lh, T rh, E UNUSED_PARAM(epsilon)) TBAG_NOEXCEPT
+    inline static bool test(T lh, T rh, E UNUSED_PARAM(epsilon)) TBAG_NOEXCEPT
     { return lh == rh; }
 };
 
@@ -78,32 +102,136 @@ struct Equals : protected __EqualsBackend<T, E, std::is_floating_point<T>::value
 
     TBAG_CONSTEXPR static bool isFloatingPoint() TBAG_NOEXCEPT { return value; }
 
-    inline static bool equals(T lh, T rh, E epsilon = static_cast<E>(TBAG_DEFAULT_FLOATING_POINT_EQUALS_EPSILON)) TBAG_NOEXCEPT
-    { return Parent::__equals(lh, rh, epsilon); }
+    inline static bool test(T lh, T rh, E epsilon = static_cast<E>(TBAG_DEFAULT_FLOATING_POINT_EQUALS_EPSILON)) TBAG_NOEXCEPT
+    { return Parent::test(lh, rh, epsilon); }
+};
+
+// ----------------
+// GreaterOrEquals.
+// ----------------
+
+template <typename T, typename E, bool __is_floating_point__> struct __GreaterOrEqualsBackend;
+
+template <typename T, typename E>
+struct __GreaterOrEqualsBackend<T, E, true> : public std::true_type
+{
+    static_assert(std::is_floating_point<T>::value, "Only floating point types are supported.");
+
+    inline static bool test(T lh, T rh, E epsilon) TBAG_NOEXCEPT
+    { return isFloatingPointGreaterOrEqual(lh, rh, epsilon); }
 };
 
 template <typename T, typename E>
-inline bool equals(T a, T b, E epsilon)
+struct __GreaterOrEqualsBackend<T, E, false> : public std::false_type
 {
-    return Equals<T, E>::equals(a, b, epsilon);
+    static_assert(std::is_integral<T>::value, "Only integer types are supported.");
+
+    inline static bool test(T lh, T rh, E UNUSED_PARAM(epsilon)) TBAG_NOEXCEPT
+    { return lh >= rh; }
+};
+
+/**
+ * GreaterOrEquals class prototype.
+ *
+ * @author zer0
+ * @date   2017-12-14
+ */
+template <typename T, typename E>
+struct GreaterOrEquals : protected __GreaterOrEqualsBackend<T, E, std::is_floating_point<T>::value>
+{
+    using Parent = __GreaterOrEqualsBackend<T, E, std::is_floating_point<T>::value>;
+    static_assert(std::is_pod<T>::value, "Only POD types are supported.");
+
+    TBAG_CONSTEXPR static bool const value = Parent::value;
+
+    TBAG_CONSTEXPR static bool isFloatingPoint() TBAG_NOEXCEPT { return value; }
+
+    inline static bool test(T lh, T rh, E epsilon = static_cast<E>(TBAG_DEFAULT_FLOATING_POINT_EQUALS_EPSILON)) TBAG_NOEXCEPT
+    { return Parent::test(lh, rh, epsilon); }
+};
+
+// -------------
+// LessOrEquals.
+// -------------
+
+template <typename T, typename E, bool __is_floating_point__> struct __LessOrEqualsBackend;
+
+template <typename T, typename E>
+struct __LessOrEqualsBackend<T, E, true> : public std::true_type
+{
+    static_assert(std::is_floating_point<T>::value, "Only floating point types are supported.");
+
+    inline static bool test(T lh, T rh, E epsilon) TBAG_NOEXCEPT
+    { return isFloatingPointLessOrEqual(lh, rh, epsilon); }
+};
+
+template <typename T, typename E>
+struct __LessOrEqualsBackend<T, E, false> : public std::false_type
+{
+    static_assert(std::is_integral<T>::value, "Only integer types are supported.");
+
+    inline static bool test(T lh, T rh, E UNUSED_PARAM(epsilon)) TBAG_NOEXCEPT
+    { return lh <= rh; }
+};
+
+/**
+ * LessOrEquals class prototype.
+ *
+ * @author zer0
+ * @date   2017-12-14
+ */
+template <typename T, typename E>
+struct LessOrEquals : protected __LessOrEqualsBackend<T, E, std::is_floating_point<T>::value>
+{
+    using Parent = __LessOrEqualsBackend<T, E, std::is_floating_point<T>::value>;
+    static_assert(std::is_pod<T>::value, "Only POD types are supported.");
+
+    TBAG_CONSTEXPR static bool const value = Parent::value;
+
+    TBAG_CONSTEXPR static bool isFloatingPoint() TBAG_NOEXCEPT { return value; }
+
+    inline static bool test(T lh, T rh, E epsilon = static_cast<E>(TBAG_DEFAULT_FLOATING_POINT_EQUALS_EPSILON)) TBAG_NOEXCEPT
+    { return Parent::test(lh, rh, epsilon); }
+};
+
+// ----------
+// Front-end.
+// ----------
+
+template <typename T, typename E>
+inline bool equals(T a, T b, E epsilon) TBAG_NOEXCEPT
+{
+    return Equals<T, E>::test(a, b, epsilon);
 }
 
 template <typename T>
-inline bool equals(T a, T b)
+inline bool equals(T a, T b) TBAG_NOEXCEPT
 {
-    return Equals<T, T>::equals(a, b);
+    return Equals<T, T>::test(a, b);
 }
 
 template <typename T, typename E>
-inline bool __check_floating_point_of_equals(T a, T b, E epsilon)
+inline bool lessOrEquals(T a, T b, E epsilon) TBAG_NOEXCEPT
 {
-    return Equals<T, E>::isFloatingPoint();
+    return LessOrEquals<T, E>::test(a, b, epsilon);
 }
 
 template <typename T>
-inline bool __check_floating_point_of_equals(T a, T b)
+inline bool lessOrEquals(T a, T b) TBAG_NOEXCEPT
 {
-    return Equals<T, T>::isFloatingPoint();
+    return LessOrEquals<T, T>::test(a, b);
+}
+
+template <typename T, typename E>
+inline bool greaterOrEquals(T a, T b, E epsilon) TBAG_NOEXCEPT
+{
+    return GreaterOrEquals<T, E>::test(a, b, epsilon);
+}
+
+template <typename T>
+inline bool greaterOrEquals(T a, T b) TBAG_NOEXCEPT
+{
+    return GreaterOrEquals<T, T>::test(a, b);
 }
 
 } // namespace algorithm
--- tester/libtbag/algorithm/EqualsTest.cpp
+++ tester/libtbag/algorithm/EqualsTest.cpp
@@ -11,7 +11,19 @@
 using namespace libtbag;
 using namespace libtbag::algorithm;
 
-TEST(EqualsTest, Default)
+template <typename T, typename E>
+inline bool __check_floating_point_of_equals(T a, T b, E epsilon)
+{
+    return Equals<T, E>::isFloatingPoint();
+}
+
+template <typename T>
+inline bool __check_floating_point_of_equals(T a, T b)
+{
+    return Equals<T, T>::isFloatingPoint();
+}
+
+TEST(EqualsTest, Equals)
 {
     ASSERT_FALSE(isFloatingPointEquals(0.015, 0.017, 0.001));
     ASSERT_TRUE (isFloatingPointEquals(0.015, 0.017, 0.1));
@@ -29,8 +41,47 @@ TEST(EqualsTest, Default)
 
     ASSERT_FALSE(equals(2, 1));
     ASSERT_TRUE (equals(1, 1));
+    ASSERT_FALSE(equals(1, 2));
 
     ASSERT_FALSE(equals(2, 1, 0.001));
     ASSERT_TRUE (equals(1, 1, 0.001));
+
+    ASSERT_TRUE(equals(5.0, 4.9999999987));
+}
+
+TEST(EqualsTest, GreaterOrEquals)
+{
+    ASSERT_FALSE(greaterOrEquals(0.101, 0.102, 0.0001));
+    ASSERT_TRUE (greaterOrEquals(0.101, 0.102, 0.01));
+
+    ASSERT_TRUE (greaterOrEquals(2, 1));
+    ASSERT_TRUE (greaterOrEquals(1, 1));
+    ASSERT_FALSE(greaterOrEquals(1, 2));
+
+    ASSERT_TRUE (greaterOrEquals(2, 1, 0.001));
+    ASSERT_TRUE (greaterOrEquals(1, 1, 0.001));
+    ASSERT_FALSE(greaterOrEquals(1, 2, 0.001));
+
+    ASSERT_TRUE (greaterOrEquals(5.009, 4.9999999987));
+    ASSERT_TRUE (greaterOrEquals(5.0  , 4.9999999987));
+    ASSERT_FALSE(greaterOrEquals(4.9  , 4.9999999987));
+}
+
+TEST(EqualsTest, LessOrEquals)
+{
+    ASSERT_TRUE(lessOrEquals(0.101, 0.102, 0.0001));
+    ASSERT_TRUE(lessOrEquals(0.101, 0.102, 0.01));
+
+    ASSERT_FALSE(lessOrEquals(2, 1));
+    ASSERT_TRUE (lessOrEquals(1, 1));
+    ASSERT_TRUE (lessOrEquals(1, 2));
+
+    ASSERT_FALSE(lessOrEquals(2, 1, 0.001));
+    ASSERT_TRUE (lessOrEquals(1, 1, 0.001));
+    ASSERT_TRUE (lessOrEquals(1, 2, 0.001));
+
+    ASSERT_FALSE(lessOrEquals(5.009, 4.9999999987));
+    ASSERT_TRUE (lessOrEquals(5.0  , 4.9999999987));
+    ASSERT_TRUE (lessOrEquals(4.9  , 4.9999999987));
 }
 
