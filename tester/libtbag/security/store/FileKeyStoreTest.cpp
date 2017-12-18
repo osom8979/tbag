4a423b6a4c805e30d5613e41bbd7e549ca80f44a
zer0 <osom8979@gmail.com>
2017-12-18 11:32:43 +0900

    Create FileKeyStoreTest.Encrypt tester.

--- tester/libtbag/security/store/FileKeyStoreTest.cpp
+++ tester/libtbag/security/store/FileKeyStoreTest.cpp
@@ -38,7 +38,45 @@ TEST(FileKeyStoreTest, Default)
 
     ASSERT_TRUE(store.set(KEY1, VAL1));
     ASSERT_TRUE(store.get(KEY1, value));
+    ASSERT_FALSE(value.empty());
+
     ASSERT_EQ(VAL1, value);
+    ASSERT_TRUE(store.cmp(KEY1, VAL1));
+
+    ASSERT_TRUE(store.remove(KEY1));
+    ASSERT_FALSE(store.get(KEY1, value));
+
+    ASSERT_TRUE(store.remove(KEY1));
+    ASSERT_FALSE(store.get(KEY1, value));
+    ASSERT_TRUE(store.list().empty());
+}
+
+TEST(FileKeyStoreTest, Encrypt)
+{
+    tttDir(true, true);
+    auto const FILE_PATH = tttDirGet() / "store";
+
+    std::string const KEY1 = "key1";
+    std::string const VAL1 = "value1";
+
+    std::string value;
+    std::vector<std::string> keys;
+
+    FileKeyStore store(FILE_PATH.toString());
+    ASSERT_TRUE(store.list().empty());
+    ASSERT_FALSE(store.get(KEY1, value));
+
+    ASSERT_TRUE(store.create(KEY1));
+    keys = store.list();
+    ASSERT_EQ(1, keys.size());
+    ASSERT_EQ(KEY1, keys[0]);
+
+    ASSERT_TRUE(store.set(KEY1, VAL1, true));
+    ASSERT_TRUE(store.get(KEY1, value));
+    ASSERT_FALSE(value.empty());
+
+    ASSERT_NE(VAL1, value);
+    ASSERT_TRUE(store.cmp(KEY1, VAL1, true));
 
     ASSERT_TRUE(store.remove(KEY1));
     ASSERT_FALSE(store.get(KEY1, value));
