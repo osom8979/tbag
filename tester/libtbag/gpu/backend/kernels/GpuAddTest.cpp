7a1718ac21dc85e01fea6f5d6ca89b63f1fd6830
zer0 <osom8979@gmail.com>
2018-01-07 19:53:58 +0900

    Fixed ambiguous symbol error in MSVC.

--- tester/libtbag/gpu/backend/kernels/GpuAddTest.cpp
+++ tester/libtbag/gpu/backend/kernels/GpuAddTest.cpp
@@ -31,7 +31,7 @@ struct GpuAddKernelTest
     HostMemory  host_v2;
     HostMemory  host_result;
 
-    GpuAddKernelTest(GpuBackendType t, int c) : GpuAddKernelTest(createGpuContext(t), c)
+    GpuAddKernelTest(libtbag::gpu::GpuBackendType t, int c) : GpuAddKernelTest(createGpuContext(t), c)
     {
         // EMPTY.
     }
@@ -98,7 +98,7 @@ TEST(GpuAddTest, Default)
     runAllIfSupported([](UniqueGpu & gpu){
         std::cout << "GPU type: " << gpu->getTypeString() << std::endl;
 
-        if (gpu->getType() != GpuBackendType::GBT_CPU && gpu->getType() != GpuBackendType::GBT_CUDA) {
+        if (gpu->getType() != libtbag::gpu::GpuBackendType::GBT_CPU && gpu->getType() != libtbag::gpu::GpuBackendType::GBT_CUDA) {
             return;
         }
 
