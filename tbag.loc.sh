686b7af2692d3471e5555ccb19387774efd84f59
zer0 <osom8979@gmail.com>
2018-01-08 16:05:35 +0900

    Added *.simd to LOC file pattern.

--- tbag.loc.sh
+++ tbag.loc.sh
@@ -2,11 +2,17 @@
 
 echo 'Line of count in tbag project.'
 
-find . -name "*.cpp" -o -name "*.hpp" -o -name "*.c" -o -name "*.h" -o -name "*.py" -o -name "*.cmake" | \
-    grep -v dep/        | \
-    grep -v build/      | \
-    grep -v 3rd/        | \
-    grep -v external/   | \
+find .     -name "*.cpp"     \
+        -o -name "*.hpp"     \
+        -o -name "*.c"       \
+        -o -name "*.h"       \
+        -o -name "*.py"      \
+        -o -name "*.simd"    \
+        -o -name "*.cmake" | \
+    grep -v dep/      | \
+    grep -v build/    | \
+    grep -v 3rd/      | \
+    grep -v external/ | \
     egrep -v "cmake-build-.*/" | \
     xargs wc -l | \
     tail -1
