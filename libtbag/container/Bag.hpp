cee13129d6280b8d4930a60df876dbb0384504e4
zer0 <osom8979@gmail.com>
2018-05-13 17:47:03 +0900

    Create Bag class template.

--- libtbag/container/Bag.hpp
+++ libtbag/container/Bag.hpp
@@ -15,6 +15,11 @@
 
 #include <libtbag/config.h>
 #include <libtbag/predef.hpp>
+#include <libtbag/Err.hpp>
+#include <libtbag/algorithm/MinMax.hpp>
+
+#include <cstdint>
+#include <cassert>
 
 #include <memory>
 #include <vector>
@@ -23,17 +28,32 @@
 #include <utility>
 #include <algorithm>
 
+#define DEFAULT_ELEMENTS_BUFFER_SIZE_OF_BAG 4
+
 // -------------------
 NAMESPACE_LIBTBAG_OPEN
 // -------------------
 
 namespace container {
 
+/**
+ * Base container interface of Bag class.
+ *
+ * @tparam T  Base type.
+ * @tparam A  Base allocator.
+ * @tparam C  Base container.
+ */
 template <typename T,
           template <typename Tp> class A,
           typename C>
 class BagContainer;
 
+/**
+ * Vector container interface of Bag class.
+ *
+ * @tparam T  Base type.
+ * @tparam A  Base allocator.
+ */
 template <typename T, template <typename Tp> class A>
 class BagContainer<T, A, std::vector<T, A<T> > >
 {
@@ -46,46 +66,207 @@ private:
     Container _container;
 
 public:
-    BagContainer() : _container()
+    BagContainer() TBAG_NOEXCEPT_SPECIFIER(std::is_nothrow_default_constructible<Container>::value) : _container()
     { /* EMPTY. */ }
 
     ~BagContainer()
     { /* EMPTY. */ }
 
 public:
-    void clear()
+    void clear() TBAG_NOEXCEPT_SP_OP(_container.clear())
     {
         _container.clear();
     }
 
-    void resize(std::size_t size)
+    void resize(std::size_t size) TBAG_NOEXCEPT_SP_OP(_container.resize(size))
     {
         _container.resize(size);
     }
 
-    void swap(BagContainer & obj)
+    void swap(BagContainer & obj) TBAG_NOEXCEPT_SP_OP(_container.swap(obj._container))
     {
-        _container.swap(obj);
+        _container.swap(obj._container);
     }
 
-    void copy(BagContainer const & obj)
+    void copy(BagContainer const & obj) TBAG_NOEXCEPT_SPECIFIER(std::is_nothrow_copy_assignable<Container>::value)
     {
         _container = obj._container;
     }
 
-    Value & at(std::size_t index)
+    Value & at(std::size_t index) TBAG_NOEXCEPT_SP_OP(_container.operator[](index))
     {
-        return _container[index];
+        return _container.operator[](index);
     }
 
-    Value const & at(std::size_t index) const
+    Value const & at(std::size_t index) const TBAG_NOEXCEPT_SP_OP(_container.operator[](index))
     {
-        return _container[index];
+        return _container.operator[](index);
     }
 
-    std::size_t size() const
+    Value * data() TBAG_NOEXCEPT_SP_OP(_container.data())
+    {
+        return _container.data();
+    }
+
+    Value const * data() const TBAG_NOEXCEPT_SP_OP(_container.data())
+    {
+        return _container.data();
+    }
+
+    std::size_t size() const TBAG_NOEXCEPT_SP_OP(_container.size())
     {
         return _container.size();
+    }
+
+    bool empty() const TBAG_NOEXCEPT_SP_OP(_container.empty())
+    {
+        return _container.empty();
+    }
+};
+
+/**
+ * Base dimension interface of Bag class.
+ *
+ * @tparam L Max dimensions.
+ */
+template <unsigned L = DEFAULT_ELEMENTS_BUFFER_SIZE_OF_BAG>
+class BagDimensions
+{
+public:
+    using DimValue = unsigned;
+
+public:
+    TBAG_CONSTEXPR static DimValue const ELEMENTS_BUFFER_SIZE = L;
+    TBAG_CONSTEXPR static DimValue const NOT_FOUND = -1;
+
+public:
+    TBAG_CONSTEXPR static DimValue const maxDimSize() TBAG_NOEXCEPT { return ELEMENTS_BUFFER_SIZE; }
+    TBAG_CONSTEXPR static DimValue const getNotFound() TBAG_NOEXCEPT  { return NOT_FOUND; }
+
+private:
+    DimValue _dims[ELEMENTS_BUFFER_SIZE];
+    DimValue _size;
+
+public:
+    BagDimensions() TBAG_NOEXCEPT : _dims{0,}, _size(0)
+    { /* EMPTY */ }
+
+    ~BagDimensions()
+    { /* EMPTY */ }
+
+public:
+    void clear() TBAG_NOEXCEPT
+    {
+        for (DimValue i = 0; i < ELEMENTS_BUFFER_SIZE; ++i) {
+            _dims[i] = 0;
+        }
+        _size = 0;
+    }
+
+    void resize(DimValue const * dims, DimValue size)
+    {
+        assert(0 < COMPARE_AND(size) <= maxDimSize());
+        DimValue const MIN_SIZE = libtbag::algorithm::getMin(size, maxDimSize());
+        for (DimValue i = 0; i < MIN_SIZE; ++i) {
+            _dims[i] = dims[i];
+        }
+        _size = size;
+    }
+
+    template <typename T>
+    void resize(std::initializer_list<T> list)
+    {
+        resize((DimValue const *)list.begin(), (DimValue)list.size());
+    }
+
+    template <typename ... Args>
+    void resize(Args && ... list)
+    {
+        resize({std::forward<Args>(list) ...});
+    }
+
+    void swap(BagDimensions & obj) TBAG_NOEXCEPT
+    {
+        for (DimValue i = 0; i < maxDimSize(); ++i) {
+            std::swap(_dims[i], obj._dims[i]);
+        }
+        std::swap(_size, obj._size);
+    }
+
+    void copy(BagDimensions const & obj) TBAG_NOEXCEPT
+    {
+        for (DimValue i = 0; i < maxDimSize(); ++i) {
+            _dims[i] = obj._dims[i];
+        }
+        _size = obj._size;
+    }
+
+    DimValue & at(DimValue index)
+    {
+        assert(0 <= COMPARE_AND(index) < maxDimSize());
+        return _dims[index];
+    }
+
+    DimValue const & at(DimValue index) const
+    {
+        assert(0 <= COMPARE_AND(index) < maxDimSize());
+        return _dims[index];
+    }
+
+    DimValue size() const TBAG_NOEXCEPT
+    {
+        return _size;
+    }
+
+    bool empty() const TBAG_NOEXCEPT
+    {
+        return _size == 0;
+    }
+
+    static DimValue calcTotal(DimValue const * dims, DimValue size)
+    {
+        if (size == 0) {
+            return 0;
+        }
+        DimValue result = dims[0];
+        assert(dims[0] > 0);
+        for (DimValue i = 1; i < size; ++i) {
+            assert(dims[i] > 0);
+            result *= dims[i];
+        }
+        return result;
+    }
+
+    DimValue total() const
+    {
+        return calcTotal(_dims, _size);
+    }
+
+    DimValue offset(DimValue const * offsets, DimValue size)
+    {
+        assert(0 < COMPARE_AND(size) <= _size);
+        DimValue const MIN_SIZE = libtbag::algorithm::getMin(size, _size);
+        DimValue result = offsets[0];
+        DimValue dim_step = _dims[0];
+        assert(_dims[0] > 0);
+        for (DimValue i = 1; i < MIN_SIZE; ++i) {
+            result += offsets[i] * dim_step;
+            assert(_dims[i] > 0);
+            dim_step *= _dims[i];
+        }
+        return result;
+    }
+
+    template <typename T>
+    DimValue offset(std::initializer_list<T> list)
+    {
+        return offset((DimValue const *)list.begin(), (DimValue)list.size());
+    }
+
+    template <typename ... Args>
+    DimValue offset(Args && ... list)
+    {
+        return offset({std::forward<Args>(list) ...});
     }
 };
 
@@ -97,32 +278,35 @@ public:
  */
 template <typename T,
           template <typename Tp> class A = std::allocator,
-          typename C = std::vector<T, A<T> > >
-class Bag : protected BagContainer<T, A, C>
+          typename C = std::vector<T, A<T> >,
+          unsigned L = DEFAULT_ELEMENTS_BUFFER_SIZE_OF_BAG>
+class Bag : protected BagContainer<T, A, C>,
+            protected BagDimensions<L>
 {
 public:
-    using Base = BagContainer<T, A, C>;
+    using Self = Bag<T, A, C, L>;
+    using BaseContainer   = BagContainer<T, A, C>;
+    using BaseDimenstions = BagDimensions<L>;
 
 public:
-    using Value        = typename Base::Value;
-    using Allocator    = typename Base::Allocator;
-    using Container    = typename Base::Container;
-
-    using DimValue     = int;
-    using DimAllocator = A<DimValue>;
-    using Dimensions   = std::vector<int, DimAllocator>;
+    using Value     = typename BaseContainer::Value;
+    using Allocator = typename BaseContainer::Allocator;
+    using Container = typename BaseContainer::Container;
+    using DimValue  = typename BaseDimenstions::DimValue;
 
 public:
-    static_assert(!std::is_pointer<Value>::value, "Value must not be a pointer type");
+    static_assert(!std::is_pointer  <Value>::value, "Value must not be a pointer type");
     static_assert(!std::is_reference<Value>::value, "Value must not be a reference type");
 
-private:
-    Dimensions _dims;
-    int _padding;
-
 public:
-    Bag() : Base(), _dims(), _padding()
+    Bag() : BaseContainer(), BaseDimenstions()
     { /* EMPTY. */ }
+
+    template <typename Tp>
+    Bag(std::initializer_list<Tp> list) : Bag()
+    {
+        Self::resize(list);
+    }
 
     Bag(Bag const & obj) : Bag()
     {
@@ -154,24 +338,86 @@ public:
     void copy(Bag const & obj)
     {
         if (this != &obj) {
-            _dims = obj._dims;
-            _padding = obj._padding;
-            Base::copy(obj);
+            BaseDimenstions::copy(obj);
+            BaseContainer::copy(obj);
         }
     }
 
     void swap(Bag & obj)
     {
         if (this != &obj) {
-            _dims.swap(obj._dims);
-            std::swap(_padding, obj._padding);
-            Base::swap(obj);
+            BaseDimenstions::swap(obj);
+            BaseContainer::swap(obj);
         }
     }
 
     friend void swap(Bag & lh, Bag & rh)
     {
         lh.swap(rh);
+    }
+
+public:
+    void resize(DimValue const * dims, DimValue size)
+    {
+        BaseDimenstions::resize(dims, size);
+        BaseContainer::resize(BaseDimenstions::total());
+    }
+
+    template <typename Tp>
+    void resize(std::initializer_list<Tp> list)
+    {
+        Self::resize((DimValue const *)list.begin(), (DimValue)list.size());
+    }
+
+    template <typename ... Args>
+    void resize(Args && ... list)
+    {
+        Self::resize({std::forward<Args>(list) ...});
+    }
+
+    void clear()
+    {
+        BaseDimenstions::clear();
+        BaseContainer::clear();
+    }
+
+public:
+    DimValue size() const TBAG_NOEXCEPT_SP_OP(BaseDimenstions::total())
+    {
+        return BaseDimenstions::total();
+    }
+
+    DimValue size(DimValue index) const TBAG_NOEXCEPT_SP_OP(BaseDimenstions::at(index))
+    {
+        return BaseDimenstions::at(index);
+    }
+
+    bool empty() const TBAG_NOEXCEPT_SP_OP(BaseDimenstions::empty())
+    {
+        return BaseDimenstions::empty();
+    }
+
+public:
+    Value & at(DimValue index) TBAG_NOEXCEPT_SP_OP(BaseContainer::at(index))
+    {
+        return BaseContainer::at(index);
+    }
+
+    Value const & at(DimValue index) const TBAG_NOEXCEPT_SP_OP(BaseContainer::at(index))
+    {
+        return BaseContainer::at(index);
+    }
+
+    template <typename ... Args>
+    Value & atOffset(Args && ... args)
+    {
+        return BaseContainer::at(BaseDimenstions::offset({std::forward<Args>(args) ...}));
+    }
+
+    template <typename ... Args>
+    Value const & atOffset(Args && ... args) const
+    {
+        return BaseContainer::at(BaseDimenstions::offset({std::forward<Args>(args) ...}));
     }
 };
 
--- tester/libtbag/container/BagTest.cpp
+++ tester/libtbag/container/BagTest.cpp
@@ -11,6 +11,99 @@
 using namespace libtbag;
 using namespace libtbag::container;
 
+TEST(BagContainerTest, Default)
+{
+    using TestBagAllocator = std::allocator<int>;
+    using TestBagVector    = std::vector<int, TestBagAllocator>;
+    using TestBagContainer = BagContainer<int, std::allocator, TestBagVector>;
+
+    TestBagContainer container;
+    ASSERT_EQ(0, container.size());
+    ASSERT_TRUE(container.empty());
+
+    int const TEST_SIZE = 10;
+
+    container.resize(TEST_SIZE);
+    for (int i = 0; i < TEST_SIZE; ++i) {
+        container.at(i) = i;
+    }
+    ASSERT_EQ(TEST_SIZE, container.size());
+    ASSERT_FALSE(container.empty());
+    for (int i = 0; i < TEST_SIZE; ++i) {
+        ASSERT_EQ(i, *(container.data() + i));
+    }
+
+    TestBagContainer container2;
+    ASSERT_EQ(0, container2.size());
+    ASSERT_TRUE(container2.empty());
+
+    container2.swap(container);
+    ASSERT_EQ(TEST_SIZE, container2.size());
+    ASSERT_FALSE(container2.empty());
+    ASSERT_EQ(0, container.size());
+    ASSERT_TRUE(container.empty());
+    for (int i = 0; i < TEST_SIZE; ++i) {
+        ASSERT_EQ(i, container2.at(i));
+    }
+
+    container.copy(container2);
+    ASSERT_EQ(TEST_SIZE, container.size());
+    ASSERT_FALSE(container.empty());
+    ASSERT_EQ(TEST_SIZE, container2.size());
+    ASSERT_FALSE(container2.empty());
+
+    container.clear();
+    ASSERT_EQ(0, container.size());
+    ASSERT_TRUE(container.empty());
+
+    container2.clear();
+    ASSERT_EQ(0, container2.size());
+    ASSERT_TRUE(container2.empty());
+}
+
+TEST(BagDimensions, Default)
+{
+    using TestBagDimensions = BagDimensions<8>;
+    ASSERT_EQ(8, TestBagDimensions::maxDimSize());
+
+    TestBagDimensions dims;
+    ASSERT_EQ(0, dims.size());
+    ASSERT_EQ(0, dims.total());
+    ASSERT_TRUE(dims.empty());
+
+    dims.resize(2, 3, 4, 5);
+    ASSERT_EQ(2, dims.at(0));
+    ASSERT_EQ(3, dims.at(1));
+    ASSERT_EQ(4, dims.at(2));
+    ASSERT_EQ(5, dims.at(3));
+
+    ASSERT_EQ(4, dims.size());
+    ASSERT_EQ(2*3*4*5, dims.total());
+    ASSERT_FALSE(dims.empty());
+
+    ASSERT_EQ(0, dims.offset(0));
+    ASSERT_EQ(1, dims.offset(1));
+    ASSERT_EQ((2)+(1), dims.offset(1, 1));
+    ASSERT_EQ((2*3)+(2)+(1), dims.offset(1, 1, 1));
+
+    dims.clear();
+    ASSERT_EQ(0, dims.size());
+    ASSERT_EQ(0, dims.total());
+    ASSERT_TRUE(dims.empty());
+
+    TestBagDimensions dims2;
+    dims2.resize(2, 3);
+    ASSERT_EQ(2, dims2.size());
+
+    dims.swap(dims2);
+    ASSERT_EQ(2, dims.size());
+    ASSERT_EQ(0, dims2.size());
+
+    dims2.copy(dims);
+    ASSERT_EQ(2, dims.size());
+    ASSERT_EQ(2, dims2.size());
+}
+
 template <typename T>
 struct BagTest : public testing::Test
 {
@@ -18,20 +111,96 @@ struct BagTest : public testing::Test
 
     virtual void SetUp() override
     {
-        // EMPTY.
+        bag.resize(2, 3, 4);
+        for (int i = 0; i < bag.size(); ++i) {
+            bag.at(i) = i;
+        }
     }
 
     virtual void TearDown() override
     {
-        // EMPTY.
+        bag.clear();
     }
 };
 
-using BagTypes = testing::Types<char, int, unsigned int>;
+using BagTypes = testing::Types<char, int, unsigned, double>;
 TYPED_TEST_CASE(BagTest, BagTypes);
 
 TYPED_TEST(BagTest, Default)
 {
-    //ASSERT_EQ(0, this->bag.size());
+    auto & b = this->bag;
+    ASSERT_EQ(2*3*4, b.size());
+    ASSERT_EQ(2, b.size(0));
+    ASSERT_EQ(3, b.size(1));
+    ASSERT_EQ(4, b.size(2));
+    ASSERT_FALSE(b.empty());
+}
+
+TYPED_TEST(BagTest, Elements)
+{
+    auto & b = this->bag;
+
+    ASSERT_EQ( 0, b.atOffset(0, 0, 0));
+    ASSERT_EQ( 1, b.atOffset(1, 0, 0));
+    ASSERT_EQ( 2, b.atOffset(0, 1, 0));
+    ASSERT_EQ( 3, b.atOffset(1, 1, 0));
+    ASSERT_EQ( 4, b.atOffset(0, 2, 0));
+    ASSERT_EQ( 5, b.atOffset(1, 2, 0));
+
+    ASSERT_EQ( 6, b.atOffset(0, 0, 1));
+    ASSERT_EQ( 7, b.atOffset(1, 0, 1));
+    ASSERT_EQ( 8, b.atOffset(0, 1, 1));
+    ASSERT_EQ( 9, b.atOffset(1, 1, 1));
+    ASSERT_EQ(10, b.atOffset(0, 2, 1));
+    ASSERT_EQ(11, b.atOffset(1, 2, 1));
+
+    ASSERT_EQ(12, b.atOffset(0, 0, 2));
+    ASSERT_EQ(13, b.atOffset(1, 0, 2));
+    ASSERT_EQ(14, b.atOffset(0, 1, 2));
+    ASSERT_EQ(15, b.atOffset(1, 1, 2));
+    ASSERT_EQ(16, b.atOffset(0, 2, 2));
+    ASSERT_EQ(17, b.atOffset(1, 2, 2));
+
+    ASSERT_EQ(18, b.atOffset(0, 0, 3));
+    ASSERT_EQ(19, b.atOffset(1, 0, 3));
+    ASSERT_EQ(20, b.atOffset(0, 1, 3));
+    ASSERT_EQ(21, b.atOffset(1, 1, 3));
+    ASSERT_EQ(22, b.atOffset(0, 2, 3));
+    ASSERT_EQ(23, b.atOffset(1, 2, 3));
+}
+
+TEST(BagTest_NoTyped, Default)
+{
+    using TestBag = Bag<int>;
+
+    TestBag bag1;
+    ASSERT_EQ(0, bag1.size());
+    ASSERT_TRUE(bag1.empty());
+
+    TestBag bag2 = {2, 3};
+    ASSERT_EQ(2*3, bag2.size());
+    ASSERT_FALSE(bag2.empty());
+
+    TestBag bag3 = bag2;
+    ASSERT_EQ(2*3, bag3.size());
+    ASSERT_FALSE(bag3.empty());
+
+    TestBag bag4 = std::move(bag3);
+    ASSERT_EQ(0, bag3.size());
+    ASSERT_TRUE(bag3.empty());
+    ASSERT_EQ(2*3, bag4.size());
+    ASSERT_FALSE(bag4.empty());
+
+    TestBag bag5;
+    bag5 = bag4;
+    ASSERT_EQ(2*3, bag5.size());
+    ASSERT_FALSE(bag5.empty());
+
+    TestBag bag6;
+    bag6 = std::move(bag5);
+    ASSERT_EQ(2*3, bag6.size());
+    ASSERT_FALSE(bag6.empty());
+    ASSERT_EQ(0, bag5.size());
+    ASSERT_TRUE(bag5.empty());
 }
 
