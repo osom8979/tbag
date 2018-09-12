f828f3575efcd67248041ae42ee0bf47ee0f0174
zer0 <osom8979@gmail.com>
2018-09-12 16:04:01 +0900

    Fixed include directory error in FindLive555.cmake

--- cmake/TbagFindPackage/FindLive555.cmake
+++ cmake/TbagFindPackage/FindLive555.cmake
@@ -1,7 +1,10 @@
 ## Find the Live555 (OpenRTSP) library.
 #
 # The following variables are optionally searched for defaults
-#  Live555_ROOT
+#  BasicUsageEnvironment_ROOT
+#  UsageEnvironment_ROOT
+#  Groupsock_ROOT
+#  LiveMedia_ROOT
 #
 # The following are set after configuration is done:
 #  Live555_FOUND
@@ -12,15 +15,38 @@ if (Live555_FOUND)
     return ()
 endif ()
 
-set (__headers BasicUsageEnvironment/BasicUsageEnvironment.hh
-               UsageEnvironment/UsageEnvironment.hh
-               groupsock/Groupsock.hh
-               liveMedia/liveMedia.hh)
-set (__libs    BasicUsageEnvironment
-               UsageEnvironment
-               groupsock
-               liveMedia)
-
 include (TbagSimpleFindLibrary)
-tbag_simple_find_library_with_module_prefix ("${__headers}" "${__libs}")
+tbag_simple_find_library (BasicUsageEnvironment BasicUsageEnvironment/BasicUsageEnvironment.hh BasicUsageEnvironment)
+tbag_simple_find_library (UsageEnvironment UsageEnvironment/UsageEnvironment.hh UsageEnvironment)
+tbag_simple_find_library (Groupsock groupsock/Groupsock.hh Groupsock)
+tbag_simple_find_library (LiveMedia liveMedia/liveMedia.hh liveMedia)
+
+if (BasicUsageEnvironment_FOUND OR UsageEnvironment_FOUND OR Groupsock_FOUND OR LiveMedia_FOUND)
+    set (Live555_FOUND ON)
+else ()
+    set (Live555_FOUND OFF)
+endif ()
+
+set (Live555_INCLUDE_DIRS)
+set (Live555_LIBRARIES)
+if (BasicUsageEnvironment_FOUND)
+    list (APPEND Live555_INCLUDE_DIRS ${BasicUsageEnvironment_INCLUDE_DIRS}/BasicUsageEnvironment)
+    list (APPEND Live555_LIBRARIES ${BasicUsageEnvironment_LIBRARIES})
+endif ()
+if (UsageEnvironment_FOUND)
+    list (APPEND Live555_INCLUDE_DIRS ${UsageEnvironment_INCLUDE_DIRS}/UsageEnvironment)
+    list (APPEND Live555_LIBRARIES ${UsageEnvironment_LIBRARIES})
+endif ()
+if (Groupsock_FOUND)
+    list (APPEND Live555_INCLUDE_DIRS ${Groupsock_INCLUDE_DIRS}/groupsock)
+    list (APPEND Live555_LIBRARIES ${Groupsock_LIBRARIES})
+endif ()
+if (LiveMedia_FOUND)
+    list (APPEND Live555_INCLUDE_DIRS ${LiveMedia_INCLUDE_DIRS}/liveMedia)
+    list (APPEND Live555_LIBRARIES ${LiveMedia_LIBRARIES})
+endif ()
+
+mark_as_advanced (Live555_FOUND
+                  Live555_LIBRARIES
+                  Live555_INCLUDE_DIRS)
 
