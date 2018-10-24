c482b472492cb859ef1c73bd7b4105c484c55eb1
zer0 <osom8979@gmail.com>
2018-10-24 21:00:47 +0900

    Create TbagPacketBuilder & TbagPacketParser classes.

--- libtbag/proto/TbagPacket.cpp
+++ libtbag/proto/TbagPacket.cpp
@@ -5,8 +5,42 @@
  * @date   2018-10-24
  */
 
+#if defined(TBAG_COMP_MSVC)
+# if defined(min)
+TBAG_PUSH_MACRO(min);
+# undef min
+# define __RESTORE_MIN__
+# endif // defined(min)
+# if defined(max)
+TBAG_PUSH_MACRO(max);
+# undef max
+# define __RESTORE_MAX__
+# endif // defined(max)
+#endif // defined(TBAG_COMP_MSVC)
+
+// FlatBuffers generated files.
+#include <flatbuffers/flatbuffers.h>
+#include <flatbuffers/idl.h>
+#include <libtbag/proto/fbs/tbag_generated.h>
+#include <libtbag/proto/fbs/tbag_t2s.h>
+
+#if defined(TBAG_COMP_MSVC)
+# if defined(__RESTORE_MIN__)
+TBAG_POP_MACRO(min);
+# undef __RESTORE_MIN__
+# endif // defined(__RESTORE_MIN__)
+# if defined(__RESTORE_MAX__)
+TBAG_POP_MACRO(max);
+# undef __RESTORE_MAX__
+# endif // defined(__RESTORE_MAX__)
+#endif // defined(TBAG_COMP_MSVC)
+
 #include <libtbag/proto/TbagPacket.hpp>
 #include <libtbag/log/Log.hpp>
+#include <libtbag/debug/Assert.hpp>
+#include <libtbag/Type.hpp>
+
+#include <cassert>
 
 // -------------------
 NAMESPACE_LIBTBAG_OPEN
@@ -14,14 +48,164 @@ NAMESPACE_LIBTBAG_OPEN
 
 namespace proto {
 
-TbagPacket::TbagPacket()
+using namespace proto::fbs;
+
+using FlatBufferBuilder = flatbuffers::FlatBufferBuilder;
+using FlatBufferParser  = flatbuffers::Parser;
+
+using StringOffset       = flatbuffers::Offset<flatbuffers::String>;
+using StringOffsetVector = flatbuffers::Vector<StringOffset>;
+
+/**
+ * FlatBuffer builder implementation.
+ *
+ * @author zer0
+ * @date   2018-10-24
+ */
+struct TbagPacketBuilder::Impl
+{
+private:
+    TbagPacketBuilder * _parent = nullptr;
+
+private:
+    FlatBufferBuilder builder;
+    FlatBufferParser  parser;
+
+public:
+    Impl(TbagPacketBuilder * parent, std::size_t capacity) : _parent(parent), builder(capacity, nullptr)
+    {
+        if (parser.Parse(__get_text_to_cpp11_string__tbag__()) == false) {
+            tDLogA("TbagPacketBuilder::Impl() Parse fail.");
+            throw std::bad_alloc();
+        }
+    }
+
+    ~Impl()
+    {
+        // EMPTY.
+    }
+
+public:
+    uint8_t * point() const
+    {
+        return builder.GetBufferPointer();
+    }
+
+    std::size_t size() const
+    {
+        return builder.GetSize();
+    }
+
+    void clear()
+    {
+        builder.Clear();
+    }
+
+    std::string toJsonString() const
+    {
+        std::string result;
+        if (flatbuffers::GenerateText(parser, builder.GetBufferPointer(), &result)) {
+            return result;
+        }
+        return std::string();
+    }
+
+    template <typename T>
+    void finish(flatbuffers::Offset<T> root, char const * file_identifier = nullptr)
+    {
+        builder.Finish(root, file_identifier);
+    }
+};
+
+// ---------------------------------
+// TbagPacketBuilder implementation.
+// ---------------------------------
+
+TbagPacketBuilder::TbagPacketBuilder(std::size_t capacity) : _impl(std::make_unique<Impl>(this, capacity))
+{
+    assert(static_cast<bool>(_impl));
+}
+
+TbagPacketBuilder::~TbagPacketBuilder()
 {
     // EMPTY.
 }
 
-TbagPacket::~TbagPacket()
+uint8_t * TbagPacketBuilder::point() const
+{
+    assert(exists());
+    return _impl->point();
+}
+
+std::size_t TbagPacketBuilder::size() const
+{
+    assert(exists());
+    return _impl->size();
+}
+
+std::string TbagPacketBuilder::toJsonString() const
+{
+    assert(exists());
+    return _impl->toJsonString();
+}
+
+void TbagPacketBuilder::clear()
+{
+    assert(exists());
+    _impl->clear();
+}
+
+void TbagPacketBuilder::finish()
+{
+    assert(exists());
+}
+
+/**
+ * FlatBuffer parser implementation.
+ *
+ * @author zer0
+ * @date   2018-10-24
+ */
+struct TbagPacketParser::Impl
+{
+private:
+    TbagPacketParser * _parent = nullptr;
+
+public:
+    Impl(TbagPacketParser * parent) : _parent(parent)
+    {
+        // EMPTY.
+    }
+
+    ~Impl()
+    {
+        // EMPTY.
+    }
+
+public:
+    Err parse(char const * buffer, std::size_t size, void * arg)
+    {
+        return Err::E_SUCCESS;
+    }
+};
+
+// --------------------------------
+// TbagPacketParser implementation.
+// --------------------------------
+
+TbagPacketParser::TbagPacketParser() : _impl(std::make_unique<Impl>(this))
+{
+    assert(static_cast<bool>(_impl));
+}
+
+TbagPacketParser::~TbagPacketParser()
 {
     // EMPTY.
+}
+
+Err TbagPacketParser::parse(char const * buffer, std::size_t size, void * arg)
+{
+    return Err::E_SUCCESS;
 }
 
 } // namespace proto
--- libtbag/proto/TbagPacket.hpp
+++ libtbag/proto/TbagPacket.hpp
@@ -16,6 +16,11 @@
 #include <libtbag/config.h>
 #include <libtbag/predef.hpp>
 #include <libtbag/Noncopyable.hpp>
+#include <libtbag/Err.hpp>
+#include <libtbag/Unit.hpp>
+
+#include <string>
+#include <memory>
 
 // -------------------
 NAMESPACE_LIBTBAG_OPEN
@@ -23,20 +28,100 @@ NAMESPACE_LIBTBAG_OPEN
 
 namespace proto {
 
+struct TbagPacketTypes : private Noncopyable
+{
+    // EMPTY.
+};
+
+/**
+ * TbagPacketBuilder class prototype.
+ *
+ * @author zer0
+ * @date   2018-10-24
+ */
+class TBAG_API TbagPacketBuilder : public TbagPacketTypes
+{
+public:
+    struct Impl;
+    friend struct Impl;
+    using UniqueImpl = std::unique_ptr<Impl>;
+
+public:
+    TBAG_CONSTEXPR static std::size_t const DEFAULT_BUILDER_CAPACITY = 1 * MEGA_BYTE_TO_BYTE;
+    TBAG_CONSTEXPR static char const * const DEFAULT_ECHO_MESSAGE = "TBAG";
+
+    TBAG_CONSTEXPR static unsigned int const TBAG_VER_MAJOR = LIBTBAG_VERSION_PACKET_MAJOR;
+    TBAG_CONSTEXPR static unsigned int const TBAG_VER_MINOR = LIBTBAG_VERSION_PACKET_MINOR;
+
+private:
+    UniqueImpl _impl;
+
+public:
+    TbagPacketBuilder(std::size_t capacity = DEFAULT_BUILDER_CAPACITY);
+    virtual ~TbagPacketBuilder();
+
+public:
+    inline bool exists() const TBAG_NOEXCEPT
+    { return static_cast<bool>(_impl); }
+
+    inline operator bool() const TBAG_NOEXCEPT
+    { return exists(); }
+
+public:
+    uint8_t * point() const;
+    std::size_t size() const;
+
+public:
+    std::string toJsonString() const;
+
+public:
+    void clear();
+    void finish();
+};
+
+/**
+ * TbagPacketParser class prototype.
+ *
+ * @author zer0
+ * @date   2018-10-24
+ */
+class TBAG_API TbagPacketParser : public TbagPacketTypes
+{
+public:
+    class Impl;
+    friend class Impl;
+    using UniqueImpl = std::unique_ptr<Impl>;
+
+private:
+    UniqueImpl _impl;
+
+public:
+    TbagPacketParser();
+    virtual ~TbagPacketParser();
+
+public:
+    Err parse(char const * buffer, std::size_t size, void * arg = nullptr);
+};
+
 /**
  * TbagPacket class prototype.
  *
  * @author zer0
  * @date   2018-10-24
  */
-class TBAG_API TbagPacket : private Noncopyable
+class TBAG_API TbagPacket : public TbagPacketBuilder, public TbagPacketParser
 {
-private:
-    // Insert member variables.
-
 public:
-    TbagPacket();
-    virtual ~TbagPacket();
+    TbagPacket(std::size_t capacity = DEFAULT_BUILDER_CAPACITY)
+            : TbagPacketBuilder(capacity), TbagPacketParser()
+    {
+        // EMPTY.
+    }
+
+    virtual ~TbagPacket()
+    {
+        // EMPTY.
+    }
 };
 
 } // namespace proto
