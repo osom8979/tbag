74915e803f06bdb0d36cc5cb83b92c2b12e9a014
zer0 <osom8979@gmail.com>
2018-10-26 21:15:36 +0900

    Update event callback to the Node class.

--- libtbag/network/node/Node.cpp
+++ libtbag/network/node/Node.cpp
@@ -39,10 +39,15 @@ public:
     using ThreadPool = libtbag::thread::ThreadPool;
 
 public:
-    using HttpServer = libtbag::network::http::HttpServer;
-    using HttpClient = libtbag::network::http::HttpClient;
-    using StreamType = libtbag::network::details::StreamType;
-    using Loop       = libtbag::uvpp::Loop;
+    using HttpServer   = libtbag::network::http::HttpServer;
+    using HttpClient   = libtbag::network::http::HttpClient;
+    using StreamType   = libtbag::network::details::StreamType;
+    using HttpRequest  = libtbag::network::http::HttpRequest;
+    using HttpResponse = libtbag::network::http::HttpResponse;
+    using HttpProperty = libtbag::network::http::HttpProperty;
+    using WsOpCode     = libtbag::network::http::ws::WsOpCode;
+    using EventType    = libtbag::network::http::EventType;
+    using Loop         = libtbag::uvpp::Loop;
 
 public:
     struct Server;
@@ -60,12 +65,106 @@ public:
      */
     struct Server : public HttpServer
     {
-        Server(Loop & loop, StreamType type = StreamType::TCP) : HttpServer(loop, type)
+        using Base = HttpServer;
+
+        Server(Loop & loop, StreamType type = StreamType::TCP) : Base(loop, type)
         {
             // EMPTY.
         }
 
         virtual ~Server()
+        {
+            // EMPTY.
+        }
+
+        // -----------------------
+        // ServerInterface events.
+        // -----------------------
+
+        virtual void onConnection(Err code) override
+        {
+            Base::onConnection(code);
+        }
+
+        virtual void onClientShutdown(WeakClient node, Err code) override
+        {
+            // EMPTY.
+        }
+
+        virtual void onClientWrite(WeakClient node, Err code) override
+        {
+            // EMPTY.
+        }
+
+        // Don't implement this method.
+        // virtual void onClientRead(WeakClient node, Err code, ReadPacket const & packet) override
+        // { /* EMPTY. */ }
+
+        virtual void onClientClose(WeakClient node) override
+        {
+            // EMPTY.
+        }
+
+        virtual void onClientTimer(WeakClient node) override
+        {
+            // EMPTY.
+        }
+
+        virtual void onServerClose() override
+        {
+            // EMPTY.
+        }
+
+        // ------------------
+        // HttpServer events.
+        // ------------------
+
+        SharedStreamNode createClient(StreamType type, Loop & loop, SharedStream & server) override
+        {
+            assert(static_cast<bool>(server));
+            return SharedStreamNode(new (std::nothrow) HttpNode(loop, type, this));
+        }
+
+        virtual void onClientEof(WeakClient node) override
+        {
+            Base::onClientEof(node);
+        }
+
+        virtual void onClientReadError(WeakClient node, Err code) override
+        {
+            Base::onClientReadError(node, code);
+        }
+
+        virtual void onClientParseError(WeakClient node, Err code) override
+        {
+            Base::onClientParseError(node, code);
+        }
+
+        virtual bool onClientSwitchingProtocol(WeakClient node, HttpRequest const & request) override
+        {
+            return Base::onClientSwitchingProtocol(node, request);
+        }
+
+        // ----------------------------
+        // HttpServer extension events.
+        // ----------------------------
+
+        virtual void onClientOpen(WeakClient node) override
+        {
+            // EMPTY.
+        }
+
+        virtual void onClientContinue(WeakClient node) override
+        {
+            // EMPTY.
+        }
+
+        virtual void onClientWsMessage(WeakClient node, WsOpCode opcode, util::Buffer const & payload) override
+        {
+            // EMPTY.
+        }
+
+        virtual void onClientRequest(WeakClient node, HttpRequest const & request) override
         {
             // EMPTY.
         }
@@ -79,7 +178,9 @@ public:
      */
     struct Client : public HttpClient
     {
-        Client(Loop & loop, StreamType type = StreamType::TCP) : HttpClient(loop, type)
+        using Base = HttpClient;
+
+        Client(Loop & loop, StreamType type = StreamType::TCP) : Base(loop, type)
         {
             // EMPTY.
         }
@@ -87,6 +188,88 @@ public:
         virtual ~Client()
         {
             // EMPTY.
+        }
+
+        // -----------------------
+        // ClientInterface events.
+        // -----------------------
+
+        virtual void onConnect(Err code) override
+        {
+            Base::onConnect(code);
+        }
+
+        virtual void onShutdown(Err code) override
+        {
+            // EMPTY.
+        }
+
+        virtual void onWrite(Err code) override
+        {
+            // EMPTY.
+        }
+
+        virtual void onRead(Err code, ReadPacket const & packet) override
+        {
+            Base::onRead(code, packet);
+        }
+
+        virtual void onClose() override
+        {
+            // EMPTY.
+        }
+
+        virtual void onTimer() override
+        {
+            // EMPTY.
+        }
+
+        // ----------------------------
+        // HttpClient extension events.
+        // ----------------------------
+
+        virtual void onOpen() override
+        {
+            // EMPTY.
+        }
+
+        virtual void onEof() override
+        {
+            Base::onEof();
+        }
+
+        virtual void onError(EventType from, Err code) override
+        {
+            Base::onError(from, code);
+        }
+
+        // ---------------------------
+        // HttpReaderInterface events.
+        // ---------------------------
+
+        virtual void onContinue(void * arg) override
+        {
+            // EMPTY.
+        }
+
+        virtual bool onSwitchingProtocol(HttpProperty const & property, void * arg) override
+        {
+            return Base::onSwitchingProtocol(property, arg);
+        }
+
+        virtual void onWsMessage(WsOpCode opcode, util::Buffer const & payload, void * arg) override
+        {
+            // EMPTY.
+        }
+
+        virtual void onRegularHttp(HttpProperty const & property, void * arg) override
+        {
+            // EMPTY.
+        }
+
+        virtual void onParseError(Err code, void * arg) override
+        {
+            Base::onParseError(code, arg);
         }
     };
 
