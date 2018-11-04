b4bc965a28284ea9d19aeac072ad3d401db880f9
zer0 <osom8979@gmail.com>
2018-11-04 23:10:03 +0900

    Create StorageNodeTest.Default tester.

--- tester/libtbag/res/node/StorageNodeTest.cpp
+++ tester/libtbag/res/node/StorageNodeTest.cpp
@@ -7,17 +7,27 @@
 
 #include <gtest/gtest.h>
 #include <tester/DemoAsset.hpp>
+#include <libtbag/filesystem/File.hpp>
 #include <libtbag/res/node/StorageNode.hpp>
 
 using namespace libtbag;
 using namespace libtbag::res;
 using namespace libtbag::res::node;
 
+TBAG_CONSTEXPR static char const * const STORAGE_NODE_XML = R"(
+<storage default_root='dir' archive='dir'>
+  <env name='env1.xml' default='true' system='true' />
+  <config />
+</storage>
+)";
+
 TEST(StorageNodeTest, Default)
 {
     tttDir_Automatic();
-    auto path = tttDir_Get() / "test";
+    auto const PATH = tttDir_Get() / "filename.xml";
+    ASSERT_EQ(Err::E_SUCCESS, libtbag::filesystem::writeFile(PATH.toString(), STORAGE_NODE_XML));
 
-    StorageNode object;
+    StorageNode node;
+    ASSERT_TRUE(node.loadFile(PATH));
 }
 
