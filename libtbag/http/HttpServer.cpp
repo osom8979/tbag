b0784a9806cede995ccbdcbb221f675c97041bf4
zer0 <osom8979@gmail.com>
2019-01-12 15:45:52 +0900

    Create libtbag/http/HttpServer class.

---
+++ libtbag/http/HttpServer.cpp
@@ -0,0 +1,312 @@
+/**
+ * @file   HttpServer.cpp
+ * @brief  HttpServer class implementation.
+ * @author zer0
+ * @date   2019-01-11
+ */
+
+#include <libtbag/http/HttpServer.hpp>
+#include <libtbag/log/Log.hpp>
+#include <libtbag/net/socket/NetStreamServer.hpp>
+#include <libtbag/http/HttpReader.hpp>
+#include <libtbag/Type.hpp>
+
+#include <cassert>
+#include <unordered_map>
+#include <algorithm>
+#include <utility>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace http {
+
+/**
+ * HttpServer::Impl class implementation.
+ *
+ * @author zer0
+ * @date   2019-01-11
+ */
+struct HttpServer::Impl : public libtbag::net::socket::NetStreamServer
+{
+public:
+    using NetStreamServer     = libtbag::net::socket::NetStreamServer;
+    using HttpReaderInterface = libtbag::http::HttpReaderInterface;
+
+public:
+    STATIC_ASSERT_CHECK_IS_SAME(Buffer, HttpReaderInterface::Buffer);
+
+public:
+    /**
+     * Client node properties.
+     *
+     * @author zer0
+     * @date   2019-01-11
+     */
+    struct Node : public HttpReaderInterface
+    {
+    public:
+        using Reader = libtbag::http::HttpReaderForCallback<Node>;
+
+    public:
+        Impl * const IMPL;
+        std::intptr_t const ID;
+
+    private:
+        Reader _reader;
+
+    public:
+        Node(Impl * impl, std::intptr_t id, std::string const & key, bool use_websocket)
+                : IMPL(impl), ID(id), _reader(this, key, use_websocket)
+        { assert(IMPL != nullptr); }
+
+        ~Node()
+        { /* EMPTY. */ }
+
+    public:
+        Err parse(char const * buffer, std::size_t size)
+        { return _reader.parse(buffer, size); }
+
+    public:
+        virtual void onContinue(void * arg) override
+        { return IMPL->onContinue(this); }
+
+        virtual bool onSwitchingProtocol(HttpProperty const & property, void * arg) override
+        { return IMPL->onSwitchingProtocol(this, property); }
+
+        virtual void onWsMessage(WsOpCode opcode, Buffer const & payload, void * arg) override
+        { return IMPL->onWsMessage(this, opcode, payload); }
+
+        virtual void onRegularHttp(HttpProperty const & property, void * arg) override
+        { return IMPL->onRegularHttp(this, property); }
+
+        virtual void onParseError(Err code, void * arg) override
+        { return IMPL->onParseError(this, code); }
+    };
+
+public:
+    using SharedNode = std::shared_ptr<Node>;
+    using NodeMap    = std::unordered_map<std::intptr_t, SharedNode>;
+
+public:
+    HttpServer * const PARENT;
+    std::string  const KEY;
+    bool         const ENABLE_WEBSOCKET;
+
+private:
+    NodeMap _nodes;
+
+public:
+    Impl(HttpServer * parent, MqParams const & params, std::string const & key, bool use_websocket)
+            : NetStreamServer(params), PARENT(parent), KEY(key), ENABLE_WEBSOCKET(use_websocket)
+    {
+        assert(PARENT != nullptr);
+    }
+
+    ~Impl()
+    {
+        // EMPTY.
+    }
+
+protected:
+    virtual bool onAccept(std::intptr_t id, std::string const & ip) override
+    {
+        if (!PARENT->onAccept(id, ip)) {
+            return false;
+        }
+
+        SharedNode node;
+        if (ENABLE_WEBSOCKET) {
+            if (KEY.empty()) {
+                node = std::make_shared<Node>(this, id, libtbag::http::generateRandomWebSocketKey(), true);
+            } else {
+                node = std::make_shared<Node>(this, id, KEY, true);
+            }
+        } else {
+            node = std::make_shared<Node>(this, id, std::string(), false);
+        }
+        assert(static_cast<bool>(node));
+
+        bool const INSERT_RESULT = _nodes.insert(std::make_pair(id, node)).second;
+        assert(INSERT_RESULT);
+
+        return true;
+    }
+
+    virtual void onRecv(std::intptr_t id, char const * buffer, std::size_t size) override
+    {
+        auto itr = _nodes.find(id);
+        assert(itr != _nodes.end());
+
+        auto & node = itr->second;
+        assert(static_cast<bool>(node));
+
+        node->parse(buffer, size);
+    }
+
+    virtual void onClose(std::intptr_t id) override
+    {
+        auto const ERASE_RESULT = _nodes.erase(id);
+        assert(ERASE_RESULT == 1);
+    }
+
+private:
+    void onContinue(Node * node)
+    {
+        assert(node != nullptr);
+    }
+
+    bool onSwitchingProtocol(Node * node, HttpProperty const & property)
+    {
+        assert(node != nullptr);
+        return true;
+    }
+
+    void onWsMessage(Node * node, WsOpCode opcode, Buffer const & payload)
+    {
+        assert(node != nullptr);
+    }
+
+    void onRegularHttp(Node * node, HttpProperty const & property)
+    {
+        assert(node != nullptr);
+
+        auto response = PARENT->onRegularHttp(static_cast<HttpRequest>(property));
+        libtbag::http::updateDefaultResponse(response);
+
+        auto const CODE = send(libtbag::http::toResponseString(response), node->ID);
+        if (isFailure(CODE)) {
+            onParseError(node, CODE);
+        }
+    }
+
+    void onParseError(Node * node, Err code)
+    {
+        assert(node != nullptr);
+    }
+};
+
+// --------------------------
+// HttpServer implementation.
+// --------------------------
+
+using HttpResponse = HttpServer::HttpResponse;
+
+HttpServer::HttpServer(MqParams const & params, bool use_websocket)
+        : HttpServer(params, std::string(), use_websocket)
+{
+    assert(static_cast<bool>(_impl));
+}
+
+HttpServer::HttpServer(MqParams const & params, std::string const & key, bool use_websocket)
+        : _impl(std::make_unique<Impl>(this, params, key, use_websocket))
+{
+    assert(static_cast<bool>(_impl));
+}
+
+HttpServer::~HttpServer()
+{
+    // EMPTY.
+}
+
+bool HttpServer::onAccept(std::intptr_t id, std::string const & ip)
+{
+    return true;
+}
+
+void HttpServer::onClose(std::intptr_t id)
+{
+    // EMPTY.
+}
+
+HttpResponse HttpServer::onRegularHttp(HttpRequest const & request)
+{
+    return HttpResponse{};
+}
+
+bool HttpServer::onSwitchingProtocol(HttpRequest const & request)
+{
+    return true;
+}
+
+void HttpServer::onWsMessage(std::intptr_t id, WsOpCode opcode, Buffer const & payload)
+{
+    // EMPTY.
+}
+
+void HttpServer::onError(Err code)
+{
+    // EMPTY.
+}
+
+Err HttpServer::writeWsFrame(std::intptr_t id, WsFrame const & frame)
+{
+    assert(static_cast<bool>(_impl));
+    if (!_impl->ENABLE_WEBSOCKET) {
+        return Err::E_ILLSTATE;
+    }
+
+    Buffer buffer;
+    auto const SIZE = frame.copyTo(buffer);
+
+    if (SIZE == 0) {
+        tDLogE("HttpServer::writeWsFrame() WsFrame -> Buffer copy error");
+        return Err::E_ECOPY;
+    }
+    return _impl->send(buffer.data(), buffer.size(), id);
+}
+
+Err HttpServer::writeText(std::intptr_t id, char const * buffer, std::size_t size, bool finish)
+{
+    WsFrame frame;
+    frame.text(buffer, size, finish);
+    return writeWsFrame(id, frame);
+}
+
+Err HttpServer::writeText(std::intptr_t id, std::string const & text, bool finish)
+{
+    WsFrame frame;
+    frame.text(text, finish);
+    return writeWsFrame(id, frame);
+}
+
+Err HttpServer::writeBinary(std::intptr_t id, char const * buffer, std::size_t size, bool finish)
+{
+    WsFrame frame;
+    frame.binary(buffer, size, finish);
+    return writeWsFrame(id, frame);
+}
+
+Err HttpServer::writeBinary(std::intptr_t id, Buffer const & buffer, bool finish)
+{
+    WsFrame frame;
+    frame.binary(buffer, finish);
+    return writeWsFrame(id, frame);
+}
+
+Err HttpServer::writeClose(std::intptr_t id, WsStatus const & status)
+{
+    WsFrame frame;
+    frame.close(status);
+    return writeWsFrame(id, frame);
+}
+
+Err HttpServer::writeClose(std::intptr_t id, WsStatusCode code)
+{
+    WsFrame frame;
+    frame.close(code);
+    return writeWsFrame(id, frame);
+}
+
+Err HttpServer::writeClose(std::intptr_t id)
+{
+    return writeClose(id, WsStatusCode::WSSC_NORMAL_CLOSURE);
+}
+
+} // namespace http
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
---
+++ libtbag/http/HttpServer.hpp
@@ -0,0 +1,90 @@
+/**
+ * @file   HttpServer.hpp
+ * @brief  HttpServer class prototype.
+ * @author zer0
+ * @date   2019-01-11
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_HTTP_HTTPSERVER_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_HTTP_HTTPSERVER_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+#include <libtbag/predef.hpp>
+#include <libtbag/mq/details/MqCommon.hpp>
+#include <libtbag/http/HttpCommon.hpp>
+#include <libtbag/http/WsFrame.hpp>
+
+#include <memory>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace http {
+
+/**
+ * HttpServer class prototype.
+ *
+ * @author zer0
+ * @date   2019-01-11
+ */
+class TBAG_API HttpServer
+{
+public:
+    struct Impl;
+    friend struct Impl;
+
+public:
+    using UniqueImpl   = std::unique_ptr<Impl>;
+    using MqParams     = libtbag::mq::details::MqParams;
+    using HttpRequest  = libtbag::http::HttpRequest;
+    using HttpResponse = libtbag::http::HttpResponse;
+    using Buffer       = libtbag::util::Buffer;
+    using WsFrame      = libtbag::http::WsFrame;
+
+private:
+    UniqueImpl _impl;
+
+public:
+    HttpServer(MqParams const & params, bool use_websocket = false);
+    HttpServer(MqParams const & params, std::string const & key, bool use_websocket = false);
+    ~HttpServer();
+
+protected:
+    virtual bool onAccept(std::intptr_t id, std::string const & ip);
+    virtual void onClose(std::intptr_t id);
+
+protected:
+    virtual HttpResponse onRegularHttp(HttpRequest const & request);
+
+protected:
+    virtual bool onSwitchingProtocol(HttpRequest const & request);
+    virtual void onWsMessage(std::intptr_t id, WsOpCode opcode, Buffer const & payload);
+
+protected:
+    virtual void onError(Err code);
+
+public:
+    Err writeWsFrame(std::intptr_t id, WsFrame const & frame);
+    Err writeText   (std::intptr_t id, char const * buffer, std::size_t size, bool finish = true);
+    Err writeText   (std::intptr_t id, std::string const & text, bool finish = true);
+    Err writeBinary (std::intptr_t id, char const * buffer, std::size_t size, bool finish = true);
+    Err writeBinary (std::intptr_t id, Buffer const & buffer, bool finish = true);
+    Err writeClose  (std::intptr_t id, WsStatus const & status);
+    Err writeClose  (std::intptr_t id, WsStatusCode code);
+    Err writeClose  (std::intptr_t id);
+};
+
+} // namespace http
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_HTTP_HTTPSERVER_HPP__
+
