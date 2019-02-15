885378c1daab563dd4753314e6b39e7aaf3c2504
zer0 <osom8979@gmail.com>
2019-02-15 15:58:58 +0900

    Update tbag_simple_find_library() cmake method.

--- cmake/TbagFindPackage/TbagSimpleFindLibrary.cmake
+++ cmake/TbagFindPackage/TbagSimpleFindLibrary.cmake
@@ -7,18 +7,26 @@ include (TbagUtils)
 
 #/// Find headers function.
 #///
-#/// @param __result  [out] Variable result prefix.
-#/// @param __paths   [in]  Search directories.
-#/// @param __headers [in]  Find header files.
-function (tbag_find_headers __result __paths __headers)
+#/// @param __result     [out] Variable result prefix.
+#/// @param __paths      [in]  Search directories.
+#/// @param __headers    [in]  Find header files.
+#/// @param __no_default [in]  Enable NO_DEFAULT_PATH flag.
+function (tbag_find_headers __result __paths __headers __no_default)
     set (${__result})
     set (__name_index 0)
 
     foreach (__header_cursor ${__headers})
         set (__find_name "${__result}_${__name_index}")
-        find_path (${__find_name}
-                NAMES ${__header_cursor}
-                PATHS ${__paths})
+        if (${__no_default})
+            find_path (${__find_name}
+                    NAMES ${__header_cursor}
+                    PATHS ${__paths}
+                    NO_DEFAULT_PATH)
+        else ()
+            find_path (${__find_name}
+                    NAMES ${__header_cursor}
+                    PATHS ${__paths})
+        endif ()
         math (EXPR __name_index "${__name_index} + 1")
 
         if (IS_DIRECTORY "${${__find_name}}")
@@ -40,19 +48,28 @@ endfunction ()
 
 #/// Find libraries function.
 #///
-#/// @param __result [out] Variable result prefix.
-#/// @param __paths  [in]  Search directories.
-#/// @param __libs   [in]  Find library files.
-function (tbag_find_libraries __result __paths __libs)
+#/// @param __result     [out] Variable result prefix.
+#/// @param __paths      [in]  Search directories.
+#/// @param __libs       [in]  Find library files.
+#/// @param __no_default [in]  Enable NO_DEFAULT_PATH flag.
+function (tbag_find_libraries __result __paths __libs __no_default)
     set (${__result})
     set (__name_index 0)
 
     foreach (__lib_cursor ${__libs})
         set (__find_name ${__result}_${__name_index})
-        find_library (${__find_name}
-                NAMES "${__lib_cursor}"
-                PATHS ${__paths}
-                PATH_SUFFIXES Debug Release)
+        if (${__no_default})
+            find_library (${__find_name}
+                    NAMES "${__lib_cursor}"
+                    PATHS ${__paths}
+                    PATH_SUFFIXES Debug Release
+                    NO_DEFAULT_PATH)
+        else ()
+            find_library (${__find_name}
+                    NAMES "${__lib_cursor}"
+                    PATHS ${__paths}
+                    PATH_SUFFIXES Debug Release)
+        endif ()
         math (EXPR __name_index "${__name_index} + 1")
 
         if (EXISTS "${${__find_name}}")
@@ -83,6 +100,8 @@ endfunction ()
 #///   ${__prefix}_ROOT
 #///   ${__prefix}_ROOT_INCLUDE_PATHS
 #///   ${__prefix}_ROOT_LIBRARY_PATHS
+#///   ${__prefix}_ROOT_INCLUDE_PATHS_ONLY:BOOL
+#///   ${__prefix}_ROOT_LIBRARY_PATHS_ONLY:BOOL
 #///
 #///  The following are set after configuration is done:
 #///   ${__prefix}_FOUND
@@ -90,9 +109,20 @@ endfunction ()
 #///   ${__prefix}_LIBRARIES
 #///
 function (tbag_simple_find_library __prefix  __headers __libs)
-    set (__root_dir           "${__prefix}_ROOT")
-    set (__root_include_paths "${__prefix}_ROOT_INCLUDE_PATHS")
-    set (__root_library_paths "${__prefix}_ROOT_LIBRARY_PATHS")
+    set (__root_dir                "${__prefix}_ROOT")
+    set (__root_include_paths      "${__prefix}_ROOT_INCLUDE_PATHS")
+    set (__root_library_paths      "${__prefix}_ROOT_LIBRARY_PATHS")
+
+    if (${${__prefix}_ROOT_INCLUDE_PATHS_ONLY})
+        set (__root_include_paths_only ON)
+    else ()
+        set (__root_include_paths_only OFF)
+    endif ()
+    if (${${__prefix}_ROOT_LIBRARY_PATHS_ONLY})
+        set (__root_library_paths_only ON)
+    else ()
+        set (__root_library_paths_only OFF)
+    endif ()
 
     # Check 64bit library directories:
     if (CMAKE_SIZEOF_VOID_P EQUAL 8)
@@ -114,8 +144,15 @@ function (tbag_simple_find_library __prefix  __headers __libs)
                                  "/usr/lib"
                                  ${__lib64_paths})
 
-    tbag_find_headers   (${__prefix}_INCLUDE_DIRS "${__search_include_paths}" "${__headers}")
-    tbag_find_libraries (${__prefix}_LIBRARIES    "${__search_library_paths}" "${__libs}")
+    if (__root_include_paths_only)
+        set (__search_include_paths ${${__root_include_paths}})
+    endif ()
+    if (__root_library_paths_only)
+        set (__search_library_paths ${${__root_library_paths}})
+    endif ()
+
+    tbag_find_headers   (${__prefix}_INCLUDE_DIRS "${__search_include_paths}" "${__headers}" ${__root_include_paths_only})
+    tbag_find_libraries (${__prefix}_LIBRARIES    "${__search_library_paths}" "${__libs}"    ${__root_library_paths_only})
 
     set (__looked_for ${__prefix}_INCLUDE_DIRS)
     if (NOT "${__libs}" STREQUAL "")
