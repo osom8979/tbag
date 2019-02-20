4ce203a46d7aca8f8462ce0e7410ddb028ff45e9
zer0 <osom8979@gmail.com>
2019-02-20 19:32:20 +0900

    Implement Shader::loadFromFile() method.

--- libtbag/gui/Shader.cpp
+++ libtbag/gui/Shader.cpp
@@ -8,9 +8,16 @@
 #include <libtbag/gui/Shader.hpp>
 #include <libtbag/log/Log.hpp>
 
+#ifndef _STATIC_ASSERT_EQUAL
+#define _STATIC_ASSERT_EQUAL(x, y) STATIC_ASSERT_CHECK_IS_EQUALS((int)(x), (int)(y))
+#endif
+
 #if defined(USE_GUI)
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+_STATIC_ASSERT_EQUAL(sf::Shader::Vertex,   libtbag::gui::Shader::Type::T_VERTEX);
+_STATIC_ASSERT_EQUAL(sf::Shader::Geometry, libtbag::gui::Shader::Type::T_GEOMETRY);
+_STATIC_ASSERT_EQUAL(sf::Shader::Fragment, libtbag::gui::Shader::Type::T_FRAGMENT);
 #else
 #include <libtbag/dummy/Sfml.hpp>
 using namespace libtbag::dummy;
@@ -65,6 +72,36 @@ void Shader::swap(Shader & obj) TBAG_NOEXCEPT
     if (this != &obj) {
         SfNative::swap(obj);
     }
+}
+
+bool Shader::loadFromFile(std::string const & filename, Type type)
+{
+    return _self_sf()->loadFromFile(filename, (sf::Shader::Type)type);
+}
+
+bool Shader::loadFromFile(std::string const & vertex, std::string const & fragment)
+{
+    return _self_sf()->loadFromFile(vertex, fragment);
+}
+
+bool Shader::loadFromFile(std::string const & vertex, std::string const & geometry, std::string const & fragment)
+{
+    return _self_sf()->loadFromFile(vertex, geometry, fragment);
+}
+
+bool Shader::loadFromMemory(std::string const & shader, Type type)
+{
+    return _self_sf()->loadFromMemory(shader, (sf::Shader::Type)type);
+}
+
+bool Shader::loadFromMemory(std::string const & vertex, std::string const & fragment)
+{
+    return _self_sf()->loadFromMemory(vertex, fragment);
+}
+
+bool Shader::loadFromMemory(std::string const & vertex, std::string const & geometry, std::string const & fragment)
+{
+    return _self_sf()->loadFromMemory(vertex, geometry, fragment);
 }
 
 } // namespace gui
--- libtbag/gui/Shader.hpp
+++ libtbag/gui/Shader.hpp
@@ -23,6 +23,9 @@ NAMESPACE_LIBTBAG_OPEN
 
 namespace gui {
 
+// Forward declaration.
+class Texture;
+
 /**
  * Shader class prototype.
  *
@@ -31,6 +34,42 @@ namespace gui {
  */
 class TBAG_API Shader : public SfNative
 {
+public:
+    enum class Type
+    {
+        T_VERTEX,
+        T_GEOMETRY,
+        T_FRAGMENT
+    };
+
+    using Vec2  = libtbag::geometry::BasePoint<float>;
+    using IVec2 = libtbag::geometry::BasePoint<int>;
+    using BVec2 = libtbag::geometry::BasePoint<bool>;
+
+//    using Vec3  = libtbag::geometry::BasePoint3d<float>;
+//    using IVec3 = libtbag::geometry::BasePoint3d<int>;
+//    using BVec3 = libtbag::geometry::BasePoint3d<bool>;
+
+//    using Vec4  = libtbag::geometry::BasePoint4d<float>;
+//    using IVec4 = libtbag::geometry::BasePoint4d<int>;
+//    using BVec4 = libtbag::geometry::BasePoint4d<bool>;
+
+//    typedef Vector3<float> Vec3;
+//    typedef Vector3<int>   Ivec3;
+//    typedef Vector3<bool>  Bvec3;
+//
+//    typedef priv::Vector4<float> Vec4;
+//    typedef priv::Vector4<int>   Ivec4;
+//    typedef priv::Vector4<bool>  Bvec4;
+//    typedef priv::Matrix<3, 3> Mat3;
+//    typedef priv::Matrix<4, 4> Mat4;
+
+public:
+    struct current_texture_t {};
+
+public:
+    TBAG_CONSTEXPR static current_texture_t current_texture = current_texture_t{};
+
 public:
     Shader();
     Shader(void * handle, no_init_no_ref_t);
@@ -46,6 +85,51 @@ public:
 public:
     inline friend void swap(Shader & lh, Shader & rh) TBAG_NOEXCEPT
     { lh.swap(rh); }
+
+public:
+    bool loadFromFile(std::string const & filename, Type type);
+    bool loadFromFile(std::string const & vertex, std::string const & fragment);
+    bool loadFromFile(std::string const & vertex, std::string const & geometry, std::string const & fragment);
+
+    bool loadFromMemory(std::string const & shader, Type type);
+    bool loadFromMemory(std::string const & vertex, std::string const & fragment);
+    bool loadFromMemory(std::string const & vertex, std::string const & geometry, std::string const & fragment);
+
+public:
+//    void setUniform(std::string const & name, float x);
+//    void setUniform(std::string const & name, Vec2 const & vector);
+//    void setUniform(std::string const & name, Glsl::Vec3 const & vector);
+//    void setUniform(std::string const & name, Glsl::Vec4 const & vector);
+//
+//    void setUniform(std::string const & name, int x);
+//    void setUniform(std::string const & name, Ivec2 const & vector);
+//    void setUniform(std::string const & name, Glsl::Ivec3 const & vector);
+//    void setUniform(std::string const & name, Glsl::Ivec4 const & vector);
+//
+//    void setUniform(std::string const & name, bool x);
+//    void setUniform(std::string const & name, Bvec2 const & vector);
+//    void setUniform(std::string const & name, Glsl::Bvec3 const & vector);
+//    void setUniform(std::string const & name, Glsl::Bvec4 const & vector);
+//
+//    void setUniform(std::string const & name, Glsl::Mat3 const & matrix);
+//    void setUniform(std::string const & name, Glsl::Mat4 const & matrix);
+//
+//    void setUniform(std::string const & name, Texture const & texture);
+//    void setUniform(std::string const & name, current_texture_t);
+//
+//    void setUniformArray(std::string const & name, float const * scalarArray, std::size_t length);
+//    void setUniformArray(std::string const & name, Glsl::Vec2 const * vectorArray, std::size_t length);
+//    void setUniformArray(std::string const & name, Glsl::Vec3 const * vectorArray, std::size_t length);
+//    void setUniformArray(std::string const & name, Glsl::Vec4 const * vectorArray, std::size_t length);
+//    void setUniformArray(std::string const & name, Glsl::Mat3 const * matrixArray, std::size_t length);
+//    void setUniformArray(std::string const & name, Glsl::Mat4 const * matrixArray, std::size_t length);
+//
+//    unsigned int getNativeHandle() const;
+//
+//public:
+//    static void bind(Shader const * shader);
+//    static bool isAvailable();
+//    static bool isGeometryAvailable();
 };
 
 } // namespace gui
