7ee3fae60025a5768289cad483e9bcd4d851eea5
zer0 <osom8979@gmail.com>
2019-02-25 17:36:21 +0900

    Fixed build error.

--- libtbag/archive/Archive.cpp
+++ libtbag/archive/Archive.cpp
@@ -30,24 +30,26 @@ static void write_archive(const char *outname, const char **filename)
     archive_write_add_filter_gzip(a);
     archive_write_set_format_pax_restricted(a); // Note 1
     archive_write_open_filename(a, outname);
-    while (*filename) {
-        stat(*filename, &st);
-        entry = archive_entry_new(); // Note 2
-        archive_entry_set_pathname(entry, *filename);
-        archive_entry_set_size(entry, st.st_size); // Note 3
-        archive_entry_set_filetype(entry, AE_IFREG);
-        archive_entry_set_perm(entry, 0644);
-        archive_write_header(a, entry);
-        fd = open(*filename, O_RDONLY);
-        len = read(fd, buff, sizeof(buff));
-        while ( len > 0 ) {
-            archive_write_data(a, buff, len);
-            len = read(fd, buff, sizeof(buff));
-        }
-        close(fd);
-        archive_entry_free(entry);
-        filename++;
-    }
+
+//    while (*filename) {
+//        stat(*filename, &st);
+//        entry = archive_entry_new(); // Note 2
+//        archive_entry_set_pathname(entry, *filename);
+//        archive_entry_set_size(entry, st.st_size); // Note 3
+//        archive_entry_set_filetype(entry, AE_IFREG);
+//        archive_entry_set_perm(entry, 0644);
+//        archive_write_header(a, entry);
+//        fd = open(*filename, O_RDONLY);
+//        len = read(fd, buff, sizeof(buff));
+//        while ( len > 0 ) {
+//            archive_write_data(a, buff, len);
+//            len = read(fd, buff, sizeof(buff));
+//        }
+//        close(fd);
+//        archive_entry_free(entry);
+//        filename++;
+//    }
+
     archive_write_close(a); // Note 4
     archive_write_free(a); // Note 5
 }
