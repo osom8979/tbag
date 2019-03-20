8ff34e1873fa4de7d66305d72f4a942fdf8d5605
zer0 <osom8979@gmail.com>
2019-03-20 19:13:33 +0900

    Create getSizeOfAspectRatio() method.

--- libtbag/math/AspectRatio.hpp
+++ libtbag/math/AspectRatio.hpp
@@ -18,6 +18,7 @@
 #include <libtbag/geometry/Size2.hpp>
 #include <libtbag/math/Euclidean.hpp>
 
+#include <cassert>
 #include <utility>
 #include <type_traits>
 
@@ -31,6 +32,7 @@ template <typename T>
 void calcAspectRatio(T a, T b, T & a_result, T & b_result)
 {
     auto const GCD = gcd(a, b);
+    assert(GCD != 0);
     a_result = a / GCD;
     b_result = b / GCD;
 }
@@ -47,13 +49,36 @@ template <typename T, typename ScaleType = typename std::conditional<std::is_flo
 libtbag::geometry::BaseSize2<T> scaleUpAspectRatio(libtbag::geometry::BaseSize2<T> const & src,
                                                    libtbag::geometry::BaseSize2<T> const & scale_up)
 {
+    assert(static_cast<ScaleType>(src.width) != 0);
+    assert(static_cast<ScaleType>(src.height) != 0);
+
     auto const SCALE_X = scale_up.width / static_cast<ScaleType>(src.width);
     auto const SCALE_Y = scale_up.height / static_cast<ScaleType>(src.height);
+
     if (SCALE_X < SCALE_Y) {
         return libtbag::geometry::BaseSize2<T>(src.width * SCALE_X, src.height * SCALE_X);
     } else {
         return libtbag::geometry::BaseSize2<T>(src.width * SCALE_Y, src.height * SCALE_Y);
     }
+}
+
+template <typename T>
+libtbag::geometry::BaseSize2<T> getSizeOfAspectRatio(T input_width, T input_height, T resize_width)
+{
+    assert(input_width != 0);
+    return libtbag::geometry::BaseSize2<T>(resize_width, (input_height * resize_width) / input_width);
+}
+
+template <typename T>
+libtbag::geometry::BaseSize2<T> getSizeOfAspectRatio(T input_width, T input_height, T resize_width, T minimum_height)
+{
+    auto result = getSizeOfAspectRatio<T>(input_width, input_height, resize_width);
+    if (minimum_height > 0 && result.height >= minimum_height) {
+        assert(input_height != 0);
+        result.width = ((input_width * minimum_height) / input_height);
+        result.height = minimum_height;
+    }
+    return result;
 }
 
 } // namespace math
