ef8dfd2005ff160d55a846fff56bd1d536cb03a3
zer0 <osom8979@gmail.com>
2019-04-20 19:25:19 +0900

    Fixed test error: ImageIOTest.UseJpeg

--- libtbag/graphic/ImageIO.cpp
+++ libtbag/graphic/ImageIO.cpp
@@ -13,12 +13,12 @@
 
 #include <cassert>
 
-// Avoid 'duplicate symbol' errors.
-// Check the 'raylib' library.
-// #define STB_IMAGE_IMPLEMENTATION
-// #define STB_IMAGE_WRITE_IMPLEMENTATION
-
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_STATIC
 #include <stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_STATIC
 #include <stb_image_write.h>
 
 // -------------------
