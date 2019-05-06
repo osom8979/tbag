7bd3c7f3245f647433cb635ca0b899b79d8a8443
zer0 <osom8979@gmail.com>
2019-05-06 12:31:20 +0900

    Create empty RayGui package.

---
+++ libtbag/ray/RayGui.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file   RayGui.cpp
+ * @brief  RayGui class implementation.
+ * @author zer0
+ * @date   2019-05-06
+ */
+
+#include <libtbag/ray/RayGui.hpp>
+#include <libtbag/log/Log.hpp>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace ray {
+
+} // namespace ray
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
---
+++ libtbag/ray/RayGui.hpp
@@ -0,0 +1,36 @@
+/**
+ * @file   RayGui.hpp
+ * @brief  RayGui class prototype.
+ * @author zer0
+ * @date   2019-05-06
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_RAY_RAYGUI_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_RAY_RAYGUI_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+#include <libtbag/predef.hpp>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+namespace ray {
+
+struct RayGuiContext
+{
+};
+
+} // namespace ray
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_RAY_RAYGUI_HPP__
+
