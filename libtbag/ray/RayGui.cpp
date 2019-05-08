3e10b90b0f5b1277d6db1ebb25802f3844f9c0da
zer0 <osom8979@gmail.com>
2019-05-08 19:36:21 +0900

    Clear RenderRayGui() method implementation.

--- libtbag/ray/RayGui.cpp
+++ libtbag/ray/RayGui.cpp
@@ -9,36 +9,11 @@
 #include <libtbag/Noncopyable.hpp>
 
 #include <imgui.h>
+
 #include <cassert>
 #include <cstdlib>
 
-#include <external/raylib/src/rlgl.h>
-
-#if defined(_MSC_VER) && (_MSC_VER <= 1500) // MSVC 2008 or earlier
-# include <stddef.h> // intptr_t
-#else
-# include <stdint.h> // intptr_t
-#endif
-
-#if defined(_WIN32) && !defined(APIENTRY)
-// It is customary to use APIENTRY for OpenGL function pointer declarations on all platforms.
-// Additionally, the Windows OpenGL header needs APIENTRY.
-# define APIENTRY __stdcall
-#endif
-
-#if defined(_WIN32) && !defined(WINGDIAPI)
-// Some Windows OpenGL headers need this
-# define WINGDIAPI __declspec(dllimport)
-#endif
-
-#if defined(__APPLE__)
-# ifndef GL_SILENCE_DEPRECATION
-# define GL_SILENCE_DEPRECATION
-# endif
-# include <OpenGL/gl.h>
-#else
-# include <GL/gl.h>
-#endif
+#include <vector>
 
 // -------------------
 NAMESPACE_LIBTBAG_OPEN
@@ -71,12 +46,11 @@ bool InitRayGui()
     ImGuiContext * context = ImGui::CreateContext();
     assert(context != nullptr);
 
-    ImGuiIO & io = ImGui::GetIO();
-    io.Fonts->AddFontDefault();
-
     // User data configuration.
     auto * user = new RayGui();
     assert(user != nullptr);
+
+    ImGuiIO & io = ImGui::GetIO();
     io.UserData = (void*)user;
 
     // Keyboard input configuration.
@@ -108,6 +82,8 @@ bool InitRayGui()
     io.DisplaySize.y = (float)GetScreenHeight();
 
     {
+        io.Fonts->AddFontDefault();
+
         unsigned char * pixels;
         int width, height;
         io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);
@@ -118,12 +94,17 @@ bool InitRayGui()
         UnloadImage(font_image);
     }
 
+    ImGui::StyleColorsDark();
     return true;
 }
 
 void UpdateRayGui()
 {
     ImGuiIO & io = ImGui::GetIO();
+
+    io.DeltaTime = 1.0f/60.0f;              // set the time elapsed since the previous frame (in seconds)
+    io.DisplaySize.x = 1920.0f;             // set the current display width
+    io.DisplaySize.y = 1280.0f;             // set the current display height here
 
     io.MouseDown[0] = IsMouseButtonDown(MOUSE_LEFT_BUTTON);
     io.MouseDown[1] = IsMouseButtonDown(MOUSE_RIGHT_BUTTON);
@@ -247,9 +228,9 @@ void UpdateRayGui()
         io.AddInputCharacter(static_cast<ImWchar>(key));
     }
 
+    io.DeltaTime = GetFrameTime();
     io.DisplaySize.x = (float)GetScreenWidth();
     io.DisplaySize.y = (float)GetScreenHeight();
-    io.DeltaTime = GetFrameTime();
 
     if (io.WantSetMousePos) {
         SetMousePosition((int)io.MousePos.x, (int)io.MousePos.y);
@@ -260,129 +241,10 @@ void UpdateRayGui()
     ImGui::NewFrame();
 }
 
-static void ImGui_ImplOpenGL2_SetupRenderState(ImDrawData* draw_data, int fb_width, int fb_height)
-{
-    // Setup render state: alpha-blending enabled, no face culling, no depth testing, scissor enabled, vertex/texcoord/color pointers, polygon fill.
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glDisable(GL_CULL_FACE);
-    glDisable(GL_DEPTH_TEST);
-    glDisable(GL_LIGHTING);
-    glDisable(GL_COLOR_MATERIAL);
-    glEnable(GL_SCISSOR_TEST);
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-    glEnableClientState(GL_COLOR_ARRAY);
-    glEnable(GL_TEXTURE_2D);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-    // If you are using this code with non-legacy OpenGL header/contexts (which you should not, prefer using imgui_impl_opengl3.cpp!!),
-    // you may need to backup/reset/restore current shader using the lines below. DO NOT MODIFY THIS FILE! Add the code in your calling function:
-    //  GLint last_program;
-    //  glGetIntegerv(GL_CURRENT_PROGRAM, &last_program);
-    //  glUseProgram(0);
-    //  ImGui_ImplOpenGL2_RenderDrawData(...);
-    //  glUseProgram(last_program)
-
-    // Setup viewport, orthographic projection matrix
-    // Our visible imgui space lies from draw_data->DisplayPos (top left) to draw_data->DisplayPos+data_data->DisplaySize (bottom right). DisplayMin is typically (0,0) for single viewport apps.
-    glViewport(0, 0, (GLsizei)fb_width, (GLsizei)fb_height);
-    glMatrixMode(GL_PROJECTION);
-    glPushMatrix();
-    glLoadIdentity();
-    glOrtho(draw_data->DisplayPos.x, draw_data->DisplayPos.x + draw_data->DisplaySize.x, draw_data->DisplayPos.y + draw_data->DisplaySize.y, draw_data->DisplayPos.y, -1.0f, +1.0f);
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-    glLoadIdentity();
-}
-
-// OpenGL2 Render function.
-// (this used to be set in io.RenderDrawListsFn and called by ImGui::Render(), but you can now call this directly from your main loop)
-// Note that this implementation is little overcomplicated because we are saving/setting up/restoring every OpenGL state explicitly, in order to be able to run within any OpenGL engine that doesn't do so.
-void ImGui_ImplOpenGL2_RenderDrawData(ImDrawData* draw_data)
-{
-    // Avoid rendering when minimized, scale coordinates for retina displays (screen coordinates != framebuffer coordinates)
-    int fb_width = (int)(draw_data->DisplaySize.x * draw_data->FramebufferScale.x);
-    int fb_height = (int)(draw_data->DisplaySize.y * draw_data->FramebufferScale.y);
-    if (fb_width == 0 || fb_height == 0)
-        return;
-
-    // Backup GL state
-    GLint last_texture; glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
-    GLint last_polygon_mode[2]; glGetIntegerv(GL_POLYGON_MODE, last_polygon_mode);
-    GLint last_viewport[4]; glGetIntegerv(GL_VIEWPORT, last_viewport);
-    GLint last_scissor_box[4]; glGetIntegerv(GL_SCISSOR_BOX, last_scissor_box);
-    glPushAttrib(GL_ENABLE_BIT | GL_COLOR_BUFFER_BIT | GL_TRANSFORM_BIT);
-
-    // Setup desired GL state
-    ImGui_ImplOpenGL2_SetupRenderState(draw_data, fb_width, fb_height);
-
-    // Will project scissor/clipping rectangles into framebuffer space
-    ImVec2 clip_off = draw_data->DisplayPos;         // (0,0) unless using multi-viewports
-    ImVec2 clip_scale = draw_data->FramebufferScale; // (1,1) unless using retina display which are often (2,2)
-
-    // Render command lists
-    for (int n = 0; n < draw_data->CmdListsCount; n++)
-    {
-        const ImDrawList* cmd_list = draw_data->CmdLists[n];
-        const ImDrawVert* vtx_buffer = cmd_list->VtxBuffer.Data;
-        const ImDrawIdx* idx_buffer = cmd_list->IdxBuffer.Data;
-        glVertexPointer(2, GL_FLOAT, sizeof(ImDrawVert), (const GLvoid*)((const char*)vtx_buffer + IM_OFFSETOF(ImDrawVert, pos)));
-        glTexCoordPointer(2, GL_FLOAT, sizeof(ImDrawVert), (const GLvoid*)((const char*)vtx_buffer + IM_OFFSETOF(ImDrawVert, uv)));
-        glColorPointer(4, GL_UNSIGNED_BYTE, sizeof(ImDrawVert), (const GLvoid*)((const char*)vtx_buffer + IM_OFFSETOF(ImDrawVert, col)));
-
-        for (int cmd_i = 0; cmd_i < cmd_list->CmdBuffer.Size; cmd_i++)
-        {
-            const ImDrawCmd* pcmd = &cmd_list->CmdBuffer[cmd_i];
-            if (pcmd->UserCallback)
-            {
-                // User callback, registered via ImDrawList::AddCallback()
-                // (ImDrawCallback_ResetRenderState is a special callback value used by the user to request the renderer to reset render state.)
-                if (pcmd->UserCallback == nullptr)
-                    ImGui_ImplOpenGL2_SetupRenderState(draw_data, fb_width, fb_height);
-                else
-                    pcmd->UserCallback(cmd_list, pcmd);
-            }
-            else
-            {
-                // Project scissor/clipping rectangles into framebuffer space
-                ImVec4 clip_rect;
-                clip_rect.x = (pcmd->ClipRect.x - clip_off.x) * clip_scale.x;
-                clip_rect.y = (pcmd->ClipRect.y - clip_off.y) * clip_scale.y;
-                clip_rect.z = (pcmd->ClipRect.z - clip_off.x) * clip_scale.x;
-                clip_rect.w = (pcmd->ClipRect.w - clip_off.y) * clip_scale.y;
-
-                if (clip_rect.x < fb_width && clip_rect.y < fb_height && clip_rect.z >= 0.0f && clip_rect.w >= 0.0f)
-                {
-                    // Apply scissor/clipping rectangle
-                    glScissor((int)clip_rect.x, (int)(fb_height - clip_rect.w), (int)(clip_rect.z - clip_rect.x), (int)(clip_rect.w - clip_rect.y));
-
-                    // Bind texture, Draw
-                    glBindTexture(GL_TEXTURE_2D, (GLuint)(intptr_t)pcmd->TextureId);
-                    glDrawElements(GL_TRIANGLES, (GLsizei)pcmd->ElemCount, sizeof(ImDrawIdx) == 2 ? GL_UNSIGNED_SHORT : GL_UNSIGNED_INT, idx_buffer);
-                }
-            }
-            idx_buffer += pcmd->ElemCount;
-        }
-    }
-
-    // Restore modified GL state
-    glDisableClientState(GL_COLOR_ARRAY);
-    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-    glDisableClientState(GL_VERTEX_ARRAY);
-    glBindTexture(GL_TEXTURE_2D, (GLuint)last_texture);
-    glMatrixMode(GL_MODELVIEW);
-    glPopMatrix();
-    glMatrixMode(GL_PROJECTION);
-    glPopMatrix();
-    glPopAttrib();
-    glPolygonMode(GL_FRONT, (GLenum)last_polygon_mode[0]); glPolygonMode(GL_BACK, (GLenum)last_polygon_mode[1]);
-    glViewport(last_viewport[0], last_viewport[1], (GLsizei)last_viewport[2], (GLsizei)last_viewport[3]);
-    glScissor(last_scissor_box[0], last_scissor_box[1], (GLsizei)last_scissor_box[2], (GLsizei)last_scissor_box[3]);
-}
-
 void RenderRayGui()
 {
+    ImGui::EndFrame();
+
     ImGui::Render();
     ImDrawData * draw_data = ImGui::GetDrawData();
     assert(draw_data != nullptr);
@@ -390,36 +252,68 @@ void RenderRayGui()
     if (draw_data->CmdListsCount == 0) {
         return;
     }
-
     ImGuiIO & io = ImGui::GetIO();
 
-    // scale stuff (needed for proper handling of window resize)
-    int fb_width = static_cast<int>(io.DisplaySize.x * io.DisplayFramebufferScale.x);
-    int fb_height = static_cast<int>(io.DisplaySize.y * io.DisplayFramebufferScale.y);
-    if (fb_width <= 0 || fb_height <= 0) {
+    // Avoid rendering when minimized,
+    // scale coordinates for retina displays
+    // (screen coordinates != framebuffer coordinates)
+    auto const FRAME_BUFFER_WIDTH  = (int)(draw_data->DisplaySize.x * draw_data->FramebufferScale.x);
+    auto const FRAME_BUFFER_HEIGHT = (int)(draw_data->DisplaySize.y * draw_data->FramebufferScale.y);
+    if (FRAME_BUFFER_WIDTH <= 0 || FRAME_BUFFER_HEIGHT <= 0) {
         return;
     }
-    draw_data->ScaleClipRects(io.DisplayFramebufferScale);
 
-    for (int n = 0; n < draw_data->CmdListsCount; ++n) {
-        ImDrawList const * cmd_list = draw_data->CmdLists[n];
-        unsigned char const * vtx_buffer = (unsigned char const *)&cmd_list->VtxBuffer.front();
-        ImDrawIdx const * idx_buffer = &cmd_list->IdxBuffer.front();
+    // Will project scissor/clipping rectangles into framebuffer space
+    ImVec2 const CLIP_OFF = draw_data->DisplayPos;         // (0,0) unless using multi-viewports
+    ImVec2 const CLIP_SCALE = draw_data->FramebufferScale; // (1,1) unless using retina display which are often (2,2)
 
-        glVertexPointer(2, GL_FLOAT, sizeof(ImDrawVert), (void*)(vtx_buffer + offsetof(ImDrawVert, pos)));
-        glTexCoordPointer(2, GL_FLOAT, sizeof(ImDrawVert), (void*)(vtx_buffer + offsetof(ImDrawVert, uv)));
-        glColorPointer(4, GL_UNSIGNED_BYTE, sizeof(ImDrawVert), (void*)(vtx_buffer + offsetof(ImDrawVert, col)));
+    // Render command lists
+    for (int n = 0; n < draw_data->CmdListsCount; n++) {
+        ImDrawList const * cmd_list   = draw_data->CmdLists[n];
+        ImDrawVert const * vtx_buffer = cmd_list->VtxBuffer.Data;
+        ImDrawIdx  const * idx_buffer = cmd_list->IdxBuffer.Data;
 
-        for (int cmd_i = 0; cmd_i < cmd_list->CmdBuffer.size(); ++cmd_i) {
+        std::vector<Vector2> vectors(cmd_list->VtxBuffer.Size);
+        for (int i = 0; i < cmd_list->VtxBuffer.Size; ++i) {
+            vectors[i] = cmd_list->VtxBuffer[i].pos;
+        }
+
+        DrawPolyEx(vectors.data(), vectors.size(), Color{255, 0, 0, 255});
+        // glVertexPointer(2, GL_FLOAT, sizeof(ImDrawVert), (const GLvoid*)((const char*)vtx_buffer + IM_OFFSETOF(ImDrawVert, pos)));
+        // glTexCoordPointer(2, GL_FLOAT, sizeof(ImDrawVert), (const GLvoid*)((const char*)vtx_buffer + IM_OFFSETOF(ImDrawVert, uv)));
+        // glColorPointer(4, GL_UNSIGNED_BYTE, sizeof(ImDrawVert), (const GLvoid*)((const char*)vtx_buffer + IM_OFFSETOF(ImDrawVert, col)));
+
+        for (int cmd_i = 0; cmd_i < cmd_list->CmdBuffer.Size; cmd_i++) {
             ImDrawCmd const * pcmd = &cmd_list->CmdBuffer[cmd_i];
             if (pcmd->UserCallback) {
-                pcmd->UserCallback(cmd_list, pcmd);
+                // User callback, registered via ImDrawList::AddCallback()
+                // (ImDrawCallback_ResetRenderState is a special callback value used by the user to request the renderer to reset render state.)
+                if (pcmd->UserCallback == ImDrawCallback_ResetRenderState) {
+                    // draw_data
+                    // FRAME_BUFFER_WIDTH
+                    // FRAME_BUFFER_HEIGHT
+                } else {
+                    pcmd->UserCallback(cmd_list, pcmd);
+                }
             } else {
-                GLuint tex_id = (GLuint)*((unsigned int*)&pcmd->TextureId);
-                glBindTexture(GL_TEXTURE_2D, tex_id);
-                glScissor((int)pcmd->ClipRect.x, (int)(fb_height - pcmd->ClipRect.w),
-                          (int)(pcmd->ClipRect.z - pcmd->ClipRect.x), (int)(pcmd->ClipRect.w - pcmd->ClipRect.y));
-                glDrawElements(GL_TRIANGLES, (GLsizei)pcmd->ElemCount, GL_UNSIGNED_SHORT, idx_buffer);
+                // Project scissor/clipping rectangles into framebuffer space
+                ImVec4 clip_rect;
+                clip_rect.x = (pcmd->ClipRect.x - CLIP_OFF.x) * CLIP_SCALE.x;
+                clip_rect.y = (pcmd->ClipRect.y - CLIP_OFF.y) * CLIP_SCALE.y;
+                clip_rect.z = (pcmd->ClipRect.z - CLIP_OFF.x) * CLIP_SCALE.x;
+                clip_rect.w = (pcmd->ClipRect.w - CLIP_OFF.y) * CLIP_SCALE.y;
+
+                if (/**/clip_rect.x < FRAME_BUFFER_WIDTH &&
+                        clip_rect.y < FRAME_BUFFER_HEIGHT &&
+                        clip_rect.z >= 0.0f &&
+                        clip_rect.w >= 0.0f) {
+                    // // Apply scissor/clipping rectangle
+                    // glScissor((int)clip_rect.x, (int)(FRAME_BUFFER_HEIGHT - clip_rect.w), (int)(clip_rect.z - clip_rect.x), (int)(clip_rect.w - clip_rect.y));
+                    //
+                    // // Bind texture, Draw
+                    // glBindTexture(GL_TEXTURE_2D, (GLuint)(intptr_t)pcmd->TextureId);
+                    // glDrawElements(GL_TRIANGLES, (GLsizei)pcmd->ElemCount, sizeof(ImDrawIdx) == 2 ? GL_UNSIGNED_SHORT : GL_UNSIGNED_INT, idx_buffer);
+                }
             }
             idx_buffer += pcmd->ElemCount;
         }
