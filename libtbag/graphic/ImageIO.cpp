7bc80388a4eb7b9e17c1fe3105b6b1bda70887a5
zer0 <osom8979@gmail.com>
2019-05-19 20:17:51 +0900

    Restore ImageIO package.

--- libtbag/graphic/ImageIO.cpp
+++ libtbag/graphic/ImageIO.cpp
@@ -6,7 +6,6 @@
  * @date   2019-02-20 (Rename: Image -> ImageIO)
  */
 
-/*
 #include <libtbag/graphic/ImageIO.hpp>
 #include <libtbag/log/Log.hpp>
 #include <libtbag/filesystem/Path.hpp>
@@ -58,109 +57,49 @@ bool writeTga(char const * path, int width, int height, int channels, char const
     return stbi_write_tga(path, width, height, channels, data) != 0;
 }
 
-Err readImage(std::string const & path, libtbag::util::Buffer & image, int * width, int * height, int * channels)
+Err readImage(std::string const & path, Box & image)
 {
     if (!filesystem::Path(path).exists()) {
         return E_EEXIST;
     }
-    //if (!filesystem::Path(path).isReadable()) {
-    //    return E_RDERR;
-    //}
 
-    int read_width = 0;
-    int read_height = 0;
-    int read_channels = 0;
+    int width = 0;
+    int height = 0;
+    int channels = 0;
 
     // process data if not NULL ...
     // x = width, y = height, n = # 8-bit components per pixel ...
     // replace '0' with '1'..'4' to force that many components per pixel
     // but 'n' will always be the number that it would have been if you said 0
-    unsigned char * data = ::stbi_load(path.c_str(), &read_width, &read_height, &read_channels, 0);
+    unsigned char * data = ::stbi_load(path.c_str(), &width, &height, &channels, 0);
     if (data == nullptr) {
         return E_RDERR;
     }
-
-    image.assign(data, data + (read_width * read_height * read_channels));
-    if (width != nullptr) {
-        *width = read_width;
-    }
-    if (height != nullptr) {
-        *height = read_height;
-    }
-    if (channels != nullptr) {
-        *channels = read_channels;
-    }
-
-    stbi_image_free(data);
-    return E_SUCCESS;
-}
-
-Err readImage(std::string const & path, ImageRgb24 & image)
-{
-    int width = 0;
-    int height = 0;
-    int channels = 0;
-
-    libtbag::util::Buffer buffer;
-    auto const CODE = readImage(path, buffer, &width, &height, &channels);
-    if (isFailure(CODE)) {
-        return CODE;
-    }
-
-    auto const * data = buffer.data();
-    assert(data != nullptr);
 
     // 1: grey
     // 2: grey, alpha
     // 3: red, green, blue
     // 4: red, green, blue, alpha
     assert(1 <= COMPARE_AND(channels) <= 4);
-    assert(width > 0);
-    assert(height > 0);
-    image.resize(static_cast<unsigned>(width), static_cast<unsigned>(height));
-    auto const SIZE = static_cast<std::size_t>(width * height);
+    assert(width >= 1);
+    assert(height >= 1);
+    image.reshape<uint8_t>(height, width, channels);
+    memcpy(image.data(), data, image.size());
 
-    if (channels == 1) {
-        for (std::size_t i = 0; i < SIZE; ++i) {
-            image[i].r = *(data + i);
-            image[i].g = *(data + i);
-            image[i].b = *(data + i);
-        }
-    } else if (channels == 2) {
-        for (std::size_t i = 0; i < SIZE; ++i) {
-            image[i].r = *(data + (2 * i));
-            image[i].g = *(data + (2 * i));
-            image[i].b = *(data + (2 * i));
-        }
-    } else if (channels == 3 || channels == 4) {
-        for (std::size_t i = 0; i < SIZE; ++i) {
-            image[i].r = *(data + (channels * i) + 0);
-            image[i].g = *(data + (channels * i) + 1);
-            image[i].b = *(data + (channels * i) + 2);
-            // image[i].a = *(data + (channels * i) + 3);
-        }
-    } else {
-        assert(false && "Inaccessible block.");
-    }
-
+    stbi_image_free(data);
     return E_SUCCESS;
 }
 
-// ---------------
-namespace __impl {
-// ---------------
-
-template <typename ImageType>
-static Err writeImageToFile(std::string const & path, ImageType const & image, int if_jpeg_quality)
+Err writeImage(std::string const & path, Box const & image)
 {
     auto const PATH = filesystem::Path(path);
-    if (PATH.exists() == true) {
+    if (PATH.exists()) {
         return E_ALREADY;
     }
 
-    auto const WIDTH     = image.width();
-    auto const HEIGHT    = image.height();
-    auto const CHANNELS  = GetChannels<ImageType>::channels;
+    auto const HEIGHT    = image.dim(0);
+    auto const WIDTH     = image.dim(1);
+    auto const CHANNELS  = image.dim(2);
     auto const * DATA    = (char const *)image.data();
     auto const LOWER_EXT = string::lower(PATH.getExtensionName());
 
@@ -168,7 +107,7 @@ static Err writeImageToFile(std::string const & path, ImageType const & image, i
     if (LOWER_EXT == PNG_LOWER_EXT) {
         result = writePng(path.c_str(), WIDTH, HEIGHT, CHANNELS, DATA, WIDTH * CHANNELS);
     } else if (LOWER_EXT == JPG_LOWER_EXT) {
-        result = writeJpg(path.c_str(), WIDTH, HEIGHT, CHANNELS, DATA, if_jpeg_quality);
+        result = writeJpg(path.c_str(), WIDTH, HEIGHT, CHANNELS, DATA, DEFAULT_JPG_QUALITY);
     } else if (LOWER_EXT == BMP_LOWER_EXT) {
         result = writeBmp(path.c_str(), WIDTH, HEIGHT, CHANNELS, DATA);
     } else if (LOWER_EXT == TGA_LOWER_EXT) {
@@ -180,42 +119,41 @@ static Err writeImageToFile(std::string const & path, ImageType const & image, i
     return result != 0 ? E_SUCCESS : E_UNKNOWN;
 }
 
-static void __write_image_cb__(void * context, void * data, int size)
+static void __write_image_cb(void * context, void * data, int size)
 {
     assert(context != nullptr);
     assert(data != nullptr);
     assert(size > 0);
 
-    auto * buffer = static_cast<util::Buffer*>(context);
+    auto * buffer = static_cast<libtbag::util::Buffer*>(context);
     assert(buffer != nullptr);
 
     buffer->resize(static_cast<std::size_t>(size));
     for (int i = 0; i < size; ++i) {
-        (*buffer)[i] = ((util::Buffer::const_pointer)data)[i];
+        (*buffer)[i] = ((libtbag::util::Buffer::const_pointer)data)[i];
     }
 }
 
-template <typename ImageType>
-static Err writeImageToBuffer(util::Buffer & buffer, ImageType const & image, ImageFileFormat format, int if_jpeg_quality)
+Err writeImage(util::Buffer & buffer, Box const & image, ImageFileFormat format)
 {
-    auto const WIDTH    = image.width();
-    auto const HEIGHT   = image.height();
-    auto const CHANNELS = GetChannels<ImageType>::channels;
-    auto const * DATA   = image.data();
+    auto const HEIGHT    = image.dim(0);
+    auto const WIDTH     = image.dim(1);
+    auto const CHANNELS  = image.dim(2);
+    auto const * DATA    = (char const *)image.data();
 
     int result = 0;
     switch (format) {
     case ImageFileFormat::IFF_PNG:
-        result = stbi_write_png_to_func(&__write_image_cb__, &buffer, WIDTH, HEIGHT, CHANNELS, DATA, WIDTH * CHANNELS);
+        result = stbi_write_png_to_func(&__write_image_cb, &buffer, WIDTH, HEIGHT, CHANNELS, DATA, WIDTH * CHANNELS);
         break;
     case ImageFileFormat::IFF_JPG:
-        result = stbi_write_jpg_to_func(&__write_image_cb__, &buffer, WIDTH, HEIGHT, CHANNELS, DATA, if_jpeg_quality);
+        result = stbi_write_jpg_to_func(&__write_image_cb, &buffer, WIDTH, HEIGHT, CHANNELS, DATA, DEFAULT_JPG_QUALITY);
         break;
     case ImageFileFormat::IFF_BMP:
-        result = stbi_write_bmp_to_func(&__write_image_cb__, &buffer, WIDTH, HEIGHT, CHANNELS, DATA);
+        result = stbi_write_bmp_to_func(&__write_image_cb, &buffer, WIDTH, HEIGHT, CHANNELS, DATA);
         break;
     case ImageFileFormat::IFF_TGA:
-        result = stbi_write_tga_to_func(&__write_image_cb__, &buffer, WIDTH, HEIGHT, CHANNELS, DATA);
+        result = stbi_write_tga_to_func(&__write_image_cb, &buffer, WIDTH, HEIGHT, CHANNELS, DATA);
         break;
     default:
         return E_ILLARGS;
@@ -223,57 +161,9 @@ static Err writeImageToBuffer(util::Buffer & buffer, ImageType const & image, Im
     return result != 0 ? E_SUCCESS : E_UNKNOWN;
 }
 
-// ------------------
-} // namespace __impl
-// ------------------
-
-Err writeImage(std::string const & path, ImageRgb24 const & image)
-{
-    return __impl::writeImageToFile(path, image, DEFAULT_JPG_QUALITY);
-}
-
-Err writeImage(std::string const & path, ImageGray const & image)
-{
-    return __impl::writeImageToFile(path, image, DEFAULT_JPG_QUALITY);
-}
-
-Err writeImage(util::Buffer & buffer, ImageRgb24 const & image, ImageFileFormat format)
-{
-    return __impl::writeImageToBuffer(buffer, image, format, DEFAULT_JPG_QUALITY);
-}
-
-Err writeImage(util::Buffer & buffer, ImageGray const & image, ImageFileFormat format)
-{
-    return __impl::writeImageToBuffer(buffer, image, format, DEFAULT_JPG_QUALITY);
-}
-
-Err convert(ImageRgb24 const & source, ImageGray & destination)
-{
-    destination.resize(source.width(), source.height());
-    auto const ITR = std::transform(source.begin(), source.end(), destination.begin(), [](Color const & color) -> Channel {
-        return static_cast<Channel>((color.r + color.g + color.b) / 3);
-    });
-    if (ITR == destination.begin()) {
-        return E_ILLARGS;
-    }
-    return E_SUCCESS;
-}
-
-Err convert(ImageGray const & source, ImageRgb24 & destination)
-{
-    destination.resize(source.width(), source.height());
-    auto const ITR = std::transform(source.begin(), source.end(), destination.begin(), [](Channel const & channel) -> Color {
-        return Color{channel, channel, channel};
-    });
-    if (ITR == destination.begin()) {
-        return E_ILLARGS;
-    }
-    return E_SUCCESS;
-}
-
 } // namespace graphic
 
 // --------------------
 NAMESPACE_LIBTBAG_CLOSE
 // --------------------
-*/
+
--- tester/libtbag/graphic/ImageIOTest.cpp
+++ tester/libtbag/graphic/ImageIOTest.cpp
@@ -6,78 +6,81 @@
  * @date   2019-02-20 (Rename: Image -> ImageIO)
  */
 
-//#include <gtest/gtest.h>
-//#include <tester/DemoAsset.hpp>
-//#include <libtbag/graphic/ImageIO.hpp>
-//#include <libtbag/filesystem/Path.hpp>
-//
-//using namespace libtbag;
-//using namespace libtbag::graphic;
-//
-//TEST(ImageIOTest, ReadImage)
-//{
-//    auto path = DemoAsset::get_tester_dir_image() / "lena.png";
-//
-//    Image image;
-//    ASSERT_EQ(E_SUCCESS, readImage(path.getString(), image));
-//
-//    ASSERT_EQ(512, image.width());
-//    ASSERT_EQ(512, image.height());
-//
-//    // First RGB pixel.
-//    ASSERT_EQ(226, image[0].r);
-//    ASSERT_EQ(137, image[0].g);
-//    ASSERT_EQ(125, image[0].b);
-//
-//    // Last RGB pixel.
-//    ASSERT_EQ(185, image[image.size() - 1].r);
-//    ASSERT_EQ( 74, image[image.size() - 1].g);
-//    ASSERT_EQ( 81, image[image.size() - 1].b);
-//
-//    util::Buffer buffer;
-//    ASSERT_EQ(E_SUCCESS, writeImage(buffer, image, ImageFileFormat::IFF_PNG));
-//    ASSERT_FALSE(buffer.empty());
-//
-//    // Save & Load.
-//    tttDir_Automatic();
-//    auto const SAVE_PATH = tttDir_Get() / "save.png";
-//    ASSERT_EQ(E_SUCCESS, writeImage(SAVE_PATH.getString(), image));
-//    ASSERT_EQ(buffer.size(), SAVE_PATH.getState().size);
-//
-//    Image reload;
-//    ASSERT_EQ(E_SUCCESS, readImage(SAVE_PATH.getString(), reload));
-//
-//    ASSERT_EQ(512, reload.width());
-//    ASSERT_EQ(512, reload.height());
-//
-//    // First RGB pixel.
-//    ASSERT_EQ(226, reload[0].r);
-//    ASSERT_EQ(137, reload[0].g);
-//    ASSERT_EQ(125, reload[0].b);
-//
-//    // Last RGB pixel.
-//    ASSERT_EQ(185, reload[reload.size() - 1].r);
-//    ASSERT_EQ( 74, reload[reload.size() - 1].g);
-//    ASSERT_EQ( 81, reload[reload.size() - 1].b);
-//}
-//
-//TEST(ImageIOTest, UseJpeg)
-//{
-//    auto path = DemoAsset::get_tester_dir_image() / "lena.png";
-//
-//    Image image;
-//    ASSERT_EQ(E_SUCCESS, readImage(path.getString(), image));
-//
-//    // Save & Load.
-//    tttDir_Automatic();
-//    auto const JPEG_PATH = tttDir_Get() / "save.jpg";
-//    ASSERT_EQ(E_SUCCESS, writeImage(JPEG_PATH.getString(), image));
-//    ASSERT_TRUE(JPEG_PATH.isRegularFile());
-//
-//    Image reload;
-//    ASSERT_EQ(E_SUCCESS, readImage(JPEG_PATH.getString(), reload));
-//
-//    ASSERT_EQ(512, reload.width());
-//    ASSERT_EQ(512, reload.height());
-//}
+#include <gtest/gtest.h>
+#include <tester/DemoAsset.hpp>
+#include <libtbag/graphic/ImageIO.hpp>
+#include <libtbag/filesystem/Path.hpp>
+
+using namespace libtbag;
+using namespace libtbag::graphic;
+
+TEST(ImageIOTest, ReadImage)
+{
+    auto path = DemoAsset::get_tester_dir_image() / "lena.png";
+
+    Box image;
+    ASSERT_EQ(E_SUCCESS, readImage(path.getString(), image));
+
+    ASSERT_EQ(3, image.dim(2));
+    ASSERT_EQ(512, image.dim(1));
+    ASSERT_EQ(512, image.dim(0));
+
+    // First RGB pixel.
+    ASSERT_EQ(226, image.offset<uint8_t>(0)); // r
+    ASSERT_EQ(137, image.offset<uint8_t>(1)); // g
+    ASSERT_EQ(125, image.offset<uint8_t>(2)); // b
+
+    // Last RGB pixel.
+    ASSERT_EQ(185, image.offset<uint8_t>(image.size() - 3)); // r
+    ASSERT_EQ( 74, image.offset<uint8_t>(image.size() - 2)); // g
+    ASSERT_EQ( 81, image.offset<uint8_t>(image.size() - 1)); // b
+
+    libtbag::util::Buffer buffer;
+    ASSERT_EQ(E_SUCCESS, writeImage(buffer, image, ImageFileFormat::IFF_PNG));
+    ASSERT_FALSE(buffer.empty());
+
+    // Save & Load.
+    tttDir_Automatic();
+    auto const SAVE_PATH = tttDir_Get() / "save.png";
+    ASSERT_EQ(E_SUCCESS, writeImage(SAVE_PATH.getString(), image));
+    ASSERT_EQ(buffer.size(), SAVE_PATH.getState().size);
+
+    Box reload;
+    ASSERT_EQ(E_SUCCESS, readImage(SAVE_PATH.getString(), reload));
+
+    ASSERT_EQ(3, image.dim(2));
+    ASSERT_EQ(512, reload.dim(1));
+    ASSERT_EQ(512, reload.dim(0));
+
+    // First RGB pixel.
+    ASSERT_EQ(226, reload.offset<uint8_t>(0)); // r
+    ASSERT_EQ(137, reload.offset<uint8_t>(1)); // g
+    ASSERT_EQ(125, reload.offset<uint8_t>(2)); // b
+
+    // Last RGB pixel.
+    ASSERT_EQ(185, reload.offset<uint8_t>(reload.size() - 3)); // r
+    ASSERT_EQ( 74, reload.offset<uint8_t>(reload.size() - 2)); // g
+    ASSERT_EQ( 81, reload.offset<uint8_t>(reload.size() - 1)); // b
+}
+
+TEST(ImageIOTest, UseJpeg)
+{
+    auto path = DemoAsset::get_tester_dir_image() / "lena.png";
+
+    Box image;
+    ASSERT_EQ(E_SUCCESS, readImage(path.getString(), image));
+
+    // Save & Load.
+    tttDir_Automatic();
+    auto const JPEG_PATH = tttDir_Get() / "save.jpg";
+    ASSERT_EQ(E_SUCCESS, writeImage(JPEG_PATH.getString(), image));
+    ASSERT_TRUE(JPEG_PATH.isRegularFile());
+
+    Box reload;
+    ASSERT_EQ(E_SUCCESS, readImage(JPEG_PATH.getString(), reload));
+
+    ASSERT_EQ(3, reload.dim(2));
+    ASSERT_EQ(512, reload.dim(1));
+    ASSERT_EQ(512, reload.dim(0));
+}
 
