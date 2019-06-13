2b83134d74a47ceade277e18e480ecfa057207fe
zer0 <osom8979@gmail.com>
2019-06-13 16:59:36 +0900

    Migrated luaE_checkfformat() method.

--- libtbag/script/lua/StringLua.cpp
+++ libtbag/script/lua/StringLua.cpp
@@ -6,7 +6,7 @@
  */
 
 #include <libtbag/script/lua/StringLua.hpp>
-#include <libtbag/string/Format.hpp>
+#include <libtbag/string/fmt/format.h>
 
 #include <cassert>
 
@@ -19,23 +19,11 @@ namespace lua    {
 
 std::string luaE_checkfformat(lua_State * L, int greater_equals_index)
 {
-#if 0
-    TBAG_CONSTEXPR static int const MAX_PACKED_ARGS_SIZE = fmt::ArgList::MAX_PACKED_ARGS-1;
-
-    using BasicFormatter = fmt::BasicFormatter<char>;
-    using ArgList = fmt::ArgList;
-    using ArgArray = fmt::internal::ArgArray<MAX_PACKED_ARGS_SIZE>;
-    using ArgArrayType = typename ArgArray::Type;
-
     greater_equals_index = lua_absindex(L, greater_equals_index);
     int const top = lua_gettop(L);
     assert(top >= greater_equals_index);
     int const vargs_size = top - greater_equals_index;
     assert(vargs_size >= 0);
-    if (vargs_size > MAX_PACKED_ARGS_SIZE) {
-        luaL_error(L, "The number of arguments is too large.");
-        return std::string();
-    }
 
     char const * format_text = luaL_checkstring(L, greater_equals_index);
     if (vargs_size == 0) {
@@ -43,8 +31,12 @@ std::string luaE_checkfformat(lua_State * L, int greater_equals_index)
     }
     assert(vargs_size >= 1);
 
-    uint64_t types = 0;
-    ArgArrayType array = {0,};
+    auto const DATA_SIZE = vargs_size + 1;
+    using Context = fmt::format_context;
+    using ValueType = fmt::basic_format_arg<Context>;
+    std::shared_ptr<ValueType> data(new ValueType [DATA_SIZE], std::default_delete<ValueType[]>());
+    auto * array = data.get();
+
     int next_lua_arg_index = greater_equals_index + 1;
     int insert_index = 0;
 
@@ -60,8 +52,7 @@ std::string luaE_checkfformat(lua_State * L, int greater_equals_index)
         case LUA_TBOOLEAN:
             BLOCK_DO("Lua boolean type") {
                 bool value = luaL_checkboolean(L, next_lua_arg_index);
-                types |= (fmt::internal::make_type(value) << (4*insert_index));
-                array[insert_index] = ArgArray::make<BasicFormatter>(value);
+                array[insert_index] = fmt::internal::make_arg<false, Context>(value);
             } BLOCK_END();
             ++insert_index;
             break;
@@ -72,12 +63,10 @@ std::string luaE_checkfformat(lua_State * L, int greater_equals_index)
         case LUA_TNUMBER:
             if (lua_isinteger(L, next_lua_arg_index)) {
                 int value = luaL_checkinteger(L, next_lua_arg_index);
-                types |= (fmt::internal::make_type(value) << (4*insert_index));
-                array[insert_index] = ArgArray::make<BasicFormatter>(value);
+                array[insert_index] = fmt::internal::make_arg<false, Context>(value);
             } else {
                 double value = luaL_checknumber(L, next_lua_arg_index);
-                types |= (fmt::internal::make_type(value) << (4*insert_index));
-                array[insert_index] = ArgArray::make<BasicFormatter>(value);
+                array[insert_index] = fmt::internal::make_arg<false, Context>(value);
             }
             ++insert_index;
             break;
@@ -85,8 +74,7 @@ std::string luaE_checkfformat(lua_State * L, int greater_equals_index)
         case LUA_TSTRING:
             BLOCK_DO("Lua string type") {
                 char const * value = luaL_checkstring(L, next_lua_arg_index);
-                types |= (fmt::internal::make_type(value) << (4*insert_index));
-                array[insert_index] = ArgArray::make<BasicFormatter>(value);
+                array[insert_index] = fmt::internal::make_arg<false, Context>(value);
             } BLOCK_END();
             ++insert_index;
             break;
@@ -108,9 +96,11 @@ std::string luaE_checkfformat(lua_State * L, int greater_equals_index)
             return std::string();
         }
     }
-    return fmt::format(format_text, ArgList(types, array));
-#endif
-    return std::string();
+
+    fmt::basic_memory_buffer<char> buffer;
+    fmt::basic_format_args<Context> args(array, vargs_size);
+    fmt::internal::vformat_to(buffer, fmt::to_string_view(format_text), args);
+    return fmt::to_string(buffer);
 }
 
 int _format(lua_State * L)
