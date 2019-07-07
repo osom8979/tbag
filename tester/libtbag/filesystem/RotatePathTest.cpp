ea7bde6899d9f08ca84c6783ccf0464e8bcb688f
zer0 <osom8979@gmail.com>
2019-07-07 22:14:30 +0900

    Test RotatePath::createParams() method.

--- tester/libtbag/filesystem/RotatePathTest.cpp
+++ tester/libtbag/filesystem/RotatePathTest.cpp
@@ -41,3 +41,29 @@ TEST(RotatePathTest, Default)
     ASSERT_NE(path1, path2);
 }
 
+TEST(RotatePathTest, CreateParams_01)
+{
+    auto params = RotatePath::createParams("size=2k counter=/prefix/path/log,.log,2");
+    ASSERT_TRUE((bool)params.first);
+    ASSERT_TRUE((bool)params.second);
+
+    ASSERT_EQ(2048, ((SizeChecker*)params.first.get())->max_size);
+    ASSERT_STREQ("/prefix/path/log", ((CounterUpdater*)params.second.get())->prefix.c_str());
+    ASSERT_STREQ(".log", ((CounterUpdater*)params.second.get())->suffix.c_str());
+    ASSERT_EQ(2, ((CounterUpdater*)params.second.get())->count);
+}
+
+TEST(RotatePathTest, CreateParams_02)
+{
+    RotatePath::Environments envs;
+    envs.push("SIZE", "2m");
+    envs.push("FORMAT", "$ps.log");
+
+    auto params = RotatePath::createParams("size=${SIZE} time=${FORMAT}", envs);
+    ASSERT_TRUE((bool)params.first);
+    ASSERT_TRUE((bool)params.second);
+
+    ASSERT_EQ(1024*1024*2, ((SizeChecker*)params.first.get())->max_size);
+    ASSERT_STREQ("$ps.log", ((TimeFormatUpdater*)params.second.get())->format.c_str());
+}
+
