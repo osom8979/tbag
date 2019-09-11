aae857cb459d2897f1c16155c95ddf4639d47d57
zer0 <osom8979@gmail.com>
2019-09-11 10:12:55 +0900

    Create ErrPair class template.

---
+++ libtbag/ErrPair.hpp
@@ -0,0 +1,169 @@
+/**
+ * @file   ErrPair.hpp
+ * @brief  ErrPair class prototype.
+ * @author zer0
+ * @date   2019-09-11
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_ERRPAIR_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_ERRPAIR_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/config.h>
+#include <libtbag/predef.hpp>
+#include <libtbag/Err.hpp>
+
+#include <type_traits>
+#include <string>
+#include <ostream>
+
+// -------------------
+NAMESPACE_LIBTBAG_OPEN
+// -------------------
+
+/**
+ * ErrPair class template.
+ *
+ * @author zer0
+ * @date   2019-09-11
+ */
+template <typename T>
+struct ErrPair
+{
+    using ValueType = T;
+
+    static_assert(!std::is_void<ValueType>::value, "Void type is not supported.");
+    static_assert(!std::is_reference<ValueType>::value, "Reference type is not supported.");
+
+    Err code;
+    ValueType value;
+
+    ErrPair() TBAG_NOEXCEPT_SP_OP(std::is_nothrow_default_constructible<ValueType>::value)
+            : code(E_UNKNOWN), value()
+    { /* EMPTY. */ }
+    ErrPair(Err c) TBAG_NOEXCEPT_SP_OP(std::is_nothrow_default_constructible<ValueType>::value)
+            : code(c), value()
+    { /* EMPTY. */ }
+
+    ErrPair(ValueType const & v) TBAG_NOEXCEPT_SP_OP(std::is_nothrow_copy_constructible<ValueType>::value)
+            : code(E_UNKNOWN), value(v)
+    { /* EMPTY. */ }
+    ErrPair(Err c, ValueType const & v) TBAG_NOEXCEPT_SP_OP(std::is_nothrow_copy_constructible<ValueType>::value)
+            : code(c), value(v)
+    { /* EMPTY. */ }
+
+    ErrPair(ValueType && v) TBAG_NOEXCEPT_SP_OP(std::is_nothrow_move_constructible<ValueType>::value)
+            : code(E_UNKNOWN), value(std::move(v))
+    { /* EMPTY. */ }
+    ErrPair(Err c, ValueType && v) TBAG_NOEXCEPT_SP_OP(std::is_nothrow_move_constructible<ValueType>::value)
+            : code(c), value(std::move(v))
+    { /* EMPTY. */ }
+
+    ErrPair(ErrPair const & obj) TBAG_NOEXCEPT_SP_OP(std::is_nothrow_copy_constructible<ValueType>::value)
+            : code(obj.code), value(obj.value)
+    { /* EMPTY. */ }
+    ErrPair(ErrPair && obj) TBAG_NOEXCEPT_SP_OP(std::is_nothrow_move_constructible<ValueType>::value)
+            : code(obj.code), value(std::move(obj.value))
+    { /* EMPTY. */ }
+
+    ~ErrPair()
+    { /* EMPTY. */ }
+
+    ErrPair & operator =(Err c) TBAG_NOEXCEPT
+    {
+        code = c;
+        return *this;
+    }
+
+    ErrPair & operator =(ValueType const & v) TBAG_NOEXCEPT_SP_OP(std::is_nothrow_copy_assignable<ValueType>::value)
+    {
+        value = v;
+        return *this;
+    }
+
+    ErrPair & operator =(ValueType && v) TBAG_NOEXCEPT_SP_OP(std::is_nothrow_move_assignable<ValueType>::value)
+    {
+        value = std::move(v);
+        return *this;
+    }
+
+    ErrPair & operator =(ErrPair const & obj) TBAG_NOEXCEPT_SP_OP(std::is_nothrow_copy_assignable<ValueType>::value)
+    {
+        if (this != &obj) {
+            code = obj.code;
+            value = obj.value;
+        }
+        return *this;
+    }
+
+    ErrPair & operator =(ErrPair && obj) TBAG_NOEXCEPT_SP_OP(std::is_nothrow_move_assignable<ValueType>::value)
+    {
+        if (this != &obj) {
+            code = obj.code;
+            value = std::move(obj.value);
+        }
+        return *this;
+    }
+
+    char const * getErrName() const TBAG_NOEXCEPT
+    {
+        return libtbag::getErrName(code);
+    }
+
+    char const * getErrDetail() const TBAG_NOEXCEPT
+    {
+        return libtbag::getErrDetail(code);
+    }
+
+    inline operator Err() const TBAG_NOEXCEPT
+    {
+        return code;
+    }
+
+    inline operator ValueType() const TBAG_NOEXCEPT
+    {
+        return value;
+    }
+
+    inline operator bool() const TBAG_NOEXCEPT
+    {
+        return isSuccess(code);
+    }
+
+    operator char const *() const TBAG_NOEXCEPT
+    {
+        return getErrName();
+    }
+
+    operator std::string() const
+    {
+        return std::string(getErrName());
+    }
+
+    inline int toInt() const TBAG_NOEXCEPT
+    {
+        return static_cast<int>(code);
+    }
+
+    inline void fromInt(int v) TBAG_NOEXCEPT
+    {
+        code = static_cast<Err>(v);
+    }
+
+    template <class CharT, class TraitsT>
+    friend std::basic_ostream<CharT, TraitsT> & operator<<(std::basic_ostream<CharT, TraitsT> & os, ErrPair const & err)
+    {
+        return os << err.getErrName();
+    }
+};
+
+// --------------------
+NAMESPACE_LIBTBAG_CLOSE
+// --------------------
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_ERRPAIR_HPP__
+
---
+++ tester/libtbag/ErrPairTest.cpp
@@ -0,0 +1,46 @@
+/**
+ * @file   ErrPairTest.cpp
+ * @brief  ErrPair class tester.
+ * @author zer0
+ * @date   2019-09-11
+ */
+
+#include <gtest/gtest.h>
+#include <libtbag/ErrPair.hpp>
+#include <libtbag/util/TestUtils.hpp>
+
+#include <string>
+#include <sstream>
+
+using namespace libtbag;
+
+TEST(ErrPairTest, Default)
+{
+    TEST_DEFAULT_CONSTRUCTOR(ErrPair<int>, obj1);
+    TEST_DEFAULT_ASSIGNMENT(ErrPair<int>, obj2);
+}
+
+TEST(ErrPairTest, StringCast)
+{
+    ErrPair<int> obj(E_ILLARGS);
+    std::string const str = obj;
+    ASSERT_STREQ("E_ILLARGS", str.c_str());
+}
+
+TEST(ErrPairTest, StreamCast)
+{
+    ErrPair<int> obj(E_ILLARGS);
+    std::stringstream ss;
+    ss << obj;
+    ASSERT_STREQ("E_ILLARGS", ss.str().c_str());
+}
+
+TEST(ErrPairTest, BooleanCast)
+{
+    ErrPair<int> obj1(E_ILLARGS);
+    ASSERT_FALSE(static_cast<bool>(obj1));
+
+    ErrPair<int> obj2(E_SUCCESS);
+    ASSERT_TRUE(static_cast<bool>(obj2));
+}
+
