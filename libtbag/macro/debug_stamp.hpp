bcee98388461d6ff1f7cd3ab0e3f5ba1b5168e98
zer0 <osom8979@gmail.com>
2019-12-10 17:39:07 +0900

    Create TBAG_DEBUG_STAMP macro.

---
+++ libtbag/macro/debug_stamp.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file   debug_stamp.hpp
+ * @brief  debug_stamp class prototype.
+ * @author zer0
+ * @date   2019-12-10
+ */
+
+#ifndef __INCLUDE_LIBTBAG__LIBTBAG_MACRO_DEBUG_STAMP_HPP__
+#define __INCLUDE_LIBTBAG__LIBTBAG_MACRO_DEBUG_STAMP_HPP__
+
+// MS compatible compilers support #pragma once
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#include <libtbag/macro/utils.hpp>
+#include <libtbag/macro/function.hpp>
+
+#ifndef TBAG_DEBUG_STAMP
+#define TBAG_DEBUG_STAMP __LOCATION__
+#endif
+
+#endif // __INCLUDE_LIBTBAG__LIBTBAG_MACRO_DEBUG_STAMP_HPP__
+
