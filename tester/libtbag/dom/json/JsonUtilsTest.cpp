38aacc4d8992783d5e838bf92fa7d6395e84840d
zer0 <osom8979@gmail.com>
2019-12-23 10:49:57 +0900

    Create JsonUtilsTest.Path tester.

--- tester/libtbag/dom/json/JsonUtilsTest.cpp
+++ tester/libtbag/dom/json/JsonUtilsTest.cpp
@@ -12,6 +12,37 @@ using namespace libtbag;
 using namespace libtbag::dom;
 using namespace libtbag::dom::json;
 
+TEST(JsonUtilsTest, Path)
+{
+    char const * const TEST_JSON_TEXT = R"(
+{
+    "key1": { "temp": 1 },
+    "key2": { "temp": 2 },
+    "key3": { "temp": 3 }
+})";
+    Json::Value original;
+    ASSERT_TRUE(parse(TEST_JSON_TEXT, original));
+
+    Json::Path path1(".key1.temp");
+    auto const value1 = path1.make(original);
+    ASSERT_TRUE(value1.isInt());
+    ASSERT_EQ(1, value1.asInt());
+
+    Json::Path path2(".key2.temp");
+    auto const value2 = path2.make(original);
+    ASSERT_TRUE(value2.isInt());
+    ASSERT_EQ(2, value2.asInt());
+
+    Json::Path path3(".key3.temp");
+    auto const value3 = path3.make(original);
+    ASSERT_TRUE(value3.isInt());
+    ASSERT_EQ(3, value3.asInt());
+
+    Json::Path path_error(".test");
+    auto const value_error = path_error.make(original);
+    ASSERT_TRUE(value_error.isNull());
+}
+
 TEST(JsonUtilsTest, Default)
 {
     char const * const TEST_JSON_TEXT = R"({"key":"value"})";
