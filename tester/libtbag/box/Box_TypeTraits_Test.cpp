fb85a458dc134b334c02551c8016fde85dae883b
zer0 <osom8979@gmail.com>
2019-12-28 18:40:07 +0900

    Create Box_TypeTraits_Test.reshape_selector tester.

--- tester/libtbag/box/Box_TypeTraits_Test.cpp
+++ tester/libtbag/box/Box_TypeTraits_Test.cpp
@@ -159,3 +159,25 @@ TEST(Box_TypeTraits_Test, is_first_bdev_and_second_ui64_ptr)
     EXPECT_FALSE((is_first_bdev_and_second_ui64_ptr<int,bdev,ui64 *>::value));
 }
 
+TEST(Box_TypeTraits_Test, reshape_selector)
+{
+    using namespace libtbag::box::details;
+
+    EXPECT_TRUE((std::is_same<typename Box::reshape_selector<bdev,ui64*,int>::type, Box::reshape_args1_t>::value));
+    EXPECT_TRUE((std::is_same<typename Box::reshape_selector<int>::type, Box::reshape_args2_t>::value));
+
+    EXPECT_TRUE((std::is_same<typename Box::reshape_selector<bdev&&,ui64*,ui32&&,int&&>::type, Box::reshape_args1_t>::value));
+    EXPECT_TRUE((std::is_same<typename Box::reshape_selector<int&&,int&&>::type, Box::reshape_args2_t>::value));
+
+    EXPECT_TRUE((std::is_same<typename Box::reshape_selector<bdev,ui64*,ui32,va_list>::type, Box::reshape_vargs1_t>::value));
+    EXPECT_TRUE((std::is_same<typename Box::reshape_selector<ui32,va_list>::type, Box::reshape_vargs2_t>::value));
+
+    EXPECT_TRUE((std::is_same<typename Box::reshape_selector<bdev,ui64*,ui32,ui32*>::type, Box::reshape_dims1_t>::value));
+    EXPECT_TRUE((std::is_same<typename Box::reshape_selector<ui32,ui32*>::type, Box::reshape_dims2_t>::value));
+
+    EXPECT_TRUE((std::is_same<typename Box::reshape_selector<box_data&&>::type, Box::reshape_ref_box1_t>::value));
+    EXPECT_TRUE((std::is_same<typename Box::reshape_selector<Box&&>::type, Box::reshape_ref_box2_t>::value));
+
+    EXPECT_TRUE((std::is_same<typename Box::reshape_selector<Box,Box>::type, Box::reshape_unknown_t>::value));
+}
+
