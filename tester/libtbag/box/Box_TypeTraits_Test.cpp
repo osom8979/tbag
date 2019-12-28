eb1779b5dd1fb888fb95f546656c3b98a529eb3f
zer0 <osom8979@gmail.com>
2019-12-28 17:44:14 +0900

    Create Box_TypeTraits_Test tester.

---
+++ tester/libtbag/box/Box_TypeTraits_Test.cpp
@@ -0,0 +1,161 @@
+/**
+ * @file   Box_TypeTraits_Test.cpp
+ * @brief  Box class tester.
+ * @author zer0
+ * @date   2019-12-28
+ */
+
+#include <gtest/gtest.h>
+#include <libtbag/box/Box.hpp>
+
+using namespace libtbag;
+using namespace libtbag::box;
+
+TEST(Box_TypeTraits_Test, is_first_box_data)
+{
+    EXPECT_FALSE(is_first_box_data<Box const &&>::value);
+    EXPECT_FALSE(is_first_box_data<Box const &>::value);
+    EXPECT_FALSE(is_first_box_data<Box const *>::value);
+    EXPECT_FALSE(is_first_box_data<Box const>::value);
+    EXPECT_FALSE(is_first_box_data<Box &&>::value);
+    EXPECT_FALSE(is_first_box_data<Box &>::value);
+    EXPECT_FALSE(is_first_box_data<Box *>::value);
+    EXPECT_FALSE(is_first_box_data<Box>::value);
+
+    using namespace libtbag::box::details;
+    EXPECT_TRUE(is_first_box_data<box_data const &&>::value);
+    EXPECT_TRUE(is_first_box_data<box_data const &>::value);
+    EXPECT_TRUE(is_first_box_data<box_data const *>::value);
+    EXPECT_TRUE(is_first_box_data<box_data const>::value);
+    EXPECT_TRUE(is_first_box_data<box_data &&>::value);
+    EXPECT_TRUE(is_first_box_data<box_data &>::value);
+    EXPECT_TRUE(is_first_box_data<box_data *>::value);
+    EXPECT_TRUE(is_first_box_data<box_data>::value);
+
+    EXPECT_TRUE((is_first_box_data<box_data,int>::value));
+    EXPECT_TRUE((is_first_box_data<box_data,int,double>::value));
+
+    EXPECT_FALSE((is_first_box_data<int,box_data>::value));
+    EXPECT_FALSE((is_first_box_data<double,int,box_data>::value));
+}
+
+TEST(Box_TypeTraits_Test, is_first_Box)
+{
+    EXPECT_TRUE(is_first_Box<Box const &&>::value);
+    EXPECT_TRUE(is_first_Box<Box const &>::value);
+    EXPECT_TRUE(is_first_Box<Box const *>::value);
+    EXPECT_TRUE(is_first_Box<Box const>::value);
+    EXPECT_TRUE(is_first_Box<Box &>::value);
+    EXPECT_TRUE(is_first_Box<Box *>::value);
+    EXPECT_TRUE(is_first_Box<Box>::value);
+
+    using namespace libtbag::box::details;
+    EXPECT_FALSE(is_first_Box<box_data const &&>::value);
+    EXPECT_FALSE(is_first_Box<box_data const &>::value);
+    EXPECT_FALSE(is_first_Box<box_data const *>::value);
+    EXPECT_FALSE(is_first_Box<box_data const>::value);
+    EXPECT_FALSE(is_first_Box<box_data &>::value);
+    EXPECT_FALSE(is_first_Box<box_data *>::value);
+    EXPECT_FALSE(is_first_Box<box_data>::value);
+
+    EXPECT_TRUE((is_first_Box<Box,int>::value));
+    EXPECT_TRUE((is_first_Box<Box,int,double>::value));
+
+    EXPECT_FALSE((is_first_Box<int,Box>::value));
+    EXPECT_FALSE((is_first_Box<double,int,Box>::value));
+}
+
+TEST(Box_TypeTraits_Test, is_last_va_list)
+{
+    EXPECT_TRUE(is_last_va_list<va_list const &&>::value);
+    EXPECT_TRUE(is_last_va_list<va_list const &>::value);
+    EXPECT_TRUE(is_last_va_list<va_list const>::value);
+    EXPECT_TRUE(is_last_va_list<va_list &&>::value);
+    EXPECT_TRUE(is_last_va_list<va_list &>::value);
+    EXPECT_TRUE(is_last_va_list<va_list>::value);
+
+    EXPECT_FALSE(is_last_va_list<va_list const *>::value);
+    EXPECT_FALSE(is_last_va_list<va_list *>::value);
+
+    EXPECT_FALSE(is_last_va_list<int const &&>::value);
+    EXPECT_FALSE(is_last_va_list<int const &>::value);
+    EXPECT_FALSE(is_last_va_list<int const *>::value);
+    EXPECT_FALSE(is_last_va_list<int const>::value);
+    EXPECT_FALSE(is_last_va_list<int &&>::value);
+    EXPECT_FALSE(is_last_va_list<int &>::value);
+    EXPECT_FALSE(is_last_va_list<int *>::value);
+    EXPECT_FALSE(is_last_va_list<int>::value);
+
+    EXPECT_TRUE((is_last_va_list<int,va_list>::value));
+    EXPECT_TRUE((is_last_va_list<double,int,va_list>::value));
+
+    EXPECT_FALSE((is_last_va_list<va_list,int>::value));
+    EXPECT_FALSE((is_last_va_list<va_list,int,double>::value));
+}
+
+TEST(Box_TypeTraits_Test, is_last_ui32_ptr)
+{
+    using namespace libtbag::box::details;
+    EXPECT_FALSE(is_last_ui32_ptr<ui32 const &&>::value);
+    EXPECT_FALSE(is_last_ui32_ptr<ui32 const &>::value);
+    EXPECT_FALSE(is_last_ui32_ptr<ui32 const>::value);
+    EXPECT_FALSE(is_last_ui32_ptr<ui32 &&>::value);
+    EXPECT_FALSE(is_last_ui32_ptr<ui32 &>::value);
+    EXPECT_FALSE(is_last_ui32_ptr<ui32>::value);
+
+    EXPECT_TRUE(is_last_ui32_ptr<ui32 const *>::value);
+    EXPECT_TRUE(is_last_ui32_ptr<ui32 *>::value);
+
+    EXPECT_FALSE(is_last_ui32_ptr<int const &&>::value);
+    EXPECT_FALSE(is_last_ui32_ptr<int const &>::value);
+    EXPECT_FALSE(is_last_ui32_ptr<int const *>::value);
+    EXPECT_FALSE(is_last_ui32_ptr<int const>::value);
+    EXPECT_FALSE(is_last_ui32_ptr<int &&>::value);
+    EXPECT_FALSE(is_last_ui32_ptr<int &>::value);
+    EXPECT_FALSE(is_last_ui32_ptr<int *>::value);
+    EXPECT_FALSE(is_last_ui32_ptr<int>::value);
+
+    EXPECT_TRUE((is_last_ui32_ptr<int,ui32*>::value));
+    EXPECT_TRUE((is_last_ui32_ptr<double,int,ui32*>::value));
+
+    EXPECT_FALSE((is_last_ui32_ptr<ui32*,int>::value));
+    EXPECT_FALSE((is_last_ui32_ptr<ui32*,int,double>::value));
+}
+
+TEST(Box_TypeTraits_Test, is_first_bdev_and_second_ui64_ptr)
+{
+    using namespace libtbag::box::details;
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<ui64 const &&>::value);
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<ui64 const &>::value);
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<ui64 const *>::value);
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<ui64 const>::value);
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<ui64 &&>::value);
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<ui64 &>::value);
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<ui64 *>::value);
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<ui64>::value);
+
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<bdev const &&>::value);
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<bdev const &>::value);
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<bdev const *>::value);
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<bdev const>::value);
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<bdev &&>::value);
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<bdev &>::value);
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<bdev *>::value);
+    EXPECT_FALSE(is_first_bdev_and_second_ui64_ptr<bdev>::value);
+
+    EXPECT_FALSE((is_first_bdev_and_second_ui64_ptr<bdev,ui64 const &&>::value));
+    EXPECT_FALSE((is_first_bdev_and_second_ui64_ptr<bdev,ui64 const &>::value));
+    EXPECT_FALSE((is_first_bdev_and_second_ui64_ptr<bdev,ui64 const>::value));
+    EXPECT_FALSE((is_first_bdev_and_second_ui64_ptr<bdev,ui64 &&>::value));
+    EXPECT_FALSE((is_first_bdev_and_second_ui64_ptr<bdev,ui64 &>::value));
+    EXPECT_FALSE((is_first_bdev_and_second_ui64_ptr<bdev,ui64>::value));
+
+    EXPECT_TRUE((is_first_bdev_and_second_ui64_ptr<bdev,ui64 const *>::value));
+    EXPECT_TRUE((is_first_bdev_and_second_ui64_ptr<bdev,ui64 *>::value));
+    EXPECT_TRUE((is_first_bdev_and_second_ui64_ptr<bdev,ui64 const *,int>::value));
+    EXPECT_TRUE((is_first_bdev_and_second_ui64_ptr<bdev,ui64 *,int>::value));
+
+    EXPECT_FALSE((is_first_bdev_and_second_ui64_ptr<int,bdev,ui64 const *>::value));
+    EXPECT_FALSE((is_first_bdev_and_second_ui64_ptr<int,bdev,ui64 *>::value));
+}
+
