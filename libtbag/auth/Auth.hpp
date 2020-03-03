0601de9005b0e035531666ea5b09d24c1ff6c975
zer0 <osom8979@gmail.com>
2020-03-03 17:47:00 +0900

    Update documentation: Auth class.

--- libtbag/auth/Auth.hpp
+++ libtbag/auth/Auth.hpp
@@ -9,11 +9,19 @@
  * @msc
  *   Client,Server;
  *   --- [label="request/replay #1"];
+ *
+ *   # Send client's public_key -{encode ca-pub-key}-> Received by server. (decode ca-pri-key)
  *   Client->Server [label="ca_public_key/encode/client_public_key"];
+ *
+ *   # Send server's public_key -{encode ca-pri-key}-> Received by client. (decode ca-pub-key)
  *   Server->Client [label="ca_private_key/encode/server_public_key"];
  *
  *   --- [label="request/replay #2"];
+ *
+ *   # Send client's data -{encode server-pub-key}-> Received by server. (decode server-pri-key)
  *   Client->Server [label="server_public_key/encode/client_data"];
+ *
+ *   # Send server's data -{encode client-pub-key}-> Received by client. (decode client-pri-key)
  *   Server->Client [label="client_public_key/encode/server_data"];
  * @endmsc
  */
@@ -38,6 +46,91 @@ NAMESPACE_LIBTBAG_OPEN
 // -------------------
 
 namespace auth {
+
+/* [NO DOC] Example:
+ * <pre>
+ *  ------------------------------------------------------------------------------------------------------
+ *                  CLIENT SIDE                         |                SERVER SIDE
+ *  ------------------------------------------------------------------------------------------------------
+ *                                                      |
+ *  using namespace libtbag::crypto;                    | using namespace libtbag::crypto;
+ *  std::string ca_public_key_pem;                      | std::string ca_private_key_pem;
+ *                                                      |
+ *  PublicKey ca_public_key;                            | PrivateKey ca_private_key;
+ *  ca_public_key.readPem(ca_public_key_pem);           | ca_private_key.readPem(ca_private_key_pem);
+ *                                                      |
+ *  std::string client_data = "...";                    | std::string server_data = "...";
+ *                                                      |
+ *  AuthClient client;                                  | AuthServer server;
+ *  client.init(ca_public_key.key(), client_data));     | server.init(ca_private_key.key(), server_data));
+ *                                                      |
+ *  ------------------------------------------------------------------------------------------------------
+ *                                                Initalize DONE.
+ *  ------------------------------------------------------------------------------------------------------
+ *                                                      |
+ *  assert(client.isReady();                            | assert(server.isReady();
+ *  assert(client.getServerData().empty());             | assert(server.getClientData().empty());
+ *                                                      |
+ *  auto const encoded_auth0 = client.encodeAuth0();    |
+ *  // Send 'encoded_auth0' to the server ...           >
+ *                                                      > // Recv 'encoded_auth0' from the client ...
+ *                                                      | server.decodeAuth0(encoded_auth0)
+ *                                                      |
+ *                                                      | auto const encoded_auth1 = server.encodeAuth1();
+ *                                                      < // Send 'encoded_auth1' to the client ...
+ *  // Recv 'encoded_auth1' from the server ...         <
+ *  client.decodeAuth1(encoded_auth1);                  |
+ *                                                      |
+ *  ------------------------------------------------------------------------------------------------------
+ *                                          REQUEST and REPLAY #1 DONE.
+ *  ------------------------------------------------------------------------------------------------------
+ *                                                      |
+ *  auto const encoded_auth2 = client.encodeAuth2();    |
+ *  // Send 'encoded_auth2' to the server ...           >
+ *                                                      > // Recv 'encoded_auth2' from the client ...
+ *                                                      | server.decodeAuth2(encoded_auth2);
+ *                                                      |
+ *                                                      | auto const encoded_auth3 = server.encodeAuth3();
+ *                                                      < // Send 'encoded_auth3' to the client ...
+ *  // Recv 'encoded_auth3' from the server ...         <
+ *  client.decodeAuth3(encoded_auth3);                  |
+ *                                                      |
+ *  ------------------------------------------------------------------------------------------------------
+ *                                         REQUEST and REPLAY #2 DONE.
+ *  ------------------------------------------------------------------------------------------------------
+ *                                                      |
+ *  auto const server_data = client.getServerData();    | auto const client_data = server.getClientData();
+ *                                                      |
+ *  // Test 'server_data' validation ...                | // Test 'client_data' validation ...
+ *                                                      |
+ *  client.ok();                                        | server.ok();
+ *                                                      |
+ *  assert(client.validation());                        | assert(server.validation());
+ *                                                      |
+ *  ------------------------------------------------------------------------------------------------------
+ *                                                Validation OK.
+ *  ------------------------------------------------------------------------------------------------------
+ *                                                      |
+ *  std::string data1 = "...";                          |
+ *  auto const encode_data1 = client.encode(data1);     |
+ *  // Send 'encode_data1' to the server ...            >
+ *                                                      > // Recv 'encode_data1' from the client ...
+ *                                                      | auto const data1 = server.decode(encode_data1);
+ *                                                      |
+ *                                                      | // Use 'data1' ...
+ *                                                      |
+ *                                                      | std::string data2 = "...";
+ *                                                      | auto const encode_data2 = server.encode(data2);
+ *                                                      < // Send 'encode_data2' to the client ...
+ *  // Recv 'encode_data2' from the server ...          <
+ *  auto const data2 = client.decode(encode_data2);     |
+ *                                                      |
+ *  // Use 'data2' ...                                  |
+ *                                                      |
+ *  ------------------------------------------------------------------------------------------------------
+ *                                         Sensitive Data Delivery DONE.
+ *  ------------------------------------------------------------------------------------------------------
+ */
 
 using PublicKey = libtbag::crypto::PublicKey;
 using PrivateKey = libtbag::crypto::PrivateKey;
