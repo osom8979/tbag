9fe95e6cf65cc813972b3e2b2f6635a06c6894d6
zer0 <osom8979@gmail.com>
2020-04-29 15:36:33 +0900

    Update doxygen documentation: testCompatible() method.

--- libtbag/util/VersionCompatible.hpp
+++ libtbag/util/VersionCompatible.hpp
@@ -30,6 +30,43 @@ namespace util {
 /**
  * Test version compatibility.
  *
+ * Example:
+ * <table>
+ *   <caption id="multi_row">Python versioning with requirements.txt syntax</caption>
+ *   <tr>
+ *     <th>Requirement</th>
+ *     <th>Description</th>
+ *   </tr>
+ *   <tr>
+ *     <td>foo</td>
+ *     <td>any version of foo</td>
+ *   <tr>
+ *   <tr>
+ *     <td>foo>=5</td>
+ *     <td>any version of foo, above or equal to 5</td>
+ *   <tr>
+ *   <tr>
+ *     <td>foo>=5.6</td>
+ *     <td>any version of foo, above or equal to 5.6</td>
+ *   <tr>
+ *   <tr>
+ *     <td>foo==5.6.1</td>
+ *     <td>exact match</td>
+ *   <tr>
+ *   <tr>
+ *     <td>foo>5</td>
+ *     <td>foo-5 or greater, including minor and patch</td>
+ *   <tr>
+ *   <tr>
+ *     <td>foo>5, <5.7</td>
+ *     <td>foo-5 or greater, but less than foo-5.7</td>
+ *   <tr>
+ *   <tr>
+ *     <td>foo>0, <5.7</td>
+ *     <td>any foo version less than foo-5.7</td>
+ *   <tr>
+ * </table>
+ *
  * @retval E_SUCCESS
  *  Compatible version.
  * @retval E_VERSION
